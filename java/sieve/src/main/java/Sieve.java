26624be0b40413beb985b24c2991366d5255f2d0
Kai Wang <charles.wangkai@gmail.com>
2017-09-14 08:39:38 +0800

    java/sieve

---
+++ java/sieve/src/main/java/Sieve.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Sieve {
+	int maxPrime;
+
+	Sieve(int maxPrime) {
+		this.maxPrime = maxPrime;
+	}
+
+	List<Integer> getPrimes() {
+		boolean[] primes = new boolean[maxPrime + 1];
+		Arrays.fill(primes, true);
+
+		for (int i = 2; i < primes.length; i++) {
+			for (long j = (long) i * i; j < primes.length; j += i) {
+				primes[(int) j] = false;
+			}
+		}
+
+		return IntStream.range(2, primes.length).filter(x -> primes[x]).collect(ArrayList<Integer>::new, List::add,
+				List::addAll);
+	}
+}
