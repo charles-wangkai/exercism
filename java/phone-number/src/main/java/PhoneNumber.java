422806f77525b33fcd8ece1e819bd8d1493d61a7
Kai Wang <charles.wangkai@gmail.com>
2017-09-14 18:04:57 +0800

    java/phone-number

---
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -0,0 +1,28 @@
+public class PhoneNumber {
+	String number;
+
+	PhoneNumber(String s) {
+		if (!s.chars().allMatch(ch -> Character.isDigit(ch) || " ()-.".indexOf(ch) >= 0)) {
+			throw new IllegalArgumentException(
+					"Illegal character in phone number. Only digits, spaces, parentheses, hyphens or dots accepted.");
+		}
+
+		StringBuilder digits = s.chars().filter(Character::isDigit).mapToObj(ch -> (char) ch)
+				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+
+		int length = digits.length();
+		if (length != 10 && length != 11) {
+			throw new IllegalArgumentException("Number must be 10 or 11 digits");
+		}
+
+		if (length == 11 && digits.charAt(0) != '1') {
+			throw new IllegalArgumentException("Can only have 11 digits if number starts with '1'");
+		}
+
+		number = digits.substring(length - 10, length);
+	}
+
+	String getNumber() {
+		return number;
+	}
+}
---
+++ java/phone-number/src/test/java/PhoneNumberTest.java
@@ -0,0 +1,88 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class PhoneNumberTest {
+	private final static String wrongLengthExceptionMessage = "Number must be 10 or 11 digits";
+	private final static String numberIs11DigitsButDoesNotStartWith1ExceptionMessage = "Can only have 11 digits if number starts with '1'";
+	private final static String illegalCharacterExceptionMessage = "Illegal character in phone number. Only digits, spaces, parentheses, hyphens or dots accepted.";
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void cleansNumber() {
+		final String expectedNumber = "1234567890";
+		final String actualNumber = new PhoneNumber("(123) 456-7890").getNumber();
+
+		assertEquals(expectedNumber, actualNumber);
+	}
+
+	@Test
+	public void cleansNumberWithDots() {
+		final String expectedNumber = "1234567890";
+		final String actualNumber = new PhoneNumber("123.456.7890").getNumber();
+
+		assertEquals(expectedNumber, actualNumber);
+	}
+
+	@Test
+	public void cleansNumberWithMultipleSpaces() {
+		final String expectedNumber = "1234567890";
+		final String actualNumber = new PhoneNumber("123 456   7890   ").getNumber();
+
+		assertEquals(expectedNumber, actualNumber);
+	}
+
+	@Test
+	public void invalidWhen9Digits() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(wrongLengthExceptionMessage);
+		new PhoneNumber("123456789");
+	}
+
+	@Test
+	public void invalidWhen11Digits() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(numberIs11DigitsButDoesNotStartWith1ExceptionMessage);
+		new PhoneNumber("21234567890");
+	}
+
+	@Test
+	public void validWhen11DigitsAndFirstIs1() {
+		final String expectedNumber = "1234567890";
+		final String actualNumber = new PhoneNumber("11234567890").getNumber();
+
+		assertEquals(expectedNumber, actualNumber);
+	}
+
+	@Test
+	public void invalidWhen12Digits() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(wrongLengthExceptionMessage);
+		new PhoneNumber("321234567890");
+	}
+
+	@Test
+	public void invalidWithLetters() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(illegalCharacterExceptionMessage);
+		new PhoneNumber("123-abc-7890");
+	}
+
+	@Test
+	public void invalidWithPunctuation() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(illegalCharacterExceptionMessage);
+		new PhoneNumber("123-@:!-7890");
+	}
+
+	@Test
+	public void invalidWithRightNumberOfDigitsButLettersMixedIn() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(illegalCharacterExceptionMessage);
+		new PhoneNumber("1a2b3c4d5e6f7g8h9i0j");
+	}
+}
