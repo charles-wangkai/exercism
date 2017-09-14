665c2708420e48e799a3e72879a505013e72c27b
Kai Wang <charles.wangkai@gmail.com>
2017-09-14 16:05:52 +0800

    java/two-fer [Don't ignore tests]

--- java/two-fer/src/test/java/TwoferTest.java
+++ java/two-fer/src/test/java/TwoferTest.java
@@ -1,50 +1,46 @@
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class TwoferTest {
 
-    private Twofer twofer;
+	private Twofer twofer;
 
-    @Before
-    public void setup() {
-        twofer = new Twofer();
-    }
+	@Before
+	public void setup() {
+		twofer = new Twofer();
+	}
 
-    @Test
-    public void noNameGiven() {
-        String input = null;
-        String expected = "One for you, one for me.";
+	@Test
+	public void noNameGiven() {
+		String input = null;
+		String expected = "One for you, one for me.";
 
-        assertEquals(expected, twofer.twofer(input));
-    }
+		assertEquals(expected, twofer.twofer(input));
+	}
 
-    @Ignore("Remove to run test")
-    @Test
-    public void aNameGiven() {
-        String input = "Alice";
-        String expected = "One for Alice, one for me.";
+	@Test
+	public void aNameGiven() {
+		String input = "Alice";
+		String expected = "One for Alice, one for me.";
 
-        assertEquals(expected, twofer.twofer(input));
-    }
+		assertEquals(expected, twofer.twofer(input));
+	}
 
-    @Ignore("Remove to run test")
-    @Test
-    public void anotherNameGiven() {
-        String input = "Bob";
-        String expected = "One for Bob, one for me.";
+	@Test
+	public void anotherNameGiven() {
+		String input = "Bob";
+		String expected = "One for Bob, one for me.";
 
-        assertEquals(expected, twofer.twofer(input));
-    }
+		assertEquals(expected, twofer.twofer(input));
+	}
 
-    @Ignore("Remove to run test")
-    @Test
-    public void emptyStringGiven() {
-        String input = "";
-        String expected = "One for , one for me.";
+	@Test
+	public void emptyStringGiven() {
+		String input = "";
+		String expected = "One for , one for me.";
 
-        assertEquals(expected, twofer.twofer(input) );
-    }
+		assertEquals(expected, twofer.twofer(input));
+	}
 }
