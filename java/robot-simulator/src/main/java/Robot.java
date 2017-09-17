8b59db6ff8be10ad45cd2bbb2cf87299eabe6be2
Kai Wang <charles.wangkai@gmail.com>
2017-09-17 21:32:40 +0800

    java/robot-simulator

---
+++ java/robot-simulator/src/main/java/Robot.java
@@ -0,0 +1,45 @@
+public class Robot {
+	static final int[] X_OFFSETS = { 0, 1, 0, -1 };
+	static final int[] Y_OFFSETS = { 1, 0, -1, 0 };
+
+	GridPosition pos;
+	Orientation orientation;
+
+	Robot(GridPosition pos, Orientation orientation) {
+		this.pos = pos;
+		this.orientation = orientation;
+	}
+
+	GridPosition getGridPosition() {
+		return pos;
+	}
+
+	Orientation getOrientation() {
+		return orientation;
+	}
+
+	void turnRight() {
+		orientation = Orientation.values()[(orientation.ordinal() + 1) % Orientation.values().length];
+	}
+
+	void turnLeft() {
+		orientation = Orientation.values()[(orientation.ordinal() - 1 + Orientation.values().length)
+				% Orientation.values().length];
+	}
+
+	void advance() {
+		pos = new GridPosition(pos.x + X_OFFSETS[orientation.ordinal()], pos.y + Y_OFFSETS[orientation.ordinal()]);
+	}
+
+	void simulate(String instructions) {
+		instructions.chars().forEach(instruction -> {
+			if (instruction == 'R') {
+				turnRight();
+			} else if (instruction == 'L') {
+				turnLeft();
+			} else if (instruction == 'A') {
+				advance();
+			}
+		});
+	}
+}
