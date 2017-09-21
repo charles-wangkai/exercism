f166f8d54df43e5d0bc886846b555ecbd94e98d2
Kai Wang <charles.wangkai@gmail.com>
2017-09-21 12:33:49 +0800

    java/forth

---
+++ java/forth/src/main/java/ForthEvaluator.java
@@ -0,0 +1,112 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class ForthEvaluator {
+	List<Integer> evaluateProgram(List<String> commands) {
+		Stack<Integer> stack = new Stack<Integer>();
+		Map<String, String> name2definition = new HashMap<String, String>();
+		for (String command : commands) {
+			command = command.toLowerCase();
+
+			if (command.startsWith(": ")) {
+				List<String> tokens = tokenize(command.substring(2, command.length() - 2));
+				String name = tokens.get(0);
+
+				if (isNumber(name)) {
+					throw new IllegalArgumentException("Cannot redefine numbers");
+				}
+
+				String definition = String.join(" ", tokens.subList(1, tokens.size()));
+				name2definition.put(name, definition);
+			} else {
+				for (String token : tokenize(replace(command, name2definition))) {
+					if (isNumber(token)) {
+						int number = Integer.parseInt(token);
+						stack.push(number);
+					} else if (token.equals("+")) {
+						checkStackSize(stack, "Addition", 2);
+
+						int number1 = stack.pop();
+						int number2 = stack.pop();
+						stack.push(number2 + number1);
+					} else if (token.equals("-")) {
+						checkStackSize(stack, "Subtraction", 2);
+
+						int number1 = stack.pop();
+						int number2 = stack.pop();
+						stack.push(number2 - number1);
+					} else if (token.equals("*")) {
+						checkStackSize(stack, "Multiplication", 2);
+
+						int number1 = stack.pop();
+						int number2 = stack.pop();
+						stack.push(number2 * number1);
+					} else if (token.equals("/")) {
+						checkStackSize(stack, "Division", 2);
+
+						int number1 = stack.pop();
+						int number2 = stack.pop();
+
+						if (number1 == 0) {
+							throw new IllegalArgumentException("Division by 0 is not allowed");
+						}
+
+						stack.push(number2 / number1);
+					} else if (token.equals("dup")) {
+						checkStackSize(stack, "Duplicating", 1);
+
+						stack.push(stack.peek());
+					} else if (token.equals("drop")) {
+						checkStackSize(stack, "Dropping", 1);
+
+						stack.pop();
+					} else if (token.equals("swap")) {
+						checkStackSize(stack, "Swapping", 2);
+
+						int number1 = stack.pop();
+						int number2 = stack.pop();
+						stack.push(number1);
+						stack.push(number2);
+					} else if (token.equals("over")) {
+						checkStackSize(stack, "Overing", 2);
+
+						stack.push(stack.get(stack.size() - 2));
+					} else {
+						throw new IllegalArgumentException(
+								String.format("No definition available for operator \"%s\"", token));
+					}
+				}
+			}
+		}
+		return stack;
+	}
+
+	void checkStackSize(Stack<Integer> stack, String operation, int minSize) {
+		if (stack.size() < minSize) {
+			throw new IllegalArgumentException(String.format("%s requires that the stack contain at least %d value%s",
+					operation, minSize, minSize == 1 ? "" : "s"));
+		}
+	}
+
+	String replace(String command, Map<String, String> name2definition) {
+		return String.join(" ", tokenize(command).stream().map(token -> name2definition.getOrDefault(token, token))
+				.collect(Collectors.toList()));
+	}
+
+	List<String> tokenize(String s) {
+		return Arrays.stream(s.split(" +")).filter(token -> !token.isEmpty()).collect(Collectors.toList());
+	}
+
+	boolean isNumber(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+}
---
+++ java/forth/src/test/java/ForthEvaluatorTest.java
@@ -0,0 +1,280 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/*
+ * version: 1.2.0
+ */
+public class ForthEvaluatorTest {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	private ForthEvaluator forthEvaluator;
+
+	@Before
+	public void setUp() {
+		forthEvaluator = new ForthEvaluator();
+	}
+
+	@Test
+	public void testEmptyProgramResultsInEmptyStack() {
+		assertEquals(Collections.emptyList(), forthEvaluator.evaluateProgram(Collections.emptyList()));
+	}
+
+	@Test
+	public void testNumbersAreJustPushedOntoTheStack() {
+		assertEquals(Arrays.asList(1, 2, 3, 4, 5),
+				forthEvaluator.evaluateProgram(Collections.singletonList("1 2 3 4 5")));
+	}
+
+	@Test
+	public void testTwoNumbersCanBeAdded() {
+		assertEquals(Collections.singletonList(3), forthEvaluator.evaluateProgram(Collections.singletonList("1 2 +")));
+	}
+
+	@Test
+	public void testErrorIfAdditionAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Addition requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("+"));
+	}
+
+	@Test
+	public void testErrorIfAdditionAttemptedWithOneNumberOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Addition requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("1 +"));
+	}
+
+	@Test
+	public void testTwoNumbersCanBeSubtracted() {
+		assertEquals(Collections.singletonList(-1), forthEvaluator.evaluateProgram(Collections.singletonList("3 4 -")));
+	}
+
+	@Test
+	public void testErrorIfSubtractionAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Subtraction requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("-"));
+	}
+
+	@Test
+	public void testErrorIfSubtractionAttemptedWithOneNumberOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Subtraction requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("1 -"));
+	}
+
+	@Test
+	public void testTwoNumbersCanBeMultiplied() {
+		assertEquals(Collections.singletonList(8), forthEvaluator.evaluateProgram(Collections.singletonList("2 4 *")));
+	}
+
+	@Test
+	public void testErrorIfMultiplicationAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Multiplication requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("*"));
+	}
+
+	@Test
+	public void testErrorIfMultiplicationAttemptedWithOneNumberOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Multiplication requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("1 *"));
+	}
+
+	@Test
+	public void testTwoNumbersCanBeDivided() {
+		assertEquals(Collections.singletonList(4), forthEvaluator.evaluateProgram(Collections.singletonList("12 3 /")));
+	}
+
+	@Test
+	public void testThatIntegerDivisionIsUsed() {
+		assertEquals(Collections.singletonList(2), forthEvaluator.evaluateProgram(Collections.singletonList("8 3 /")));
+	}
+
+	@Test
+	public void testErrorIfDividingByZero() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Division by 0 is not allowed");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("4 0 /"));
+	}
+
+	@Test
+	public void testErrorIfDivisionAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Division requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("/"));
+	}
+
+	@Test
+	public void testErrorIfDivisionAttemptedWithOneNumberOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Division requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("1 /"));
+	}
+
+	@Test
+	public void testCombinedAdditionAndSubtraction() {
+		assertEquals(Collections.singletonList(-1),
+				forthEvaluator.evaluateProgram(Collections.singletonList("1 2 + 4 -")));
+	}
+
+	@Test
+	public void testCombinedMultiplicationAndDivision() {
+		assertEquals(Collections.singletonList(2),
+				forthEvaluator.evaluateProgram(Collections.singletonList("2 4 * 3 /")));
+	}
+
+	@Test
+	public void testDupCopiesTheTopValueOnTheStack() {
+		assertEquals(Arrays.asList(1, 1), forthEvaluator.evaluateProgram(Collections.singletonList("1 DUP")));
+	}
+
+	@Test
+	public void testDupParsingIsCaseInsensitive() {
+		assertEquals(Arrays.asList(1, 2, 2), forthEvaluator.evaluateProgram(Collections.singletonList("1 2 Dup")));
+	}
+
+	@Test
+	public void testErrorIfDuplicatingAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Duplicating requires that the stack contain at least 1 value");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("dup"));
+	}
+
+	@Test
+	public void testDropRemovesTheTopValueOnTheStackIfItIsTheOnlyOne() {
+		assertEquals(Collections.emptyList(), forthEvaluator.evaluateProgram(Collections.singletonList("1 drop")));
+	}
+
+	@Test
+	public void testDropRemovesTheTopValueOnTheStackIfItIsNotTheOnlyOne() {
+		assertEquals(Collections.singletonList(1),
+				forthEvaluator.evaluateProgram(Collections.singletonList("1 2 drop")));
+	}
+
+	@Test
+	public void testErrorIfDroppingAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Dropping requires that the stack contain at least 1 value");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("drop"));
+	}
+
+	@Test
+	public void testSwapSwapsTheTopTwosValueOnTheStackIfTheyAreTheOnlyOnes() {
+		assertEquals(Arrays.asList(2, 1), forthEvaluator.evaluateProgram(Collections.singletonList("1 2 swap")));
+	}
+
+	@Test
+	public void testSwapSwapsTheTopTwosValueOnTheStackIfTheyAreNotTheOnlyOnes() {
+		assertEquals(Arrays.asList(1, 3, 2), forthEvaluator.evaluateProgram(Collections.singletonList("1 2 3 swap")));
+	}
+
+	@Test
+	public void testErrorIfSwappingAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Swapping requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("swap"));
+	}
+
+	@Test
+	public void testErrorIfSwappingAttemptedWithOneNumberOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Swapping requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("1 swap"));
+	}
+
+	@Test
+	public void testOverCopiesTheSecondElementIfThereAreOnlyTwo() {
+		assertEquals(Arrays.asList(1, 2, 1), forthEvaluator.evaluateProgram(Collections.singletonList("1 2 over")));
+	}
+
+	@Test
+	public void testOverCopiesTheSecondElementIfThereAreMoreThanTwo() {
+		assertEquals(Arrays.asList(1, 2, 3, 2),
+				forthEvaluator.evaluateProgram(Collections.singletonList("1 2 3 over")));
+	}
+
+	@Test
+	public void testErrorIfOveringAttemptedWithNothingOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Overing requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("over"));
+	}
+
+	@Test
+	public void testErrorIfOveringAttemptedWithOneNumberOnTheStack() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Overing requires that the stack contain at least 2 values");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("1 over"));
+	}
+
+	@Test
+	public void testUserDefinedOperatorsCanConsistOfBuiltInOperators() {
+		assertEquals(Arrays.asList(1, 1, 1),
+				forthEvaluator.evaluateProgram(Arrays.asList(": dup-twice dup dup ;", "1 dup-twice")));
+	}
+
+	@Test
+	public void testUserDefinedOperatorsAreEvaluatedInTheCorrectOrder() {
+		assertEquals(Arrays.asList(1, 2, 3),
+				forthEvaluator.evaluateProgram(Arrays.asList(": countup 1 2 3 ;", "countup")));
+	}
+
+	@Test
+	public void testCanRedefineAUserDefinedOperator() {
+		assertEquals(Arrays.asList(1, 1, 1),
+				forthEvaluator.evaluateProgram(Arrays.asList(": foo dup ;", ": foo dup dup ;", "1 foo")));
+	}
+
+	@Test
+	public void testCanOverrideBuiltInWordOperators() {
+		assertEquals(Arrays.asList(1, 1), forthEvaluator.evaluateProgram(Arrays.asList(": swap dup ;", "1 swap")));
+	}
+
+	@Test
+	public void testCanOverrideBuiltInArithmeticOperators() {
+		assertEquals(Collections.singletonList(12), forthEvaluator.evaluateProgram(Arrays.asList(": + * ;", "3 4 +")));
+	}
+
+	@Test
+	public void testCannotRedefineNumbers() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Cannot redefine numbers");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList(": 1 2 ;"));
+	}
+
+	@Test
+	public void testErrorIfEvaluatingAnUndefinedOperator() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("No definition available for operator \"foo\"");
+
+		forthEvaluator.evaluateProgram(Collections.singletonList("foo"));
+	}
+
+}
