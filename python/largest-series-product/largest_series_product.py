a5bd766839fccd8eef0bd47c3428372c88eb74ae
Kai Wang <charles.wangkai@gmail.com>
2017-11-14 15:40:15 +0800

    python/largest-series-product

---
+++ python/largest-series-product/largest_series_product.py
@@ -0,0 +1,8 @@
+import functools
+
+
+def largest_product(series, size):
+    if not all(map(str.isdigit, series)) or size > len(series) or size < 0:
+        raise ValueError
+    
+    return max([functools.reduce(lambda x, y: x * y, [ord(series[j]) - ord('0') for j in range(i, i + size)], 1) for i in range(len(series) - size + 1)])
