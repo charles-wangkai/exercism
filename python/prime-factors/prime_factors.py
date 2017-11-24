f7ccc9947355add0b7e0feeb4f0b5571c71e9304
Kai Wang <charles.wangkai@gmail.com>
2017-11-24 15:59:09 +0800

    python/prime-factors

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,22 @@
+def prime_factors(natural_number):
+    result = []
+    i = 2
+    while i * i <= natural_number:
+        if is_prime(i):
+            while natural_number % i == 0:
+                result.append(i)
+                natural_number //= i
+        
+        i += 1
+    if natural_number != 1:
+        result.append(natural_number)
+    return result
+
+
+def is_prime(n):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
