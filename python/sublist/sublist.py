90ea889d92ec28de808e0de671459a9db2774171
Kai Wang <charles.wangkai@gmail.com>
2017-12-15 12:56:00 +0800

    python/sublist

---
+++ python/sublist/sublist.py
@@ -0,0 +1,21 @@
+SUBLIST = object()
+SUPERLIST = object()
+EQUAL = object()
+UNEQUAL = object()
+
+
+def check_lists(first_list, second_list):
+    if contains(second_list, first_list):
+        if contains(first_list, second_list):
+            return EQUAL
+        else:
+            return SUBLIST
+    else:
+        if contains(first_list, second_list):
+            return SUPERLIST
+        else:
+            return UNEQUAL
+
+
+def contains(whole, part):
+    return any([whole[i:i + len(part)] == part for i in range(len(whole) - len(part) + 1)])
