9c55cc925928bf79c3e0353a4dc32dc08ac82eb1
Kai Wang <charles.wangkai@gmail.com>
2017-12-15 23:53:58 +0800

    python/nth-prime

---
+++ python/nth-prime/nth_prime.py
@@ -0,0 +1,24 @@
+def nth_prime(positive_number):
+    if positive_number <= 0:
+        raise ValueError
+
+    prime = 1
+    for _ in range(positive_number):
+        prime = next_prime(prime)
+    return prime
+
+
+def next_prime(x):
+    while True:
+        x += 1
+        if is_prime(x):
+            return x
+
+
+def is_prime(x):
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
+            return False
+        i += 1
+    return True
