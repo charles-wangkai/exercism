3dcef646893cf6fcd6e5309810b754642171d4c8
Kai Wang <charles.wangkai@gmail.com>
2017-12-15 16:16:51 +0800

    python/circular-buffer

---
+++ python/circular-buffer/circular_buffer.py
@@ -0,0 +1,45 @@
+class BufferFullException(Exception):
+    pass
+
+
+class BufferEmptyException(Exception):
+    pass
+
+
+class CircularBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = [None] * capacity
+        self.clear()
+
+    def read(self):
+        if self.is_empty():
+            raise BufferEmptyException
+
+        result = self.buffer[self.head_index]
+        self.head_index = (self.head_index + 1) % len(self.buffer)
+        self.length -= 1
+        return result
+
+    def write(self, element):
+        if self.is_full():
+            raise BufferFullException
+
+        self.buffer[self.tail_index] = element
+        self.tail_index = (self.tail_index + 1) % len(self.buffer)
+        self.length += 1
+
+    def overwrite(self, element):
+        if self.is_full():
+            self.read()
+        self.write(element)
+
+    def clear(self):
+        self.head_index = 0
+        self.tail_index = 0
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def is_full(self):
+        return self.length > 0 and self.head_index == self.tail_index
