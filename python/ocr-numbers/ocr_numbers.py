fb5c0f458a80fa2ac42f121a70a40b9cf122e96c
Kai Wang <charles.wangkai@gmail.com>
2017-12-17 00:30:43 +0800

    python/ocr-numbers

---
+++ python/ocr-numbers/ocr_numbers.py
@@ -0,0 +1,61 @@
+IMAGES = [
+    [' _ ',
+     '| |',
+     '|_|',
+     '   '],
+    ['   ',
+     '  |',
+     '  |',
+     '   '],
+    [' _ ',
+     ' _|',
+     '|_ ',
+     '   '],
+    [' _ ',
+     ' _|',
+     ' _|',
+     '   '],
+    ['   ',
+     '|_|',
+     '  |',
+     '   '],
+    [' _ ',
+     '|_ ',
+     ' _|',
+     '   '],
+    [' _ ',
+     '|_ ',
+     '|_|',
+     '   '],
+    [' _ ',
+     '  |',
+     '  |',
+     '   '],
+    [' _ ',
+     '|_|',
+     '|_|',
+     '   '],
+    [' _ ',
+     '|_|',
+     ' _|',
+     '   ']
+]
+
+
+def convert(input_grid):
+    row = len(input_grid)
+    if row % 4 != 0:
+        raise ValueError
+
+    col = len(input_grid[0])
+    if col % 3 != 0:
+        raise ValueError
+
+    return ','.join([''.join([recognize(input_grid, r, c) for c in range(0, col, 3)]) for r in range(0, row, 4)])
+
+
+def recognize(lines, r, c):
+    try:
+        return str(IMAGES.index([lines[r][c:c + 3] for r in range(r, r + 4)]))
+    except ValueError:
+        return '?'
