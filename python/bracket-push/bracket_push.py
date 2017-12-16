ac282f2ea504804a54035a34421a7b0361e93e00
Kai Wang <charles.wangkai@gmail.com>
2017-12-16 13:37:24 +0800

    python/bracket-push

---
+++ python/bracket-push/bracket_push.py
@@ -0,0 +1,10 @@
+def check_brackets(input_string):
+    stack = []
+    for ch in filter(lambda ch: ch in '()[]{}', input_string):
+        if ch in '([{':
+            stack.append(ch)
+        elif len(stack) == 0 or (ch == ')' and stack[-1] != '(') or (ch == ']' and stack[-1] != '[') or (ch == '}' and stack[-1] != '{'):
+            return False
+        else:
+            stack.pop()
+    return len(stack) == 0
