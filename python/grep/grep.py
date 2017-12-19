45b5def9673f75ad211b84e5f49e7c10605e678d
Kai Wang <charles.wangkai@gmail.com>
2017-12-19 20:53:31 +0800

    python/grep

---
+++ python/grep/grep.py
@@ -0,0 +1,29 @@
+def grep(pattern, files, flags=''):
+    result = ''
+    for file in files:
+        found = False
+        with open(file) as fp:
+            for line_num, line in enumerate(fp, 1):
+                line = line.rstrip('\n')
+                if is_satisfied(line, pattern, flags):
+                    if '-l' in flags:
+                        if not found:
+                            result += file + '\n'
+                    else:
+                        if len(files) > 1:
+                            result += file + ':'
+                        if '-n' in flags:
+                            result += str(line_num) + ':'
+                        result += line + '\n'
+                    found = True
+    return result
+
+
+def is_satisfied(line, pattern, flags):
+    match = len(line) == len(pattern) if '-x' in flags else True
+    if match:
+        if '-i' in flags:
+            line = line.lower()
+            pattern = pattern.lower()
+        match = pattern in line
+    return not match if '-v' in flags else match
