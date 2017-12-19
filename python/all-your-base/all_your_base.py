54763938e7067ca71a420fe755a9dee2af69fd11
Kai Wang <charles.wangkai@gmail.com>
2017-12-19 18:26:47 +0800

    python/all-your-base

---
+++ python/all-your-base/all_your_base.py
@@ -0,0 +1,16 @@
+def rebase(from_base, digits, to_base):
+    if from_base < 2 or to_base < 2:
+        raise ValueError('Bases must be at least 2.')
+
+    if any(map(lambda digit: not 0 <= digit < from_base, digits)):
+        raise ValueError('Digits are out of bound.')
+
+    number = 0
+    for digit in digits:
+        number = number * from_base + digit
+
+    target_digits = []
+    while number:
+        target_digits.append(number % to_base)
+        number //= to_base
+    return target_digits[::-1]
