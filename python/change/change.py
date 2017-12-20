738a21c3217e638ab8edbfccf5bacc92b9397a8b
Kai Wang <charles.wangkai@gmail.com>
2017-12-20 16:21:54 +0800

    python/change

---
+++ python/change/change.py
@@ -0,0 +1,26 @@
+import collections
+
+
+def find_minimum_coins(total_change, coins):
+    if total_change < 0:
+        return -1
+
+    value2coins = {0: []}
+    queue = collections.deque()
+    queue.append(0)
+    while queue:
+        value = queue.popleft()
+
+        if value == total_change:
+            return value2coins[value]
+
+        for coin in coins:
+            next_value = value + coin
+            if next_value <= total_change and next_value not in value2coins:
+                next_coins = value2coins[value][:]
+                next_coins.append(coin)
+
+                value2coins[next_value] = next_coins
+                queue.append(next_value)
+
+    return -1
