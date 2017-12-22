d813d3e6441f655635a9d7b832eff411d79aafea
Kai Wang <charles.wangkai@gmail.com>
2017-12-23 01:11:58 +0800

    haskell/bob

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,15 @@
+module Bob (responseFor) where
+
+import Data.Char
+import Data.String.Utils
+
+responseFor :: String -> String
+responseFor phrase
+    | all isSpace phrase = "Fine. Be that way!"
+    | isYell && isAsk = "Calm down, I know what I'm doing!"
+    | isYell = "Whoa, chill out!"
+    | isAsk = "Sure."
+    | otherwise = "Whatever."
+    where isYell = let content = filter isAlpha phrase
+                   in not (null content) && all isUpper content
+          isAsk = endswith "?" $ rstrip phrase
