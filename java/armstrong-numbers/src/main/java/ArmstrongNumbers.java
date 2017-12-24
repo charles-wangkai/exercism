22fe755a1a670c408cf74d31e1ce5ab7cda8acb7
Kai Wang <charles.wangkai@gmail.com>
2017-12-24 22:32:34 +0800

    java/armstrong-numbers

---
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,14 @@
+class ArmstrongNumbers {
+	boolean isArmstrongNumber(int numberToCheck) {
+		return numberToCheck == String.valueOf(numberToCheck).chars()
+				.map(ch -> pow(ch - '0', String.valueOf(numberToCheck).length())).sum();
+	}
+
+	int pow(int base, int exponent) {
+		int result = 1;
+		for (int i = 0; i < exponent; i++) {
+			result *= base;
+		}
+		return result;
+	}
+}
