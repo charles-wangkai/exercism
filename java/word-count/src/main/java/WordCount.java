a9e94e667f71ac2aca1675e8900984cb508f4cfd
Kai Wang <charles.wangkai@gmail.com>
2018-07-17 11:14:49 +0800

    java/word-count [For passing more test cases and refactoring]

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -3,12 +3,23 @@ import java.util.Map;
 
 public class WordCount {
 	Map<String, Integer> phrase(String s) {
-		Map<String, Integer> word2count = new HashMap<String, Integer>();
-		for (String word : s.split("\\W+")) {
-			System.out.println(word);
+		Map<String, Integer> word2count = new HashMap<>();
+		for (String word : s.split("([,.:!&@$%^]|\\s)+")) {
+			if (word.startsWith("'") && word.endsWith("'")) {
+				word = word.substring(1, word.length() - 1);
+			}
+
+			if (word.isEmpty()) {
+				continue;
+			}
+
 			word = word.toLowerCase();
-			word2count.put(word, word2count.getOrDefault(word, 0) + 1);
+			incrementWordCount(word2count, word);
 		}
 		return word2count;
+	}
+
+	void incrementWordCount(Map<String, Integer> word2count, String word) {
+		word2count.put(word, word2count.getOrDefault(word, 0) + 1);
 	}
 }
