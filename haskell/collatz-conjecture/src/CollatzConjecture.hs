dfab97ae6bdde649a3e246a6523e69e2c41edc47
Kai Wang <charles.wangkai@gmail.com>
2018-07-21 00:51:53 +0800

    haskell/collatz-conjecture [Change to use iterate and length functions]

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -3,8 +3,5 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz number
     | number <= 0 = Nothing
-    | otherwise = Just (f number 0)
-                  where f number step
-                            | number == 1 = step
-                            | even number = f (number `div` 2) (step + 1)
-                            | otherwise = f (3 * number + 1) (step + 1)
+    | otherwise = Just (toInteger $ length $ takeWhile (/= 1) $ iterate f number)
+    where f x = if even x then (x `div` 2) else (3 * x + 1)
