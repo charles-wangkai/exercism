22a1f619d4121d6c68597d0fc3b5d5ab50a26fa7
Kai Wang <charles.wangkai@gmail.com>
2018-07-23 11:20:11 +0800

    haskell/perfect-numbers [Refactor]

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -5,11 +5,10 @@ data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 classify :: Int -> Maybe Classification
 classify number = if number <= 0 then Nothing
                   else let aliquotSum = computeAliquotSum number
-                       in if aliquotSum == number then Just Perfect
-                          else if aliquotSum > number then Just Abundant
-                          else Just Deficient
+                       in case (aliquotSum `compare` number) of EQ -> Just Perfect
+                                                                GT -> Just Abundant
+                                                                LT -> Just Deficient
 
 computeAliquotSum :: Int -> Int
-computeAliquotSum n
-    | n == 1 = 0
-    | otherwise = 1 + (sum $ map (\i -> if i * i == n then i else i + n `div` i) $ filter (\i -> n `mod` i == 0) $ takeWhile (\i -> i * i <= n) [2..])
+computeAliquotSum 1 = 0
+computeAliquotSum n = 1 + (sum $ map (\i -> if i * i == n then i else i + n `div` i) $ filter (\i -> n `mod` i == 0) $ takeWhile (\i -> i * i <= n) [2..])
