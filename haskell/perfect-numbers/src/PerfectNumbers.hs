6feb6245f1a05b19d5264a987dbd3e581902c58d
Kai Wang <charles.wangkai@gmail.com>
2018-07-23 21:35:48 +0800

    haskell/perfect-numbers [Wrap lines]

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -3,12 +3,21 @@ module PerfectNumbers (classify, Classification(..)) where
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify number = if number <= 0 then Nothing
-                  else let aliquotSum = computeAliquotSum number
-                       in case (aliquotSum `compare` number) of EQ -> Just Perfect
-                                                                GT -> Just Abundant
-                                                                LT -> Just Deficient
+classify number =
+    if number <= 0
+        then Nothing
+        else
+            let aliquotSum = computeAliquotSum number
+            in case (aliquotSum `compare` number)
+                of EQ -> Just Perfect
+                   GT -> Just Abundant
+                   LT -> Just Deficient
 
 computeAliquotSum :: Int -> Int
 computeAliquotSum 1 = 0
-computeAliquotSum n = 1 + (sum $ map (\i -> if i * i == n then i else i + n `div` i) $ filter (\i -> n `mod` i == 0) $ takeWhile (\i -> i * i <= n) [2..])
+computeAliquotSum n = 1 + (sum
+                         $ map (\i -> if i * i == n
+                                        then i
+                                        else i + n `div` i)
+                         $ filter (\i -> n `mod` i == 0)
+                         $ takeWhile (\i -> i * i <= n) [2..])
