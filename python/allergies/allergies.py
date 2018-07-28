f26b9c4e9661b6d91d876f8ca35f5125da4affb0
Kai Wang <charles.wangkai@gmail.com>
2018-07-28 22:20:15 +0800

    python/allergies [Refactor]

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,12 +1,21 @@
 class Allergies(object):
-    ITEM2SCORE = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8, 'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
+    ITEM_TO_SCORE = {
+        'eggs': 1,
+        'peanuts': 2,
+        'shellfish': 4,
+        'strawberries': 8,
+        'tomatoes': 16,
+        'chocolate': 32,
+        'pollen': 64,
+        'cats': 128
+    }
 
     def __init__(self, score):
         self.score = score
 
     def is_allergic_to(self, item):
-        return self.score & self.ITEM2SCORE[item] != 0
+        return (self.score & self.ITEM_TO_SCORE.get(item, 0)) != 0
 
     @property
     def lst(self):
-        return list(filter(self.is_allergic_to, self.ITEM2SCORE.keys()))
+        return list(filter(self.is_allergic_to, self.ITEM_TO_SCORE))
