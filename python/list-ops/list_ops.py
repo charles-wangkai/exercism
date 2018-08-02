80b397b84d7d325ee8d05c7d874fe54d2e4c8708
Kai Wang <charles.wangkai@gmail.com>
2018-08-02 14:54:53 +0800

    python/list-ops [Implement without existing functions]

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,33 +1,60 @@
-import functools
-
-
 def append(xs, ys):
-    return xs + ys
+    xs_length = length(xs)
+    ys_length = length(ys)
+
+    result = [None] * (xs_length + ys_length)
+    i = 0
+    for x in xs:
+        result[i] = x
+        i += 1
+    for y in ys:
+        result[i] = y
+        i += 1
+    return result
 
 
 def concat(lists):
-    return sum(lists, [])
+    result = []
+    for one_list in lists:
+        result = append(result, one_list)
+    return result
 
 
 def filter_clone(function, xs):
-    return list(filter(function, xs))
+    return [x for x in xs if function(x)]
 
 
 def length(xs):
-    return len(xs)
+    result = 0
+    for _ in xs:
+        result += 1
+    return result
 
 
 def map_clone(function, xs):
-    return list(map(function, xs))
+    return [function(x) for x in xs]
 
 
 def foldl(function, xs, acc):
-    return functools.reduce(function, xs, acc)
+    result = acc
+    for x in xs:
+        result = function(result, x)
+    return result
 
 
 def foldr(function, xs, acc):
-    return functools.reduce(lambda result, x: function(x, result), xs[::-1], acc)
+    result = acc
+    for x in reverse(xs):
+        result = function(x, result)
+    return result
 
 
 def reverse(xs):
-    return xs[::-1]
+    xs_length = length(xs)
+
+    result = [None] * xs_length
+    i = 0
+    while i != xs_length:
+        result[i] = xs[xs_length - 1 - i]
+        i += 1
+    return result
