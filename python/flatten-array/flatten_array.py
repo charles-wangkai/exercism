9bd9b92f4e724012ff922c4d231cf1504b30f891
Kai Wang <charles.wangkai@gmail.com>
2018-08-29 02:37:31 +0800

    python/flatten-array [Refactor]

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,13 +1,13 @@
 def flatten(iterable):
-
-    def search(result, iterable):
-        for element in iterable:
-            if element is not None:
-                if isinstance(element, list) or isinstance(element, tuple):
-                    search(result, element)
-                else:
-                    result.append(element)
-    
     result = []
     search(result, iterable)
     return result
+
+
+def search(result, iterable):
+    for element in iterable:
+        if element is not None:
+            if isinstance(element, (list, tuple)):
+                search(result, element)
+            else:
+                result.append(element)
