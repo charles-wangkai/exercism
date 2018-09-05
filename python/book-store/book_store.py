14f65a756dd28838987974caf96aabb07ffb4764
Kai Wang <charles.wangkai@gmail.com>
2018-09-06 03:20:36 +0800

    python/book-store [Pass updated test cases]

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -2,26 +2,53 @@ import collections
 import functools
 import itertools
 
-PRICE = 8
-RATIOS = [None, 1, 0.95, 0.9, 0.8, 0.75]
+
+UNIT_PRICE = 800
+
+GROUP_SIZE_TO_DISCOUNT = {
+    1: 0,
+    2: 0.05,
+    3: 0.1,
+    4: 0.2,
+    5: 0.25
+}
 
 
 def calculate_total(books):
-    return search(frozenset(collections.Counter(books).items()))
+    return calculate(to_tuple_key(collections.Counter(books)))
 
 
+# Dynamic programming
 @functools.lru_cache(maxsize=None)
-def search(book2count):
-    if all(map(lambda bc: bc[1] == 0, book2count)):
+def calculate(book_and_count_pairs):
+    if not has_books(book_and_count_pairs):
         return 0
 
-    book2count_list = list(book2count)
-    return min([get_used_num(used) * PRICE * RATIOS[get_used_num(used)]
-                + search(frozenset([(book2count_list[i][0], book2count_list[i][1] - (1 if used[i] else 0))
-                                    for i in range(len(used))]))
-                for used in itertools.product([0, 1], repeat=len(book2count))
-                if get_used_num(used) > 0 and all(map(lambda i: not used[i] or book2count_list[i][1] > 0, range(len(used))))])
+    return min([compute_group_price(group) + calculate(build_remaining_book_and_count_pairs(book_and_count_pairs, group))
+                for group in generate_all_groups(len(book_and_count_pairs))
+                if is_valid_group(group, book_and_count_pairs)])
 
 
-def get_used_num(used):
-    return len([None for x in used if x])
+def generate_all_groups(book_num):
+    return itertools.product([0, 1], repeat=book_num)
+
+
+def is_valid_group(group, book_and_count_pairs):
+    return sum(group) > 0 and all(map(lambda i: book_and_count_pairs[i][1] >= group[i], range(len(group))))
+
+
+def compute_group_price(group):
+    return sum(group) * UNIT_PRICE * (1 - GROUP_SIZE_TO_DISCOUNT[sum(group)])
+
+
+def has_books(book_and_count_pairs):
+    return any(map(lambda book_and_count: book_and_count[1] > 0, book_and_count_pairs))
+
+
+def build_remaining_book_and_count_pairs(book_and_count_pairs, group):
+    return tuple((book_and_count_pairs[i][0], book_and_count_pairs[i][1] - group[i])
+                 for i in range(len(group)))
+
+
+def to_tuple_key(d):
+    return tuple(sorted(d.items()))
