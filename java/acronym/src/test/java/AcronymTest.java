fef38b06f8cdd0f800d92c6b6d9ab7e3502c355a
Kai Wang <charles.wangkai@gmail.com>
2018-10-15 16:07:27 +0800

    java/acronym [Migrate to v2]

--- java/acronym/src/test/java/AcronymTest.java
+++ java/acronym/src/test/java/AcronymTest.java
@@ -2,50 +2,40 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/*
- * version: 1.1.0
- */
 public class AcronymTest {
 
 	@Test
-	public void fromTitleCasedPhrases() {
-		final String phrase = "Portable Network Graphics";
-		final String expected = "PNG";
+	public void basic() {
+		String phrase = "Portable Network Graphics";
+		String expected = "PNG";
 		assertEquals(expected, new Acronym(phrase).get());
 	}
 
 	@Test
-	public void fromOtherTitleCasedPhrases() {
-		final String phrase = "Ruby on Rails";
-		final String expected = "ROR";
+	public void lowercaseWords() {
+		String phrase = "Ruby on Rails";
+		String expected = "ROR";
 		assertEquals(expected, new Acronym(phrase).get());
 	}
 
 	@Test
-	public void fromPhrasesWithPunctuation() {
-		final String phrase = "First In, First Out";
-		final String expected = "FIFO";
+	public void punctuation() {
+		String phrase = "First In, First Out";
+		String expected = "FIFO";
 		assertEquals(expected, new Acronym(phrase).get());
 	}
 
 	@Test
-	public void fromOtherPhrasesWithPunctuation() {
-		final String phrase = "PHP: Hypertext Preprocessor";
-		final String expected = "PHP";
+	public void NonAcronymAllCapsWord() {
+		String phrase = "GNU Image Manipulation Program";
+		String expected = "GIMP";
 		assertEquals(expected, new Acronym(phrase).get());
 	}
 
 	@Test
-	public void fromPhrasesWithNonAcronymAllCapsWord() {
-		final String phrase = "GNU Image Manipulation Program";
-		final String expected = "GIMP";
-		assertEquals(expected, new Acronym(phrase).get());
-	}
-
-	@Test
-	public void fromPhrasesWithPunctuationAndSentenceCasing() {
-		final String phrase = "Complementary metal-oxide semiconductor";
-		final String expected = "CMOS";
+	public void punctuationWithoutWhitespace() {
+		String phrase = "Complementary metal-oxide semiconductor";
+		String expected = "CMOS";
 		assertEquals(expected, new Acronym(phrase).get());
 	}
 
