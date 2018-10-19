15b658be3d2d2000077c344f61f8ee5e9c517e98
Kai Wang <charles.wangkai@gmail.com>
2018-10-19 13:47:19 +0800

    java/pascals-triangle [Migrate to v2]

--- java/pascals-triangle/src/test/java/PascalsTriangleGeneratorTest.java
+++ java/pascals-triangle/src/test/java/PascalsTriangleGeneratorTest.java
@@ -5,9 +5,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-/*
- * version: 1.0.0
- */
 public class PascalsTriangleGeneratorTest {
 
 	private PascalsTriangleGenerator pascalsTriangleGenerator;
@@ -56,9 +53,26 @@ public class PascalsTriangleGeneratorTest {
 	}
 
 	@Test
-	public void testValidatesNotNegativeRows() {
-		expectedException.expect(IllegalArgumentException.class);
-		pascalsTriangleGenerator.generateTriangle(-1);
+	public void testTriangleWithFiveRows() {
+		int[][] expectedOutput = new int[][] { { 1 }, { 1, 1 }, { 1, 2, 1 }, { 1, 3, 3, 1 }, { 1, 4, 6, 4, 1 } };
+
+		assertArrayEquals(expectedOutput, pascalsTriangleGenerator.generateTriangle(5));
 	}
 
+	@Test
+	public void testTriangleWithSixRows() {
+		int[][] expectedOutput = new int[][] { { 1 }, { 1, 1 }, { 1, 2, 1 }, { 1, 3, 3, 1 }, { 1, 4, 6, 4, 1 },
+				{ 1, 5, 10, 10, 5, 1 } };
+
+		assertArrayEquals(expectedOutput, pascalsTriangleGenerator.generateTriangle(6));
+	}
+
+	@Test
+	public void testTriangleWithTenRows() {
+		int[][] expectedOutput = new int[][] { { 1 }, { 1, 1 }, { 1, 2, 1 }, { 1, 3, 3, 1 }, { 1, 4, 6, 4, 1 },
+				{ 1, 5, 10, 10, 5, 1 }, { 1, 6, 15, 20, 15, 6, 1 }, { 1, 7, 21, 35, 35, 21, 7, 1 },
+				{ 1, 8, 28, 56, 70, 56, 28, 8, 1 }, { 1, 9, 36, 84, 126, 126, 84, 36, 9, 1 } };
+
+		assertArrayEquals(expectedOutput, pascalsTriangleGenerator.generateTriangle(10));
+	}
 }
