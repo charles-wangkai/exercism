d997e23c4c6bdc43b2d6aef71962b1bb18340bbe
Kai Wang <charles.wangkai@gmail.com>
2018-10-22 15:22:16 +0800

    java/binary-search-tree [Pass updated test cases]

--- java/binary-search-tree/src/main/java/BinarySearchTree.java
+++ java/binary-search-tree/src/main/java/BinarySearchTree.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class BST<T extends Comparable<? super T>> {
+public class BinarySearchTree<T extends Comparable<? super T>> {
 	static class Node<T> {
 		T data;
 		Node<T> left;
