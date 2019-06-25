5b18bfd3f0478cdea1d22cb10eb10acbd7519eac
Kai Wang <charles.wangkai@gmail.com>
2019-06-26 03:02:03 +0800

    python/scrabble-score [Refactor]

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,11 +1,19 @@
-LETTER2VALUE = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-                'D': 2, 'G': 2,
-                'B': 3, 'C': 3, 'M': 3, 'P': 3,
-                'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-                'K': 5,
-                'J': 8, 'X': 8,
-                'Q': 10, 'Z': 10}
+VALUE_TO_LETTERS = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z']
+}
+
+LETTER_TO_VALUE = {
+    letter: value
+    for value in VALUE_TO_LETTERS
+    for letter in VALUE_TO_LETTERS[value]
+}
 
 
 def score(word):
-    return sum(map(lambda ch: LETTER2VALUE[ch], word.upper()))
+    return sum(map(lambda letter: LETTER_TO_VALUE[letter], word.upper()))
