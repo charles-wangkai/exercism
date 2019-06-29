1f565567a0ece9a0166fa5289241f5c0ad3128b5
Kai Wang <charles.wangkai@gmail.com>
2019-06-29 08:53:13 +0800

    python/twelve-days [Pass updated test cases]

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,25 +1,44 @@
-SEQUENCES = [None, 'first', 'second', 'third', 'fourth', 'fifth',
-             'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
-GIFTS = [None, 'a Partridge in a Pear Tree', 'two Turtle Doves', 'three French Hens', 'four Calling Birds', 'five Gold Rings', 'six Geese-a-Laying',
-         'seven Swans-a-Swimming', 'eight Maids-a-Milking', 'nine Ladies Dancing', 'ten Lords-a-Leaping', 'eleven Pipers Piping', 'twelve Drummers Drumming']
+SEQUENCES = [
+    'first',
+    'second',
+    'third',
+    'fourth',
+    'fifth',
+    'sixth',
+    'seventh',
+    'eighth',
+    'ninth',
+    'tenth',
+    'eleventh',
+    'twelfth'
+]
+GIFTS = [
+    'a Partridge in a Pear Tree',
+    'two Turtle Doves',
+    'three French Hens',
+    'four Calling Birds',
+    'five Gold Rings',
+    'six Geese-a-Laying',
+    'seven Swans-a-Swimming',
+    'eight Maids-a-Milking',
+    'nine Ladies Dancing',
+    'ten Lords-a-Leaping',
+    'eleven Pipers Piping',
+    'twelve Drummers Drumming'
+]
 
 
 def verse(day_number):
-    result = 'On the {} day of Christmas my true love gave to me, '.format(
-        SEQUENCES[day_number])
+    result = f'On the {SEQUENCES[day_number - 1]} day of Christmas my true love gave to me: '
     for i in range(day_number, 0, -1):
         if i < day_number:
             result += ', '
         if day_number > 1 and i == 1:
             result += 'and '
-        result += GIFTS[i]
-    result += '.\n'
+        result += GIFTS[i - 1]
+    result += '.'
     return result
 
 
-def verses(start, end):
-    return ''.join(map(lambda n: verse(n) + '\n', range(start, end + 1)))
-
-
-def sing():
-    return verses(1, 12)
+def recite(start, end):
+    return [verse(n) for n in range(start, end + 1)]
