2d1bb52da5e436b50736ea8a57803358d888a777
Kai Wang <charles.wangkai@gmail.com>
2019-11-10 20:34:18 +0800

    java/rational-numbers [Upgrade]

--- java/rational-numbers/src/test/java/RationalTest.java
+++ java/rational-numbers/src/test/java/RationalTest.java
@@ -148,9 +148,23 @@ public class RationalTest {
 	}
 
 	@Test
+	public void testAbsoluteValueOfAPositiveRationalNumberWithNegativeNumeratorAndDenominator() {
+		Rational expected = new Rational(1, 2);
+		Rational actual = new Rational(-1, -2).abs();
+		assertEquals(expected, actual);
+	}
+
+	@Test
 	public void testAbsoluteValueOfANegativeRationalNumber() {
 		Rational expected = new Rational(1, 2);
 		Rational actual = new Rational(-1, 2).abs();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAbsoluteValueOfANegativeRationalNumberWithNegativeDenominator() {
+		Rational expected = new Rational(1, 2);
+		Rational actual = new Rational(1, -2).abs();
 		assertEquals(expected, actual);
 	}
 
