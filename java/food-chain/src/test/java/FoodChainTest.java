090ce425ff3bd7082553f8790aea5daf8abd88c0
Kai Wang <charles.wangkai@gmail.com>
2019-11-10 19:14:08 +0800

    java/food-chain [Upgrade]

--- java/food-chain/src/test/java/FoodChainTest.java
+++ java/food-chain/src/test/java/FoodChainTest.java
@@ -36,7 +36,7 @@ public class FoodChainTest {
 	public void bird() {
 		int verse = 3;
 		String expected = "I know an old lady who swallowed a bird.\n" + "How absurd to swallow a bird!\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.";
 
@@ -49,7 +49,7 @@ public class FoodChainTest {
 		int verse = 4;
 		String expected = "I know an old lady who swallowed a cat.\n" + "Imagine that, to swallow a cat!\n"
 				+ "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.";
 
@@ -62,7 +62,7 @@ public class FoodChainTest {
 		int verse = 5;
 		String expected = "I know an old lady who swallowed a dog.\n" + "What a hog, to swallow a dog!\n"
 				+ "She swallowed the dog to catch the cat.\n" + "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.";
 
@@ -76,7 +76,7 @@ public class FoodChainTest {
 		String expected = "I know an old lady who swallowed a goat.\n"
 				+ "Just opened her throat and swallowed a goat!\n" + "She swallowed the goat to catch the dog.\n"
 				+ "She swallowed the dog to catch the cat.\n" + "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.";
 
@@ -90,7 +90,7 @@ public class FoodChainTest {
 		String expected = "I know an old lady who swallowed a cow.\n" + "I don't know how she swallowed a cow!\n"
 				+ "She swallowed the cow to catch the goat.\n" + "She swallowed the goat to catch the dog.\n"
 				+ "She swallowed the dog to catch the cat.\n" + "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.";
 
@@ -117,7 +117,7 @@ public class FoodChainTest {
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a bird.\n" + "How absurd to swallow a bird!\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.";
 
@@ -135,29 +135,29 @@ public class FoodChainTest {
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a bird.\n" + "How absurd to swallow a bird!\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a cat.\n" + "Imagine that, to swallow a cat!\n"
 				+ "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a dog.\n" + "What a hog, to swallow a dog!\n"
 				+ "She swallowed the dog to catch the cat.\n" + "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a goat.\n" + "Just opened her throat and swallowed a goat!\n"
 				+ "She swallowed the goat to catch the dog.\n" + "She swallowed the dog to catch the cat.\n"
 				+ "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a cow.\n" + "I don't know how she swallowed a cow!\n"
 				+ "She swallowed the cow to catch the goat.\n" + "She swallowed the goat to catch the dog.\n"
 				+ "She swallowed the dog to catch the cat.\n" + "She swallowed the cat to catch the bird.\n"
-				+ "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+				+ "She swallowed the bird to catch the spider that wriggled and jiggled and " + "tickled inside her.\n"
 				+ "She swallowed the spider to catch the fly.\n"
 				+ "I don't know why she swallowed the fly. Perhaps she'll die.\n" + "\n"
 				+ "I know an old lady who swallowed a horse.\n" + "She's dead, of course!";
