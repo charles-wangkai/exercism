4fcf6ab890ca75c7a7dae6570936b2fe5ea61532
Kai Wang <charles.wangkai@gmail.com>
2019-11-10 16:04:45 +0800

    java/gigasecond [Pass updated test cases]

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -13,7 +13,7 @@ class Gigasecond {
 		this.birthDateTime = birthDateTime;
 	}
 
-	LocalDateTime getDate() {
+	LocalDateTime getDateTime() {
 		return birthDateTime.plusSeconds(1_000_000_000);
 	}
 
