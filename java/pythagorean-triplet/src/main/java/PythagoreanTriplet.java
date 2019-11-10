31c25e96b6628143ffab4f264afc8ad25dc82392
Kai Wang <charles.wangkai@gmail.com>
2019-11-10 20:59:08 +0800

    java/pythagorean-triplet [Pass updated test cases]

--- java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PythagoreanTriplet {
 	int a;
@@ -10,6 +11,17 @@ public class PythagoreanTriplet {
 		this.a = a;
 		this.b = b;
 		this.c = c;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(a, b, c);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		PythagoreanTriplet other = (PythagoreanTriplet) obj;
+		return a == other.a && b == other.b && c == other.c;
 	}
 
 	int calculateSum() {
@@ -30,14 +42,8 @@ public class PythagoreanTriplet {
 }
 
 class PythagoreanTripletBuilder {
-	int lower = 1;
 	int upper;
 	Integer sum;
-
-	PythagoreanTripletBuilder withFactorsGreaterThanOrEqualTo(int lower) {
-		this.lower = lower;
-		return this;
-	}
 
 	PythagoreanTripletBuilder withFactorsLessThanOrEqualTo(int upper) {
 		this.upper = upper;
@@ -51,16 +57,21 @@ class PythagoreanTripletBuilder {
 
 	List<PythagoreanTriplet> build() {
 		List<PythagoreanTriplet> result = new ArrayList<PythagoreanTriplet>();
-		for (int a = lower; a <= upper; a++) {
+		for (int a = 1; a <= upper; a++) {
 			for (int b = a + 1; b <= upper; b++) {
-				for (int c = b + 1; c <= upper; c++) {
-					PythagoreanTriplet pt = new PythagoreanTriplet(a, b, c);
-					if (pt.isPythagorean() && (sum == null || pt.calculateSum() == sum)) {
-						result.add(pt);
-					}
+				int c = (int) Math.round(Math.sqrt(a * a + b * b));
+
+				if (c > upper) {
+					break;
+				}
+
+				PythagoreanTriplet pt = new PythagoreanTriplet(a, b, c);
+				if (pt.isPythagorean() && (sum == null || pt.calculateSum() == sum)) {
+					result.add(pt);
 				}
 			}
 		}
+
 		return result;
 	}
 }
