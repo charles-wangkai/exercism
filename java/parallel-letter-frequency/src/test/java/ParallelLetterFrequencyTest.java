7e129e62ca93c69f0ef6b609f4f29229750aae01
Kai Wang <charles.wangkai@gmail.com>
2019-11-10 19:55:24 +0800

    java/parallel-letter-frequency [Upgrade]

--- java/parallel-letter-frequency/src/test/java/ParallelLetterFrequencyTest.java
+++ java/parallel-letter-frequency/src/test/java/ParallelLetterFrequencyTest.java
@@ -1,21 +1,15 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinWorkerThread;
 
 import org.junit.Test;
 
 public class ParallelLetterFrequencyTest {
 
 	// American national anthem
-	private String StarSpangledBanner = "O say can you see by the dawn's early light,\n"
+	private String starSpangledBanner = "O say can you see by the dawn's early light,\n"
 			+ "What so proudly we hailed at the twilight's last gleaming,\n"
 			+ "Whose broad stripes and bright stars through the perilous fight,\n"
 			+ "O'er the ramparts we watched, were so gallantly streaming?\n"
@@ -93,7 +87,7 @@ public class ParallelLetterFrequencyTest {
 
 	@Test
 	public void punctuationDoesntCount() {
-		ParallelLetterFrequency p = new ParallelLetterFrequency(StarSpangledBanner);
+		ParallelLetterFrequency p = new ParallelLetterFrequency(starSpangledBanner);
 
 		assertFalse(p.letterCounts().containsKey((int) ','));
 	}
@@ -103,88 +97,5 @@ public class ParallelLetterFrequencyTest {
 		ParallelLetterFrequency p = new ParallelLetterFrequency("Testing, 1, 2, 3");
 
 		assertFalse(p.letterCounts().containsKey((int) '1'));
-	}
-
-	@Test
-	public void multipleThreadsGetUsed() throws InterruptedException, ExecutionException {
-
-		class MyForkJoinWorkerThread extends ForkJoinWorkerThread {
-
-			boolean wasStarted = false;
-
-			public MyForkJoinWorkerThread(ForkJoinPool pool) {
-				super(pool);
-			}
-
-			@Override
-			public void start() {
-				super.start();
-				this.wasStarted = true;
-			}
-		}
-
-		List<MyForkJoinWorkerThread> allThreads = new ArrayList<>();
-
-		class MyForkJoinThreadFactory implements ForkJoinPool.ForkJoinWorkerThreadFactory {
-			public MyForkJoinThreadFactory() {
-				super();
-			}
-
-			@Override
-			public MyForkJoinWorkerThread newThread(ForkJoinPool pool) {
-				final MyForkJoinWorkerThread worker = new MyForkJoinWorkerThread(pool);
-				worker.setName("Thread number: " + worker.getPoolIndex());
-				allThreads.add(worker);
-				return worker;
-			}
-		}
-
-		MyForkJoinThreadFactory factory = new MyForkJoinThreadFactory();
-		ForkJoinPool multiThreadPool = new ForkJoinPool(4, factory, null, false);
-
-		String input = "abcdefghijklmnopqrstuvwxyz";
-		StringBuilder b = new StringBuilder();
-		for (int i = 1; i < 10000000; i++) {
-			b.append(input);
-		}
-
-		Map<Integer, Integer> expectedOutput = new HashMap<Integer, Integer>() {
-			{
-				put((int) 'a', 10000000);
-				put((int) 'b', 10000000);
-				put((int) 'c', 10000000);
-				put((int) 'd', 10000000);
-				put((int) 'e', 10000000);
-				put((int) 'f', 10000000);
-				put((int) 'g', 10000000);
-				put((int) 'h', 10000000);
-				put((int) 'i', 10000000);
-				put((int) 'j', 10000000);
-				put((int) 'k', 10000000);
-				put((int) 'l', 10000000);
-				put((int) 'm', 10000000);
-				put((int) 'n', 10000000);
-				put((int) 'o', 10000000);
-				put((int) 'p', 10000000);
-				put((int) 'q', 10000000);
-				put((int) 'r', 10000000);
-				put((int) 's', 10000000);
-				put((int) 't', 10000000);
-				put((int) 'u', 10000000);
-				put((int) 'v', 10000000);
-				put((int) 'w', 10000000);
-				put((int) 'x', 10000000);
-				put((int) 'y', 10000000);
-				put((int) 'z', 10000000);
-			}
-		};
-
-		Map<Integer, Integer> multiCounts = multiThreadPool
-				.submit(() -> (new ParallelLetterFrequency(b.toString()).letterCounts())).get();
-
-		boolean startedThreadCountEqualsPoolSize = allThreads.stream().filter(thread -> thread.wasStarted)
-				.count() == allThreads.size();
-
-		assertTrue(startedThreadCountEqualsPoolSize);
 	}
 }
