c426e22264518de158ed820422cd58f5cacf4528
Kai Wang <charles.wangkai@gmail.com>
2019-11-11 09:40:49 +0800

    python/isogram [Upgrade]

--- python/isogram/isogram_test.py
+++ python/isogram/isogram_test.py
@@ -2,41 +2,49 @@ import unittest
 
 from isogram import is_isogram
 
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.7.0
 
-# test cases adapted from `x-common//canonical-data.json` @ version: 1.1.0
 
-class TestIsogram(unittest.TestCase):
-
+class IsogramTest(unittest.TestCase):
     def test_empty_string(self):
-        self.assertTrue(is_isogram(""))
+        self.assertIs(is_isogram(""), True)
 
     def test_isogram_with_only_lower_case_characters(self):
-        self.assertTrue(is_isogram("isogram"))
+        self.assertIs(is_isogram("isogram"), True)
 
     def test_word_with_one_duplicated_character(self):
-        self.assertFalse(is_isogram("eleven"))
+        self.assertIs(is_isogram("eleven"), False)
+
+    def test_word_with_one_duplicated_character_from_the_end_of_the_alphabet(self):
+        self.assertIs(is_isogram("zzyzx"), False)
 
     def test_longest_reported_english_isogram(self):
-        self.assertTrue(is_isogram("subdermatoglyphic"))
+        self.assertIs(is_isogram("subdermatoglyphic"), True)
 
     def test_word_with_duplicated_character_in_mixed_case(self):
-        self.assertFalse(is_isogram("Alphabet"))
+        self.assertIs(is_isogram("Alphabet"), False)
+
+    def test_word_with_duplicated_character_in_mixed_case_lowercase_first(self):
+        self.assertIs(is_isogram("alphAbet"), False)
 
     def test_hypothetical_isogrammic_word_with_hyphen(self):
-        self.assertTrue(is_isogram("thumbscrew-japingly"))
+        self.assertIs(is_isogram("thumbscrew-japingly"), True)
 
-    def test_isogram_with_duplicated_non_letter_character(self):
-        self.assertTrue(is_isogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"))
+    def test_hypothetical_word_with_duplicated_character_following_hyphen(self):
+        self.assertIs(is_isogram("thumbscrew-jappingly"), False)
+
+    def test_isogram_with_duplicated_hyphen(self):
+        self.assertIs(is_isogram("six-year-old"), True)
 
     def test_made_up_name_that_is_an_isogram(self):
-        self.assertTrue(is_isogram("Emily Jung Schwartzkopf"))
+        self.assertIs(is_isogram("Emily Jung Schwartzkopf"), True)
 
     def test_duplicated_character_in_the_middle(self):
-        self.assertFalse(is_isogram("accentor"))
+        self.assertIs(is_isogram("accentor"), False)
 
-    def test_isogram_with_duplicated_letter_and_nonletter_character(self):
-        self.assertFalse(is_isogram("Aleph Bot Chap"))
+    def test_same_first_and_last_characters(self):
+        self.assertIs(is_isogram("angola"), False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
