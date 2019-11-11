3694d80c094da25aee10e39358402a5546f1835c
Kai Wang <charles.wangkai@gmail.com>
2019-11-11 19:35:08 +0800

    python/phone-number [Pass updated test cases]

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,19 +1,20 @@
 class Phone(object):
     def __init__(self, phone_number):
         if not all(map(lambda ch: ch.isdigit() or ch in ' ()-.+', phone_number)):
-            raise ValueError
+            raise ValueError('Invalid character!')
 
         digits = ''.join(filter(str.isdigit, phone_number))
         length = len(digits)
         if length not in [10, 11]:
-            raise ValueError
+            raise ValueError('Length should be 10 or 11!')
 
         if length == 11 and digits[0] != '1':
-            raise ValueError
+            raise ValueError(
+                'Phone number with length 11 should begin with 1!')
 
         self.number = digits[-10:]
         if not ('2' <= self.number[0] <= '9' and '2' <= self.number[3] <= '9'):
-            raise ValueError
+            raise ValueError('Invalid area code or exchange code!')
 
         self.area_code = self.number[:3]
 
