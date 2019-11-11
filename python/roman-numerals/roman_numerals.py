6a3a11116c4362468010376d7fca611147db98a5
Kai Wang <charles.wangkai@gmail.com>
2019-11-11 16:51:06 +0800

    python/roman-numerals [Pass updated test cases]

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,6 +1,20 @@
-def numeral(number):
-    VALUE_SYMBOLS = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
-    
+def roman(number):
+    VALUE_SYMBOLS = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+    ]
+
     roman = ''
     remain = number
     for value, symbol in VALUE_SYMBOLS:
