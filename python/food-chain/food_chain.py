d1d5574d97887d85e6b7734022ccae8902515274
Kai Wang <charles.wangkai@gmail.com>
2019-11-12 01:28:01 +0800

    python/food-chain [Pass updated test cases]

--- python/food-chain/food_chain.py
+++ python/food-chain/food_chain.py
@@ -1,12 +1,38 @@
-ANIMALS = [None, 'fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow', 'horse']
-COMMENTS = [None, None, 'It wriggled and jiggled and tickled inside her.', 'How absurd to swallow a bird!', 'Imagine that, to swallow a cat!',
-            'What a hog, to swallow a dog!', 'Just opened her throat and swallowed a goat!', 'I don\'t know how she swallowed a cow!']
-PARTS = [None, None, 'She swallowed the spider to catch the fly.', 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.',
-         'She swallowed the cat to catch the bird.', 'She swallowed the dog to catch the cat.', 'She swallowed the goat to catch the dog.', 'She swallowed the cow to catch the goat.']
+ANIMALS = [
+    None,
+    'fly',
+    'spider',
+    'bird',
+    'cat',
+    'dog',
+    'goat',
+    'cow',
+    'horse'
+]
+COMMENTS = [
+    None,
+    None,
+    'It wriggled and jiggled and tickled inside her.',
+    'How absurd to swallow a bird!',
+    'Imagine that, to swallow a cat!',
+    'What a hog, to swallow a dog!',
+    'Just opened her throat and swallowed a goat!',
+    'I don\'t know how she swallowed a cow!'
+]
+PARTS = [
+    None,
+    None,
+    'She swallowed the spider to catch the fly.',
+    'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.',
+    'She swallowed the cat to catch the bird.',
+    'She swallowed the dog to catch the cat.',
+    'She swallowed the goat to catch the dog.',
+    'She swallowed the cow to catch the goat.'
+]
 
 
-def chain():
-    return '\n\n'.join(map(verse, range(1, 9)))
+def recite(begin, end):
+    return '\n\n'.join(verse(i) for i in range(begin, end + 1)).splitlines()
 
 
 def verse(n):
