ba44013ddf72cbe593f98dc2d3967fbc5a349733
Kai Wang <charles.wangkai@gmail.com>
2019-11-11 16:28:17 +0800

    python/series [Pass updated test cases]

--- python/series/series.py
+++ python/series/series.py
@@ -1,7 +1,5 @@
 def slices(series, length):
-    digits = list(map(lambda ch: ord(ch) - ord('0'), series))
+    if length < 1 or length > len(series):
+        raise ValueError('Invalid length!')
 
-    if length < 1 or length > len(digits):
-        raise ValueError
-    
-    return list(map(lambda i: digits[i: i + length], range(0, len(digits) - length + 1)))
+    return [series[i: i + length] for i in range(0, len(series) - length + 1)]
