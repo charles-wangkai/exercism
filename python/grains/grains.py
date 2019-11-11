1bfff3a8d960b82564b98d74a9f99cc0719c1b3c
Kai Wang <charles.wangkai@gmail.com>
2019-11-11 16:55:24 +0800

    python/grains [Pass updated test cases]

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,13 +1,13 @@
-def on_square(integer_number):
+def square(integer_number):
     check_input(integer_number)
     return 1 << (integer_number - 1)
 
 
-def total_after(integer_number):
+def total(integer_number=64):
     check_input(integer_number)
-    return sum(map(on_square, range(1, integer_number + 1)))
+    return sum(map(square, range(1, integer_number + 1)))
 
 
 def check_input(integer_number):
     if not 1 <= integer_number <= 64:
-        raise ValueError
+        raise ValueError('Invalid input!')
