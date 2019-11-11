918f7c7ab022e2b64e03d6c920d138f7dc211a02
Kai Wang <charles.wangkai@gmail.com>
2019-11-11 14:46:35 +0800

    python/reverse-string [Upgrade]

--- python/reverse-string/reverse_string_test.py
+++ python/reverse-string/reverse_string_test.py
@@ -2,25 +2,28 @@ import unittest
 
 from reverse_string import reverse
 
-
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.1
-
-class ReverseStringTests(unittest.TestCase):
-    def empty_string(self):
-            self.assertEqual(reverse(''), '')
-
-    def a_word(self):
-            self.assertEqual(reverse('robot'), 'tobor')
-
-    def a_capitalized_word(self):
-            self.assertEqual(reverse('Ramen'), 'nemaR')
-
-    def a_sentence_with_punctuation(self):
-            self.assertEqual(reverse('I\'m hungry!'), '!yrgnuh m\'I')
-
-    def a_palindrome(self):
-            self.assertEqual(reverse('racecar'), 'racecar')
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
 
-if __name__ == '__main__':
+class ReverseStringTest(unittest.TestCase):
+    def test_an_empty_string(self):
+        self.assertEqual(reverse(""), "")
+
+    def test_a_word(self):
+        self.assertEqual(reverse("robot"), "tobor")
+
+    def test_a_capitalized_word(self):
+        self.assertEqual(reverse("Ramen"), "nemaR")
+
+    def test_a_sentence_with_punctuation(self):
+        self.assertEqual(reverse("I'm hungry!"), "!yrgnuh m'I")
+
+    def test_a_palindrome(self):
+        self.assertEqual(reverse("racecar"), "racecar")
+
+    def test_an_even_sized_word(self):
+        self.assertEqual(reverse("drawer"), "reward")
+
+
+if __name__ == "__main__":
     unittest.main()
