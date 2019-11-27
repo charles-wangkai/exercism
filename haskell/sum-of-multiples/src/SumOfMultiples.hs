c731e690c7de8648f7a174d13cbd2984430d4022
Kai Wang <charles.wangkai@gmail.com>
2019-11-27 19:57:37 +0800

    haskell/sum-of-multiples [Refactor]

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -5,8 +5,7 @@ module SumOfMultiples
 import qualified Data.Set as Set
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ Set.unions $ map multiplesOf factors
+sumOfMultiples factors limit = sum . Set.unions . map multiplesOf $ factors
   where
-    multiplesOf n
-      | n == 0 = Set.empty
-      | otherwise = Set.fromList [n,n * 2 .. limit - 1]
+    multiplesOf 0 = Set.singleton 0
+    multiplesOf n = Set.fromList [n,n * 2 .. limit - 1]
