dbaa85b0d0b4e313bccdc1ace47098b029fee076
Kai Wang <charles.wangkai@gmail.com>
2019-12-01 00:01:02 +0800

    cpp/space-age

---
+++ cpp/space-age/space_age.h
@@ -0,0 +1,26 @@
+#if !defined(SPACE_AGE_H)
+#define SPACE_AGE_H
+
+namespace space_age
+{
+class space_age
+{
+private:
+    long long secs;
+
+public:
+    space_age(long long secs);
+
+    long long seconds() const;
+    double on_earth() const;
+    double on_mercury() const;
+    double on_venus() const;
+    double on_mars() const;
+    double on_jupiter() const;
+    double on_saturn() const;
+    double on_uranus() const;
+    double on_neptune() const;
+};
+} // namespace space_age
+
+#endif // SPACE_AGE_H
