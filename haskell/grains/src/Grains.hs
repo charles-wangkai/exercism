9e0b19d09e1938e71e82fc80562d7784e9f87e9c
Kai Wang <charles.wangkai@gmail.com>
2019-12-04 20:51:05 +0800

    haskell/grains [Refactor]

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -3,12 +3,10 @@ module Grains
   , total
   ) where
 
-import Data.Bits
-
 square :: Integer -> Maybe Integer
 square n
-  | n >= 1 && n <= 64 = Just (shift 1 $ (fromIntegral n) - 1)
+  | n >= 1 && n <= 64 = Just (2 ^ (n - 1))
   | otherwise = Nothing
 
 total :: Integer
-total = sum [x | Just x <- map square [1 .. 64]]
+total = 2 ^ (64 :: Int) - 1
