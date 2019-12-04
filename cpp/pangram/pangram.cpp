af2e36c3e04ca70afc8a0f0ab593f2023fa36079
Kai Wang <charles.wangkai@gmail.com>
2019-12-04 16:28:13 +0800

    cpp/pangram

---
+++ cpp/pangram/pangram.cpp
@@ -0,0 +1,19 @@
+#include "pangram.h"
+#include <bitset>
+
+namespace pangram
+{
+bool is_pangram(const std::string &s)
+{
+    std::bitset<26> appeared;
+    for (const char &ch : s)
+    {
+        if (std::isalpha(ch))
+        {
+            appeared.set(std::tolower(ch) - 'a');
+        }
+    }
+
+    return appeared.all();
+}
+} // namespace pangram
---
+++ cpp/pangram/pangram.h
@@ -0,0 +1,11 @@
+#if !defined(PANGRAM_H)
+#define PANGRAM_H
+
+#include <string>
+
+namespace pangram
+{
+bool is_pangram(const std::string &s);
+} // namespace pangram
+
+#endif // PANGRAM_H
