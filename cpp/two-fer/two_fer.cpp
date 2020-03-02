5753678ec02ee539c1d0878d8101793f789f1a91
Kai Wang <charles.wangkai@gmail.com>
2020-03-02 11:18:14 +0800

    cpp/two-fer

---
+++ cpp/two-fer/two_fer.cpp
@@ -0,0 +1,9 @@
+#include "two_fer.h"
+
+namespace two_fer
+{
+std::string two_fer(const std::string &name)
+{
+    return "One for " + name + ", one for me.";
+}
+} // namespace two_fer
---
+++ cpp/two-fer/two_fer.h
@@ -0,0 +1,11 @@
+#if !defined(TWO_FER_H)
+#define TWO_FER_H
+
+#include <string>
+
+namespace two_fer
+{
+std::string two_fer(const std::string &name = "you");
+} // namespace two_fer
+
+#endif //TWO_FER_H
