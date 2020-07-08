be9b040a176071615a442fd88c02c4f3a8af36d2
Kai Wang <charles.wangkai@gmail.com>
2020-07-08 15:00:41 +0800

    csharp/space-age

---
+++ csharp/space-age/SpaceAge.cs
@@ -0,0 +1,51 @@
+using System;
+
+public class SpaceAge
+{
+    double seconds;
+
+    public SpaceAge(int seconds)
+    {
+        this.seconds = seconds;
+    }
+
+    public double OnEarth()
+    {
+        return seconds / 31557600;
+    }
+
+    public double OnMercury()
+    {
+        return OnEarth() / 0.2408467;
+    }
+
+    public double OnVenus()
+    {
+        return OnEarth() / 0.61519726;
+    }
+
+    public double OnMars()
+    {
+        return OnEarth() / 1.8808158;
+    }
+
+    public double OnJupiter()
+    {
+        return OnEarth() / 11.862615;
+    }
+
+    public double OnSaturn()
+    {
+        return OnEarth() / 29.447498;
+    }
+
+    public double OnUranus()
+    {
+        return OnEarth() / 84.016846;
+    }
+
+    public double OnNeptune()
+    {
+        return OnEarth() / 164.79132;
+    }
+}
