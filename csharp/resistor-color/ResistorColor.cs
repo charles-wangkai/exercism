e8d55f75106e7fe24e0cc61686d79b980100f316
Kai Wang <charles.wangkai@gmail.com>
2020-07-08 13:43:37 +0800

    csharp/resistor-color

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    static readonly string[] COLORS = {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+
+    public static int ColorCode(string color)
+    {
+        return Array.IndexOf(COLORS, color);
+    }
+
+    public static string[] Colors()
+    {
+        return COLORS;
+    }
+}
