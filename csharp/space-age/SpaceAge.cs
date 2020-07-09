3a134ca3303aa46a978bbc80226d3cd1d86c40e9
Kai Wang <charles.wangkai@gmail.com>
2020-07-10 00:40:15 +0800

    csharp/space-age [Simplify]

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,50 +2,23 @@ using System;
 
 public class SpaceAge
 {
-    double seconds;
+    double _seconds;
 
-    public SpaceAge(int seconds)
-    {
-        this.seconds = seconds;
-    }
+    public SpaceAge(int seconds) => _seconds = seconds;
 
-    public double OnEarth()
-    {
-        return seconds / 31557600;
-    }
+    public double OnEarth() => _seconds / 31557600;
 
-    public double OnMercury()
-    {
-        return OnEarth() / 0.2408467;
-    }
+    public double OnMercury() => OnEarth() / 0.2408467;
 
-    public double OnVenus()
-    {
-        return OnEarth() / 0.61519726;
-    }
+    public double OnVenus() => OnEarth() / 0.61519726;
 
-    public double OnMars()
-    {
-        return OnEarth() / 1.8808158;
-    }
+    public double OnMars() => OnEarth() / 1.8808158;
 
-    public double OnJupiter()
-    {
-        return OnEarth() / 11.862615;
-    }
+    public double OnJupiter() => OnEarth() / 11.862615;
 
-    public double OnSaturn()
-    {
-        return OnEarth() / 29.447498;
-    }
+    public double OnSaturn() => OnEarth() / 29.447498;
 
-    public double OnUranus()
-    {
-        return OnEarth() / 84.016846;
-    }
+    public double OnUranus() => OnEarth() / 84.016846;
 
-    public double OnNeptune()
-    {
-        return OnEarth() / 164.79132;
-    }
+    public double OnNeptune() => OnEarth() / 164.79132;
 }
