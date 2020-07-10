78d7a9c9a55c42be512c1582303a1da383f034f5
Kai Wang <charles.wangkai@gmail.com>
2020-07-10 10:14:29 +0800

    csharp/high-scores [Simplify]

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -6,28 +6,13 @@ public class HighScores
 {
     List<int> _scores;
 
-    public HighScores(List<int> scores)
-    {
-        _scores = scores;
-    }
+    public HighScores(List<int> scores) => _scores = scores;
 
-    public List<int> Scores()
-    {
-        return _scores;
-    }
+    public List<int> Scores() => _scores;
 
-    public int Latest()
-    {
-        return _scores[_scores.Count - 1];
-    }
+    public int Latest() => _scores.Last();
 
-    public int PersonalBest()
-    {
-        return _scores.Max();
-    }
+    public int PersonalBest() => _scores.Max();
 
-    public List<int> PersonalTopThree()
-    {
-        return _scores.OrderByDescending(score => score).Take(3).ToList();
-    }
+    public List<int> PersonalTopThree() => _scores.OrderByDescending(score => score).Take(3).ToList();
 }
