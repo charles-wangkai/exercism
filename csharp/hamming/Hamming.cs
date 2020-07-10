8b806805e74da9347aa112137fa4c0a5c96be43d
Kai Wang <charles.wangkai@gmail.com>
2020-07-10 10:48:27 +0800

    csharp/hamming

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+using System;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        return Enumerable.Range(0, firstStrand.Length).Where(i => firstStrand[i] != secondStrand[i]).Count();
+    }
+}
