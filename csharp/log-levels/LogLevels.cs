fb48e18abb3b174a294ed520611ecc9869df576b
Kai Wang <charles.wangkai@gmail.com>
2023-02-25 23:16:14 +0800

    csharp/log-levels

---
+++ csharp/log-levels/LogLevels.cs
@@ -0,0 +1,19 @@
+using System;
+
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        return logLine.Substring(logLine.IndexOf(':') + 1).Trim();
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        return logLine.Substring(1, logLine.IndexOf(':') - 2).ToLower();
+    }
+
+    public static string Reformat(string logLine)
+    {
+        return $"{Message(logLine)} ({LogLevel(logLine)})";
+    }
+}
