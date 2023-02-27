26a97288a4846d0883719dde3b8dd4a956f2bbe3
Kai Wang <charles.wangkai@gmail.com>
2023-02-27 21:29:38 +0800

    csharp/tim-from-marketing

---
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        StringBuilder result = new StringBuilder();
+        if (id != null)
+        {
+            result.Append($"[{id}] - ");
+        }
+        result.Append(name);
+        result.Append(" - ");
+        result.Append((department == null) ? "OWNER" : department.ToUpper());
+
+        return result.ToString();
+    }
+}
