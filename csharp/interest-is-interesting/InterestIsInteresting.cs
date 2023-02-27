d7791fa55bb696c0c6de9965f3389b3108c941a3
Kai Wang <charles.wangkai@gmail.com>
2023-02-27 23:32:54 +0800

    csharp/interest-is-interesting

---
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -0,0 +1,44 @@
+using System;
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        if (balance < 0)
+        {
+            return 3.213f;
+        }
+        if (balance < 1000)
+        {
+            return 0.5f;
+        }
+        if (balance < 5000)
+        {
+            return 1.621f;
+        }
+
+        return 2.475f;
+    }
+
+    public static decimal Interest(decimal balance)
+    {
+        return balance * (decimal)InterestRate(balance) / 100;
+    }
+
+    public static decimal AnnualBalanceUpdate(decimal balance)
+    {
+        return balance + Interest(balance);
+    }
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        int result = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            ++result;
+        }
+
+        return result;
+    }
+}
