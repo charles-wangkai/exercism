fc916940fa86d52c8d9fb75e71709d0b1c0912ff
Kai Wang <charles.wangkai@gmail.com>
2023-03-03 23:17:23 +0800

    csharp/wizards-and-warriors

---
+++ csharp/wizards-and-warriors/WizardsAndWarriors.cs
@@ -0,0 +1,42 @@
+using System;
+
+abstract class Character
+{
+    string characterType;
+
+    protected Character(string characterType)
+    {
+        this.characterType = characterType;
+    }
+
+    public abstract int DamagePoints(Character target);
+
+    public virtual bool Vulnerable() => false;
+
+    public override string ToString() => $"Character is a {characterType}";
+}
+
+class Warrior : Character
+{
+    public Warrior()
+        : base("Warrior") { }
+
+    public override int DamagePoints(Character target) => target.Vulnerable() ? 10 : 6;
+}
+
+class Wizard : Character
+{
+    bool spellPrepared;
+
+    public Wizard()
+        : base("Wizard") { }
+
+    public override int DamagePoints(Character target) => spellPrepared ? 12 : 3;
+
+    public override bool Vulnerable() => !spellPrepared;
+
+    public void PrepareSpell()
+    {
+        spellPrepared = true;
+    }
+}
