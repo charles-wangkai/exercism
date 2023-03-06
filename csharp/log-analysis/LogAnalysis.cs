76d3130fb15b790a388de80cc2a57fdbf77aedf7
Kai Wang <charles.wangkai@gmail.com>
2023-03-06 22:45:21 +0800

    csharp/log-analysis

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -2,26 +2,16 @@ using System;
 
 public static class LogAnalysis
 {
-    public static string SubstringAfter(this string log, string delimiter)
-    {
-        return log.Substring(log.IndexOf(delimiter) + delimiter.Length);
-    }
+    public static string SubstringAfter(this string log, string delimiter) =>
+        log.Substring(log.IndexOf(delimiter) + delimiter.Length);
 
-    public static string SubstringBetween(this string log, string delimiter1, string delimiter2)
-    {
-        return log.Substring(
+    public static string SubstringBetween(this string log, string delimiter1, string delimiter2) =>
+        log.Substring(
             log.IndexOf(delimiter1) + delimiter1.Length,
             log.IndexOf(delimiter2) - log.IndexOf(delimiter1) - delimiter1.Length
         );
-    }
 
-    public static string Message(this string log)
-    {
-        return log.Substring(log.IndexOf(' ') + 1);
-    }
+    public static string Message(this string log) => log.Substring(log.IndexOf(' ') + 1);
 
-    public static string LogLevel(this string log)
-    {
-        return log.Substring(1, log.IndexOf(']') - 1);
-    }
+    public static string LogLevel(this string log) => log.Substring(1, log.IndexOf(']') - 1);
 }
