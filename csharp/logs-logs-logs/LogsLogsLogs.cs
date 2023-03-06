9ef45cc01ae689763a5bbadc1ca529a9276f0652
Kai Wang <charles.wangkai@gmail.com>
2023-03-07 07:36:15 +0800

    csharp/logs-logs-logs

---
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42
+}
+
+static class LogLine
+{
+    static readonly IDictionary<string, LogLevel> STR_TO_LOG_LEVEL = new Dictionary<
+        string,
+        LogLevel
+    >
+    {
+        ["TRC"] = LogLevel.Trace,
+        ["DBG"] = LogLevel.Debug,
+        ["INF"] = LogLevel.Info,
+        ["WRN"] = LogLevel.Warning,
+        ["ERR"] = LogLevel.Error,
+        ["FTL"] = LogLevel.Fatal
+    };
+
+    public static LogLevel ParseLogLevel(string logLine)
+    {
+        return STR_TO_LOG_LEVEL.TryGetValue(logLine.Substring(1, 3), out LogLevel logLevel)
+            ? logLevel
+            : LogLevel.Unknown;
+    }
+
+    public static string OutputForShortLog(LogLevel logLevel, string message)
+    {
+        return $"{(int)logLevel}:{message}";
+    }
+}
