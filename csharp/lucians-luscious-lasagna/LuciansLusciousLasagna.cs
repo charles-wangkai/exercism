9bd8e856e98e0c43e55bc9c25e72dfbeabffd100
Kai Wang <charles.wangkai@gmail.com>
2023-03-06 21:41:47 +0800

    csharp/lucians-luscious-lasagna

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,22 +1,12 @@
 class Lasagna
 {
-    public int ExpectedMinutesInOven()
-    {
-        return 40;
-    }
+    public int ExpectedMinutesInOven() => 40;
 
-    public int RemainingMinutesInOven(int alreadyMinutes)
-    {
-        return ExpectedMinutesInOven() - alreadyMinutes;
-    }
+    public int RemainingMinutesInOven(int alreadyMinutes) =>
+        ExpectedMinutesInOven() - alreadyMinutes;
 
-    public int PreparationTimeInMinutes(int layerNum)
-    {
-        return layerNum * 2;
-    }
+    public int PreparationTimeInMinutes(int layerNum) => layerNum * 2;
 
-    public int ElapsedTimeInMinutes(int layerNum, int alreadyMinutes)
-    {
-        return PreparationTimeInMinutes(layerNum) + alreadyMinutes;
-    }
+    public int ElapsedTimeInMinutes(int layerNum, int alreadyMinutes) =>
+        PreparationTimeInMinutes(layerNum) + alreadyMinutes;
 }
