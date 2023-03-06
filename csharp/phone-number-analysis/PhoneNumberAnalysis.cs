f92cfd55bb746e8680cc24d34b3e195ba3cc21cc
Kai Wang <charles.wangkai@gmail.com>
2023-03-06 22:57:25 +0800

    csharp/phone-number-analysis

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,17 +2,13 @@ using System;
 
 public static class PhoneNumber
 {
-    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
-    {
-        return (
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber) =>
+        (
             phoneNumber.Substring(0, 3).Equals("212"),
             phoneNumber.Substring(4, 3).Equals("555"),
             phoneNumber.Substring(8)
         );
-    }
 
-    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        return phoneNumberInfo.IsFake;
-    }
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) =>
+        phoneNumberInfo.IsFake;
 }
