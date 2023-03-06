b84cccf41f0db40c767c6bd4e5caf6dbe7b7d683
Kai Wang <charles.wangkai@gmail.com>
2023-03-07 00:03:31 +0800

    csharp/booking-up-for-beauty

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -2,28 +2,17 @@ using System;
 
 static class Appointment
 {
-    public static DateTime Schedule(string appointmentDateDescription)
-    {
-        return DateTime.Parse(appointmentDateDescription);
-    }
+    public static DateTime Schedule(string appointmentDateDescription) =>
+        DateTime.Parse(appointmentDateDescription);
 
-    public static bool HasPassed(DateTime appointmentDate)
-    {
-        return appointmentDate.CompareTo(DateTime.Now) < 0;
-    }
+    public static bool HasPassed(DateTime appointmentDate) =>
+        appointmentDate.CompareTo(DateTime.Now) < 0;
 
-    public static bool IsAfternoonAppointment(DateTime appointmentDate)
-    {
-        return appointmentDate.Hour >= 12 && appointmentDate.Hour <= 17;
-    }
+    public static bool IsAfternoonAppointment(DateTime appointmentDate) =>
+        appointmentDate.Hour >= 12 && appointmentDate.Hour <= 17;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        return $"You have an appointment on {appointmentDate}.";
-    }
+    public static string Description(DateTime appointmentDate) =>
+        $"You have an appointment on {appointmentDate}.";
 
-    public static DateTime AnniversaryDate()
-    {
-        return new DateTime(DateTime.Now.Year, 9, 15);
-    }
+    public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15);
 }
