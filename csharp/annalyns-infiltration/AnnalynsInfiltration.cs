d53947b5c7a65403f0308be782e333c2e4fa5485
Kai Wang <charles.wangkai@gmail.com>
2023-03-06 21:45:43 +0800

    csharp/annalyns-infiltration

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,29 +2,20 @@ using System;
 
 static class QuestLogic
 {
-    public static bool CanFastAttack(bool knightIsAwake)
-    {
-        return !knightIsAwake;
-    }
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
 
-    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
-    {
-        return knightIsAwake || archerIsAwake || prisonerIsAwake;
-    }
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
+        knightIsAwake || archerIsAwake || prisonerIsAwake;
 
-    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
-    {
-        return !archerIsAwake && prisonerIsAwake;
-    }
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
+        !archerIsAwake && prisonerIsAwake;
 
     public static bool CanFreePrisoner(
         bool knightIsAwake,
         bool archerIsAwake,
         bool prisonerIsAwake,
         bool petDogIsPresent
-    )
-    {
-        return (petDogIsPresent && !archerIsAwake)
-            || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
-    }
+    ) =>
+        (petDogIsPresent && !archerIsAwake)
+        || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
 }
