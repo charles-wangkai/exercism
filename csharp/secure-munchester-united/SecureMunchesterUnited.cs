bb7d46f53744863fbf95b965df91011845463ffa
Kai Wang <charles.wangkai@gmail.com>
2023-03-07 17:30:54 +0800

    csharp/secure-munchester-united

---
+++ csharp/secure-munchester-united/SecureMunchesterUnited.cs
@@ -0,0 +1,67 @@
+using System;
+
+public class SecurityPassMaker
+{
+    public string GetDisplayName(TeamSupport support) =>
+        (support is Staff)
+            ? (
+                support.Title
+                + ((support.GetType() == typeof(Security)) ? " Priority Personnel" : "")
+            )
+            : "Too Important for a Security Pass";
+}
+
+public interface TeamSupport
+{
+    string Title { get; }
+}
+
+public abstract class Staff : TeamSupport
+{
+    public abstract string Title { get; }
+}
+
+public class Manager : TeamSupport
+{
+    public string Title { get; } = "The Manager";
+}
+
+public class Chairman : TeamSupport
+{
+    public string Title { get; } = "The Chairman";
+}
+
+public class Physio : Staff
+{
+    public override string Title { get; } = "The Physio";
+}
+
+public class OffensiveCoach : Staff
+{
+    public override string Title { get; } = "Offensive Coach";
+}
+
+public class GoalKeepingCoach : Staff
+{
+    public override string Title { get; } = "Goal Keeping Coach";
+}
+
+public class Security : Staff
+{
+    public override string Title { get; } = "Security Team Member";
+}
+
+public class SecurityJunior : Security
+{
+    public override string Title { get; } = "Security Junior";
+}
+
+public class SecurityIntern : Security
+{
+    public override string Title { get; } = "Security Intern";
+}
+
+public class PoliceLiaison : Security
+{
+    public override string Title { get; } = "Police Liaison Officer";
+}
