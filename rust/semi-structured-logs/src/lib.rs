3b5ba1639e21868bc652fe51b72e8cdf6562e3ac
Kai Wang <charles.wangkai@gmail.com>
2023-03-17 00:38:51 +0800

    rust/semi-structured-logs

--- rust/semi-structured-logs/src/lib.rs
+++ rust/semi-structured-logs/src/lib.rs
@@ -1,7 +1,3 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 /// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
