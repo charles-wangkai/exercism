ab9559ea30c5f21654751be6387c87023880a8f0
Kai Wang <charles.wangkai@gmail.com>
2023-03-18 21:51:29 +0800

    rust/clock

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,28 @@
+use std::fmt;
+
+#[derive(PartialEq, Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let total_minutes = 60 * hours + minutes;
+
+        Self {
+            hours: total_minutes.div_euclid(60).rem_euclid(24),
+            minutes: total_minutes.rem_euclid(60),
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Self::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
