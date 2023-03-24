9d5f53dcc8acc3bc90f96c78fc394995e9dbc7f2
Kai Wang <charles.wangkai@gmail.com>
2023-03-24 23:06:08 +0800

    rust/high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,28 @@
+use itertools::Itertools;
+
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        self.scores.iter().sorted().rev().take(3).copied().collect()
+    }
+}
