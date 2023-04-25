5a7c90c8b8e4768ab4e15b101fc037b841313781
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 13:50:31 +0800

    java/luhn

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -1,28 +1,21 @@
+import java.util.stream.IntStream;
+
 class LuhnValidator {
+  boolean isValid(String candidate) {
+    candidate = candidate.replace(" ", "");
 
-	boolean isValid(String candidate) {
-		candidate = candidate.replaceAll(" ", "");
-		if (candidate.length() <= 1 || !candidate.chars().allMatch(Character::isDigit)) {
-			return false;
-		}
+    String candidate_ = candidate;
+    return candidate.length() >= 2
+        && candidate.chars().allMatch(Character::isDigit)
+        && IntStream.range(0, candidate.length())
+                    .map(
+                        i -> {
+                          int digit = candidate_.charAt(candidate_.length() - 1 - i) - '0';
 
-		int sum = 0;
-		boolean needDouble = false;
-		for (int i = candidate.length() - 1; i >= 0; i--) {
-			int digit = candidate.charAt(i) - '0';
-			sum += needDouble ? doDouble(digit) : digit;
-
-			needDouble = !needDouble;
-		}
-		return sum % 10 == 0;
-	}
-
-	int doDouble(int digit) {
-		int result = digit * 2;
-		if (result > 9) {
-			result -= 9;
-		}
-		return result;
-	}
-
+                          return (i % 2 == 0) ? digit : ((digit * 2 - 1) % 9 + 1);
+                        })
+                    .sum()
+                % 10
+            == 0;
+  }
 }
