e3309a9adf4b8abdd2976bcf7a8f392d60152930
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 18:46:57 +0800

    java/rotational-cipher

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,24 +1,29 @@
+import java.util.stream.Collectors;
+
 public class RotationalCipher {
-	private static final int ALPHABET_SIZE = 26;
+  private static final int ALPHABET_SIZE = 26;
 
-	private final int key;
+  private int key;
 
-	public RotationalCipher(int key) {
-		this.key = key;
-	}
+  public RotationalCipher(int key) {
+    this.key = key;
+  }
 
-	public String rotate(String s) {
-		return s.chars().mapToObj(this::rotate)
-				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
-	}
+  public String rotate(String s) {
+    return s.chars()
+        .mapToObj(c -> rotate((char) c))
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
 
-	private char rotate(int ch) {
-		if (Character.isLowerCase(ch)) {
-			return (char) ((ch - 'a' + key) % ALPHABET_SIZE + 'a');
-		} else if (Character.isUpperCase(ch)) {
-			return (char) ((ch - 'A' + key) % ALPHABET_SIZE + 'A');
-		} else {
-			return (char) ch;
-		}
-	}
+  private char rotate(char c) {
+    if (Character.isLowerCase(c)) {
+      return (char) ((c - 'a' + key) % ALPHABET_SIZE + 'a');
+    }
+    if (Character.isUpperCase(c)) {
+      return (char) ((c - 'A' + key) % ALPHABET_SIZE + 'A');
+    }
+
+    return (char) c;
+  }
 }
