51efd3ff4eb1c948bcae5d94fc8066d7c73ff54a
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 11:57:05 +0800

    java/beer-song

--- java/beer-song/src/main/java/BeerSong.java
+++ java/beer-song/src/main/java/BeerSong.java
@@ -2,27 +2,39 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BeerSong {
-	String verse(int verse) {
-		if (verse == 0) {
-			return "No more bottles of beer on the wall, no more bottles of beer.\n"
-					+ "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
-		} else if (verse == 1) {
-			return "1 bottle of beer on the wall, 1 bottle of beer.\n"
-					+ "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
-		} else {
-			return String.format(
-					"%d bottles of beer on the wall, %d bottles of beer.\n"
-							+ "Take one down and pass it around, %d bottle%s of beer on the wall.\n\n",
-					verse, verse, verse - 1, verse == 2 ? "" : "s");
-		}
-	}
+  String verse(int verse) {
+    if (verse == 0) {
+      return """
+				No more bottles of beer on the wall, no more bottles of beer.
+				Go to the store and buy some more, 99 bottles of beer on the wall.
 
-	String sing(int startVerse, int verseNum) {
-		return String.join("", IntStream.range(0, verseNum).map(i -> startVerse - i).mapToObj(this::verse)
-				.collect(Collectors.toList()));
-	}
+				""";
+    }
+    if (verse == 1) {
+      return """
+				1 bottle of beer on the wall, 1 bottle of beer.
+				Take it down and pass it around, no more bottles of beer on the wall.
 
-	String singSong() {
-		return sing(99, 100);
-	}
+				""";
+    }
+
+    return String.format(
+        """
+				%d bottles of beer on the wall, %d bottles of beer.
+				Take one down and pass it around, %d bottle%s of beer on the wall.
+
+				""",
+        verse, verse, verse - 1, (verse == 2) ? "" : "s");
+  }
+
+  String sing(int startVerse, int verseNum) {
+    return IntStream.range(0, verseNum)
+        .map(i -> startVerse - i)
+        .mapToObj(this::verse)
+        .collect(Collectors.joining(""));
+  }
+
+  String singSong() {
+    return sing(99, 100);
+  }
 }
