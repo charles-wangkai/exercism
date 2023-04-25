87db48c25af22b21a1c0e798a802afbac95de527
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 17:48:38 +0800

    java/darts

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,22 +1,24 @@
 class Darts {
-	double x;
-	double y;
+  double x;
+  double y;
 
-	Darts(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
+  Darts(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	int score() {
-		double distance = Math.sqrt(x * x + y * y);
-		if (distance <= 1) {
-			return 10;
-		} else if (distance <= 5) {
-			return 5;
-		} else if (distance <= 10) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
+  int score() {
+    double distance = Math.sqrt(x * x + y * y);
+    if (distance <= 1) {
+      return 10;
+    }
+    if (distance <= 5) {
+      return 5;
+    }
+    if (distance <= 10) {
+      return 1;
+    }
+
+    return 0;
+  }
 }
