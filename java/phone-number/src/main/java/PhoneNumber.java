80f934419b3a5e07760c017031aafca439f05eb8
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 14:09:56 +0800

    java/phone-number

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -1,50 +1,51 @@
+import java.util.stream.Collectors;
+
 public class PhoneNumber {
-	String number;
+  String number;
 
-	PhoneNumber(String s) {
-		for (char ch : s.toCharArray()) {
-			if (!Character.isDigit(ch) && " ()-.+".indexOf(ch) == -1) {
-				if (Character.isLetter(ch)) {
-					throw new IllegalArgumentException("letters not permitted");
-				} else {
-					throw new IllegalArgumentException("punctuations not permitted");
-				}
-			}
-		}
+  PhoneNumber(String s) {
+    for (char c : s.toCharArray()) {
+      if (!Character.isDigit(c) && " ()-.+".indexOf(c) == -1) {
+        if (Character.isLetter(c)) {
+          throw new IllegalArgumentException("letters not permitted");
+        } else {
+          throw new IllegalArgumentException("punctuations not permitted");
+        }
+      }
+    }
 
-		StringBuilder digits = s.chars().filter(Character::isDigit).mapToObj(ch -> (char) ch)
-				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+    String digits =
+        s.chars()
+            .filter(Character::isDigit)
+            .mapToObj(c -> (char) c)
+            .map(String::valueOf)
+            .collect(Collectors.joining());
+    if (digits.length() > 11) {
+      throw new IllegalArgumentException("more than 11 digits");
+    }
+    if (digits.length() != 10 && digits.length() != 11) {
+      throw new IllegalArgumentException("incorrect number of digits");
+    }
+    if (digits.length() == 11 && digits.charAt(0) != '1') {
+      throw new IllegalArgumentException("11 digits must start with 1");
+    }
 
-		int length = digits.length();
-		if (length > 11) {
-			throw new IllegalArgumentException("more than 11 digits");
-		}
-		if (length != 10 && length != 11) {
-			throw new IllegalArgumentException("incorrect number of digits");
-		}
+    number = digits.substring(digits.length() - 10);
+    if (number.charAt(0) == '0') {
+      throw new IllegalArgumentException("area code cannot start with zero");
+    }
+    if (number.charAt(0) == '1') {
+      throw new IllegalArgumentException("area code cannot start with one");
+    }
+    if (number.charAt(3) == '0') {
+      throw new IllegalArgumentException("exchange code cannot start with zero");
+    }
+    if (number.charAt(3) == '1') {
+      throw new IllegalArgumentException("exchange code cannot start with one");
+    }
+  }
 
-		if (length == 11 && digits.charAt(0) != '1') {
-			throw new IllegalArgumentException("11 digits must start with 1");
-		}
-
-		number = digits.substring(length - 10, length);
-
-		if (number.charAt(0) == '0') {
-			throw new IllegalArgumentException("area code cannot start with zero");
-		}
-		if (number.charAt(0) == '1') {
-			throw new IllegalArgumentException("area code cannot start with one");
-		}
-
-		if (number.charAt(3) == '0') {
-			throw new IllegalArgumentException("exchange code cannot start with zero");
-		}
-		if (number.charAt(3) == '1') {
-			throw new IllegalArgumentException("exchange code cannot start with one");
-		}
-	}
-
-	String getNumber() {
-		return number;
-	}
+  String getNumber() {
+    return number;
+  }
 }
