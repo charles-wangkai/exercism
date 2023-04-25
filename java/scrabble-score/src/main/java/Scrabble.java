2e5b54f96780be88c3aee3b7e32533d45e508a90
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 18:41:10 +0800

    java/scrabble-score

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,53 +1,44 @@
-import java.util.HashMap;
+import static java.util.Map.entry;
+
 import java.util.Map;
 
 class Scrabble {
-	static Map<Character, Integer> letter2value;
-	static {
-		letter2value = new HashMap<Character, Integer>();
+  static final Map<Character, Integer> LETTER_TO_VALUE =
+      Map.ofEntries(
+          entry('A', 1),
+          entry('E', 1),
+          entry('I', 1),
+          entry('O', 1),
+          entry('U', 1),
+          entry('L', 1),
+          entry('N', 1),
+          entry('R', 1),
+          entry('S', 1),
+          entry('T', 1),
+          entry('D', 2),
+          entry('G', 2),
+          entry('B', 3),
+          entry('C', 3),
+          entry('M', 3),
+          entry('P', 3),
+          entry('F', 4),
+          entry('H', 4),
+          entry('V', 4),
+          entry('W', 4),
+          entry('Y', 4),
+          entry('K', 5),
+          entry('J', 8),
+          entry('X', 8),
+          entry('Q', 10),
+          entry('Z', 10));
 
-		letter2value.put('A', 1);
-		letter2value.put('E', 1);
-		letter2value.put('I', 1);
-		letter2value.put('O', 1);
-		letter2value.put('U', 1);
-		letter2value.put('L', 1);
-		letter2value.put('N', 1);
-		letter2value.put('R', 1);
-		letter2value.put('S', 1);
-		letter2value.put('T', 1);
+  String word;
 
-		letter2value.put('D', 2);
-		letter2value.put('G', 2);
+  Scrabble(String word) {
+    this.word = word;
+  }
 
-		letter2value.put('B', 3);
-		letter2value.put('C', 3);
-		letter2value.put('M', 3);
-		letter2value.put('P', 3);
-
-		letter2value.put('F', 4);
-		letter2value.put('H', 4);
-		letter2value.put('V', 4);
-		letter2value.put('W', 4);
-		letter2value.put('Y', 4);
-
-		letter2value.put('K', 5);
-
-		letter2value.put('J', 8);
-		letter2value.put('X', 8);
-
-		letter2value.put('Q', 10);
-		letter2value.put('Z', 10);
-	}
-
-	String word;
-
-	Scrabble(String word) {
-		this.word = word;
-	}
-
-	int getScore() {
-		return word.chars().map(Character::toUpperCase).map(ch -> letter2value.get((char) ch)).sum();
-	}
-
+  int getScore() {
+    return word.chars().map(Character::toUpperCase).map(c -> LETTER_TO_VALUE.get((char) c)).sum();
+  }
 }
