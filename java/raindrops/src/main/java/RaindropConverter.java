c0a45e37c0b36af2a21a76b7fbaca41cc822648e
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 09:32:39 +0800

    java/raindrops

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,20 +1,19 @@
 class RaindropConverter {
+  String convert(int number) {
+    StringBuilder result = new StringBuilder();
+    if (number % 3 == 0) {
+      result.append("Pling");
+    }
+    if (number % 5 == 0) {
+      result.append("Plang");
+    }
+    if (number % 7 == 0) {
+      result.append("Plong");
+    }
+    if (result.length() == 0) {
+      result.append(number);
+    }
 
-	String convert(int number) {
-		StringBuilder result = new StringBuilder();
-		if (number % 3 == 0) {
-			result.append("Pling");
-		}
-		if (number % 5 == 0) {
-			result.append("Plang");
-		}
-		if (number % 7 == 0) {
-			result.append("Plong");
-		}
-		if (result.length() == 0) {
-			result.append(number);
-		}
-		return result.toString();
-	}
-
+    return result.toString();
+  }
 }
