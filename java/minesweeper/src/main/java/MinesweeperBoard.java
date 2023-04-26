deca4fa4372ffe48e5ae033d171c34cd17c0e376
Kai Wang <charles.wangkai@gmail.com>
2023-04-26 20:03:21 +0800

    java/minesweeper

--- java/minesweeper/src/main/java/MinesweeperBoard.java
+++ java/minesweeper/src/main/java/MinesweeperBoard.java
@@ -1,62 +1,55 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MinesweeperBoard {
-	static final int[] R_OFFSETS = { -1, -1, 0, 1, 1, 1, 0, -1 };
-	static final int[] C_OFFSETS = { 0, 1, 1, 1, 0, -1, -1, -1 };
+  static final int[] R_OFFSETS = {-1, -1, 0, 1, 1, 1, 0, -1};
+  static final int[] C_OFFSETS = {0, 1, 1, 1, 0, -1, -1, -1};
 
-	List<String> inputBoard;
+  List<String> inputBoard;
 
-	MinesweeperBoard(List<String> inputBoard) {
-		this.inputBoard = inputBoard;
-	}
+  MinesweeperBoard(List<String> inputBoard) {
+    this.inputBoard = inputBoard;
+  }
 
-	List<String> withNumbers() {
-		if (inputBoard.isEmpty()) {
-			return Collections.emptyList();
-		}
+  List<String> withNumbers() {
+    if (inputBoard.isEmpty()) {
+      return List.of();
+    }
 
-		int row = inputBoard.size();
-		int col = inputBoard.get(0).length();
+    int row = inputBoard.size();
+    int col = inputBoard.get(0).length();
 
-		char[][] board = new char[row][col];
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				board[r][c] = transform(inputBoard, r, c);
-			}
-		}
+    char[][] board = new char[row][col];
+    for (int r = 0; r < row; ++r) {
+      for (int c = 0; c < col; ++c) {
+        board[r][c] = transform(inputBoard, r, c);
+      }
+    }
 
-		return Arrays.stream(board).map(String::new).collect(Collectors.toList());
-	}
+    return Arrays.stream(board).map(String::new).toList();
+  }
 
-	char transform(List<String> inputBoard, int r, int c) {
-		if (inputBoard.get(r).charAt(c) == '*') {
-			return '*';
-		}
+  char transform(List<String> inputBoard, int r, int c) {
+    int row = inputBoard.size();
+    int col = inputBoard.get(0).length();
 
-		int count = 0;
-		for (int i = 0; i < R_OFFSETS.length; i++) {
-			int adjacentR = r + R_OFFSETS[i];
-			int adjacentC = c + C_OFFSETS[i];
+    if (inputBoard.get(r).charAt(c) == '*') {
+      return '*';
+    }
 
-			if (isInbound(inputBoard, adjacentR, adjacentC) && inputBoard.get(adjacentR).charAt(adjacentC) == '*') {
-				count++;
-			}
-		}
+    int count = 0;
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      int adjR = r + R_OFFSETS[i];
+      int adjC = c + C_OFFSETS[i];
+      if (adjR >= 0
+          && adjR < row
+          && adjC >= 0
+          && adjC < col
+          && inputBoard.get(adjR).charAt(adjC) == '*') {
+        ++count;
+      }
+    }
 
-		if (count == 0) {
-			return ' ';
-		} else {
-			return (char) (count + '0');
-		}
-	}
-
-	boolean isInbound(List<String> inputBoard, int r, int c) {
-		int row = inputBoard.size();
-		int col = inputBoard.get(0).length();
-
-		return r >= 0 && r < row && c >= 0 && c < col;
-	}
+    return (count == 0) ? ' ' : (char) (count + '0');
+  }
 }
--- java/minesweeper/src/test/java/MinesweeperBoardTest.java
+++ java/minesweeper/src/test/java/MinesweeperBoardTest.java
@@ -3,137 +3,137 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
 import org.junit.Test;
 
 public class MinesweeperBoardTest {
 
-	@Test
-	public void testInputBoardWithNoRowsAndNoColumns() {
-		List<String> inputBoard = Collections.emptyList();
-		List<String> expectedNumberedBoard = Collections.emptyList();
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+  @Test
+  public void testInputBoardWithNoRowsAndNoColumns() {
+    List<String> inputBoard = Collections.emptyList();
+    List<String> expectedNumberedBoard = Collections.emptyList();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithOneRowAndNoColumns() {
-		List<String> inputBoard = Collections.singletonList("");
-		List<String> expectedNumberedBoard = Collections.singletonList("");
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+  @Test
+  public void testInputBoardWithOneRowAndNoColumns() {
+    List<String> inputBoard = Collections.singletonList("");
+    List<String> expectedNumberedBoard = Collections.singletonList("");
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithNoMines() {
-		List<String> inputBoard = Arrays.asList("   ", "   ", "   ");
+  @Test
+  public void testInputBoardWithNoMines() {
+    List<String> inputBoard = Arrays.asList("   ", "   ", "   ");
 
-		List<String> expectedNumberedBoard = Arrays.asList("   ", "   ", "   ");
+    List<String> expectedNumberedBoard = Arrays.asList("   ", "   ", "   ");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithOnlyMines() {
-		List<String> inputBoard = Arrays.asList("***", "***", "***");
+  @Test
+  public void testInputBoardWithOnlyMines() {
+    List<String> inputBoard = Arrays.asList("***", "***", "***");
 
-		List<String> expectedNumberedBoard = Arrays.asList("***", "***", "***");
+    List<String> expectedNumberedBoard = Arrays.asList("***", "***", "***");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithSingleMineAtCenter() {
-		List<String> inputBoard = Arrays.asList("   ", " * ", "   ");
+  @Test
+  public void testInputBoardWithSingleMineAtCenter() {
+    List<String> inputBoard = Arrays.asList("   ", " * ", "   ");
 
-		List<String> expectedNumberedBoard = Arrays.asList("111", "1*1", "111");
+    List<String> expectedNumberedBoard = Arrays.asList("111", "1*1", "111");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithMinesAroundPerimeter() {
-		List<String> inputBoard = Arrays.asList("***", "* *", "***");
+  @Test
+  public void testInputBoardWithMinesAroundPerimeter() {
+    List<String> inputBoard = Arrays.asList("***", "* *", "***");
 
-		List<String> expectedNumberedBoard = Arrays.asList("***", "*8*", "***");
+    List<String> expectedNumberedBoard = Arrays.asList("***", "*8*", "***");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithSingleRowAndTwoMines() {
-		List<String> inputBoard = Collections.singletonList(" * * ");
+  @Test
+  public void testInputBoardWithSingleRowAndTwoMines() {
+    List<String> inputBoard = Collections.singletonList(" * * ");
 
-		List<String> expectedNumberedBoard = Collections.singletonList("1*2*1");
+    List<String> expectedNumberedBoard = Collections.singletonList("1*2*1");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithSingleRowAndTwoMinesAtEdges() {
-		List<String> inputBoard = Collections.singletonList("*   *");
+  @Test
+  public void testInputBoardWithSingleRowAndTwoMinesAtEdges() {
+    List<String> inputBoard = Collections.singletonList("*   *");
 
-		List<String> expectedNumberedBoard = Collections.singletonList("*1 1*");
+    List<String> expectedNumberedBoard = Collections.singletonList("*1 1*");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithSingleColumnAndTwoMines() {
-		List<String> inputBoard = Arrays.asList(" ", "*", " ", "*", " ");
+  @Test
+  public void testInputBoardWithSingleColumnAndTwoMines() {
+    List<String> inputBoard = Arrays.asList(" ", "*", " ", "*", " ");
 
-		List<String> expectedNumberedBoard = Arrays.asList("1", "*", "2", "*", "1");
+    List<String> expectedNumberedBoard = Arrays.asList("1", "*", "2", "*", "1");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithSingleColumnAndTwoMinesAtEdges() {
-		List<String> inputBoard = Arrays.asList("*", " ", " ", " ", "*");
+  @Test
+  public void testInputBoardWithSingleColumnAndTwoMinesAtEdges() {
+    List<String> inputBoard = Arrays.asList("*", " ", " ", " ", "*");
 
-		List<String> expectedNumberedBoard = Arrays.asList("*", "1", " ", "1", "*");
+    List<String> expectedNumberedBoard = Arrays.asList("*", "1", " ", "1", "*");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testInputBoardWithMinesInCross() {
-		List<String> inputBoard = Arrays.asList("  *  ", "  *  ", "*****", "  *  ", "  *  ");
+  @Test
+  public void testInputBoardWithMinesInCross() {
+    List<String> inputBoard = Arrays.asList("  *  ", "  *  ", "*****", "  *  ", "  *  ");
 
-		List<String> expectedNumberedBoard = Arrays.asList(" 2*2 ", "25*52", "*****", "25*52", " 2*2 ");
+    List<String> expectedNumberedBoard = Arrays.asList(" 2*2 ", "25*52", "*****", "25*52", " 2*2 ");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 
-	@Test
-	public void testLargeInputBoard() {
-		List<String> inputBoard = Arrays.asList(" *  * ", "  *   ", "    * ", "   * *", " *  * ", "      ");
+  @Test
+  public void testLargeInputBoard() {
+    List<String> inputBoard =
+        Arrays.asList(" *  * ", "  *   ", "    * ", "   * *", " *  * ", "      ");
 
-		List<String> expectedNumberedBoard = Arrays.asList("1*22*1", "12*322", " 123*2", "112*4*", "1*22*2", "111111");
+    List<String> expectedNumberedBoard =
+        Arrays.asList("1*22*1", "12*322", " 123*2", "112*4*", "1*22*2", "111111");
 
-		List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
+    List<String> actualNumberedBoard = new MinesweeperBoard(inputBoard).withNumbers();
 
-		assertEquals(expectedNumberedBoard, actualNumberedBoard);
-	}
-
+    assertEquals(expectedNumberedBoard, actualNumberedBoard);
+  }
 }
