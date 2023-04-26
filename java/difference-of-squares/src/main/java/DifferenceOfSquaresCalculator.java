8dd765c5384d23e89a4b761b80b4f3df9a283167
Kai Wang <charles.wangkai@gmail.com>
2023-04-26 11:58:21 +0800

    java/difference-of-squares

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,16 +1,17 @@
 import java.util.stream.IntStream;
 
 public class DifferenceOfSquaresCalculator {
-	int computeSumOfSquaresTo(int n) {
-		return IntStream.rangeClosed(1, n).map(x -> x * x).sum();
-	}
+  int computeSumOfSquaresTo(int n) {
+    return IntStream.rangeClosed(1, n).map(x -> x * x).sum();
+  }
 
-	int computeSquareOfSumTo(int n) {
-		int sum = IntStream.rangeClosed(1, n).sum();
-		return sum * sum;
-	}
+  int computeSquareOfSumTo(int n) {
+    int sum = IntStream.rangeClosed(1, n).sum();
 
-	int computeDifferenceOfSquares(int n) {
-		return computeSquareOfSumTo(n) - computeSumOfSquaresTo(n);
-	}
+    return sum * sum;
+  }
+
+  int computeDifferenceOfSquares(int n) {
+    return computeSquareOfSumTo(n) - computeSumOfSquaresTo(n);
+  }
 }
