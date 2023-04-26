e44bbe9943c537ee9cb3484d145357bcfa7fd92b
Kai Wang <charles.wangkai@gmail.com>
2023-04-26 18:52:17 +0800

    java/meetup

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -3,32 +3,37 @@ import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
 public class Meetup {
-	int month;
-	int year;
+  int month;
+  int year;
 
-	Meetup(int month, int year) {
-		this.month = month;
-		this.year = year;
-	}
+  Meetup(int month, int year) {
+    this.month = month;
+    this.year = year;
+  }
 
-	LocalDate day(DayOfWeek dayOfWeek, MeetupSchedule schedule) {
-		LocalDate date = LocalDate.of(year, month, 1);
-		if (schedule == MeetupSchedule.FIRST) {
-			return date.with(TemporalAdjusters.firstInMonth(dayOfWeek));
-		} else if (schedule == MeetupSchedule.SECOND) {
-			return date.with(TemporalAdjusters.dayOfWeekInMonth(2, dayOfWeek));
-		} else if (schedule == MeetupSchedule.THIRD) {
-			return date.with(TemporalAdjusters.dayOfWeekInMonth(3, dayOfWeek));
-		} else if (schedule == MeetupSchedule.FOURTH) {
-			return date.with(TemporalAdjusters.dayOfWeekInMonth(4, dayOfWeek));
-		} else if (schedule == MeetupSchedule.LAST) {
-			return date.with(TemporalAdjusters.lastInMonth(dayOfWeek));
-		} else {
-			LocalDate result = date.with(TemporalAdjusters.firstInMonth(dayOfWeek));
-			while (!(result.getDayOfMonth() >= 13 && result.getDayOfMonth() <= 19)) {
-				result = result.with(TemporalAdjusters.next(dayOfWeek));
-			}
-			return result;
-		}
-	}
+  LocalDate day(DayOfWeek dayOfWeek, MeetupSchedule schedule) {
+    LocalDate date = LocalDate.of(year, month, 1);
+    if (schedule == MeetupSchedule.FIRST) {
+      return date.with(TemporalAdjusters.firstInMonth(dayOfWeek));
+    }
+    if (schedule == MeetupSchedule.SECOND) {
+      return date.with(TemporalAdjusters.dayOfWeekInMonth(2, dayOfWeek));
+    }
+    if (schedule == MeetupSchedule.THIRD) {
+      return date.with(TemporalAdjusters.dayOfWeekInMonth(3, dayOfWeek));
+    }
+    if (schedule == MeetupSchedule.FOURTH) {
+      return date.with(TemporalAdjusters.dayOfWeekInMonth(4, dayOfWeek));
+    }
+    if (schedule == MeetupSchedule.LAST) {
+      return date.with(TemporalAdjusters.lastInMonth(dayOfWeek));
+    }
+
+    LocalDate result = date.with(TemporalAdjusters.firstInMonth(dayOfWeek));
+    while (!(result.getDayOfMonth() >= 13 && result.getDayOfMonth() <= 19)) {
+      result = result.with(TemporalAdjusters.next(dayOfWeek));
+    }
+
+    return result;
+  }
 }
