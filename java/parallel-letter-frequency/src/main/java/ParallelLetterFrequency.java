0ba33a4dd10e33da8622a8dd3b15a8a10cabcf95
Kai Wang <charles.wangkai@gmail.com>
2023-04-28 00:57:22 +0800

    java/parallel-letter-frequency

--- java/parallel-letter-frequency/src/main/java/ParallelLetterFrequency.java
+++ java/parallel-letter-frequency/src/main/java/ParallelLetterFrequency.java
@@ -3,14 +3,18 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ParallelLetterFrequency {
-	String s;
+  String s;
 
-	ParallelLetterFrequency(String s) {
-		this.s = s;
-	}
+  ParallelLetterFrequency(String s) {
+    this.s = s;
+  }
 
-	Map<Integer, Integer> letterCounts() {
-		return s.toLowerCase().chars().parallel().filter(Character::isAlphabetic).boxed()
-				.collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(ch -> 1)));
-	}
+  Map<Integer, Integer> letterCounts() {
+    return s.toLowerCase()
+        .chars()
+        .parallel()
+        .filter(Character::isAlphabetic)
+        .boxed()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
+  }
 }
