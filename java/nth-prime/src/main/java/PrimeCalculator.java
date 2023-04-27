4f617f2471fbf2209961595e56d97d2826aa2d84
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 21:45:25 +0800

    java/nth-prime

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,29 +1,33 @@
 public class PrimeCalculator {
-	int nth(int n) {
-		if (n <= 0) {
-			throw new IllegalArgumentException();
-		}
+  int nth(int n) {
+    if (n <= 0) {
+      throw new IllegalArgumentException();
+    }
 
-		int prime = 1;
-		for (int i = 0; i < n; i++) {
-			prime = nextPrime(prime);
-		}
-		return prime;
-	}
+    int prime = 1;
+    for (int i = 0; i < n; ++i) {
+      prime = nextPrime(prime);
+    }
 
-	int nextPrime(int x) {
-		do {
-			x++;
-		} while (!isPrime(x));
-		return x;
-	}
+    return prime;
+  }
 
-	boolean isPrime(int x) {
-		for (int i = 2; i * i <= x; i++) {
-			if (x % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+  int nextPrime(int x) {
+    while (true) {
+      ++x;
+      if (isPrime(x)) {
+        return x;
+      }
+    }
+  }
+
+  boolean isPrime(int x) {
+    for (int i = 2; i * i <= x; ++i) {
+      if (x % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
--- java/nth-prime/src/test/java/PrimeCalculatorTest.java
+++ java/nth-prime/src/test/java/PrimeCalculatorTest.java
@@ -1,46 +1,34 @@
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class PrimeCalculatorTest {
-	private PrimeCalculator primeCalculator;
 
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
+  private PrimeCalculator primeCalculator = new PrimeCalculator();
 
-	@Before
-	public void setup() {
-		primeCalculator = new PrimeCalculator();
-	}
+  @Test
+  public void testFirstPrime() {
+    assertThat(primeCalculator.nth(1)).isEqualTo(2);
+  }
 
-	@Test
-	public void testFirstPrime() {
-		assertThat(primeCalculator.nth(1), is(2));
-	}
+  @Test
+  public void testSecondPrime() {
+    assertThat(primeCalculator.nth(2)).isEqualTo(3);
+  }
 
-	@Test
-	public void testSecondPrime() {
-		assertThat(primeCalculator.nth(2), is(3));
-	}
+  @Test
+  public void testSixthPrime() {
+    assertThat(primeCalculator.nth(6)).isEqualTo(13);
+  }
 
-	@Test
-	public void testSixthPrime() {
-		assertThat(primeCalculator.nth(6), is(13));
-	}
+  @Test
+  public void testBigPrime() {
+    assertThat(primeCalculator.nth(10001)).isEqualTo(104743);
+  }
 
-	@Test
-	public void testBigPrime() {
-		assertThat(primeCalculator.nth(10001), is(104743));
-	}
-
-	@Test
-	public void testUndefinedPrime() {
-		expectedException.expect(IllegalArgumentException.class);
-		primeCalculator.nth(0);
-	}
-
+  @Test
+  public void testUndefinedPrime() {
+    assertThrows(IllegalArgumentException.class, () -> primeCalculator.nth(0));
+  }
 }
