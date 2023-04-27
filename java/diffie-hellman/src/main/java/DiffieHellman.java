1f815d6e46b2a853ea553fe75e694067a4efab5e
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 17:17:04 +0800

    java/diffie-hellman

--- java/diffie-hellman/src/main/java/DiffieHellman.java
+++ java/diffie-hellman/src/main/java/DiffieHellman.java
@@ -2,23 +2,22 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class DiffieHellman {
-	BigInteger privateKey(BigInteger prime) {
-		Random random = new Random();
-		BigInteger limit = prime.subtract(BigInteger.valueOf(2));
+  BigInteger privateKey(BigInteger prime) {
+    Random random = new Random();
+    BigInteger limit = prime.subtract(BigInteger.valueOf(2));
+    while (true) {
+      BigInteger r = new BigInteger(limit.bitLength(), random);
+      if (r.compareTo(limit) < 0) {
+        return r.add(BigInteger.valueOf(2));
+      }
+    }
+  }
 
-		BigInteger r;
-		do {
-			r = new BigInteger(limit.bitLength(), random);
-		} while (r.compareTo(limit) >= 0);
+  BigInteger publicKey(BigInteger primeA, BigInteger primeB, BigInteger privateKey) {
+    return primeB.modPow(privateKey, primeA);
+  }
 
-		return r.add(BigInteger.valueOf(2));
-	}
-
-	BigInteger publicKey(BigInteger primeA, BigInteger primeB, BigInteger privateKey) {
-		return primeB.modPow(privateKey, primeA);
-	}
-
-	BigInteger secret(BigInteger prime, BigInteger publicKey, BigInteger privateKey) {
-		return publicKey.modPow(privateKey, prime);
-	}
+  BigInteger secret(BigInteger prime, BigInteger publicKey, BigInteger privateKey) {
+    return publicKey.modPow(privateKey, prime);
+  }
 }
--- java/diffie-hellman/src/test/java/DiffieHellmanTest.java
+++ java/diffie-hellman/src/test/java/DiffieHellmanTest.java
@@ -3,83 +3,79 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DiffieHellmanTest {
 
-	private DiffieHellman diffieHellman;
+  private DiffieHellman diffieHellman;
 
-	@Before
-	public void setUp() {
-		diffieHellman = new DiffieHellman();
-	}
+  @Before
+  public void setUp() {
+    diffieHellman = new DiffieHellman();
+  }
 
-	@Test
-	public void testPrivateKeyInRange() {
-		BigInteger prime = BigInteger.valueOf(23);
-		List<BigInteger> privateKeys = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			privateKeys.add(diffieHellman.privateKey(prime));
-		}
+  @Test
+  public void testPrivateKeyInRange() {
+    BigInteger prime = BigInteger.valueOf(23);
+    List<BigInteger> privateKeys = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      privateKeys.add(diffieHellman.privateKey(prime));
+    }
 
-		for (BigInteger privateKey : privateKeys) {
-			assertTrue(privateKey.compareTo(BigInteger.ONE) >= 0);
-			assertTrue(privateKey.compareTo(prime) < 0);
-		}
-	}
+    for (BigInteger privateKey : privateKeys) {
+      assertTrue(privateKey.compareTo(BigInteger.ONE) >= 0);
+      assertTrue(privateKey.compareTo(prime) < 0);
+    }
+  }
 
-	// Note: due to the nature of randomness, there is always a chance that this
-	// test fails
-	// Be sure to check the actual generated values
+  // Note: due to the nature of randomness, there is always a chance that this test fails
+  // Be sure to check the actual generated values
 
-	@Test
-	public void testPrivateKeyRandomlyGenerated() {
-		BigInteger prime = BigInteger.valueOf(7919);
-		BigInteger privateKeyA = diffieHellman.privateKey(prime);
-		BigInteger privateKeyB = diffieHellman.privateKey(prime);
+  @Test
+  public void testPrivateKeyRandomlyGenerated() {
+    BigInteger prime = BigInteger.valueOf(7919);
+    BigInteger privateKeyA = diffieHellman.privateKey(prime);
+    BigInteger privateKeyB = diffieHellman.privateKey(prime);
 
-		assertNotEquals(privateKeyA, privateKeyB);
-	}
+    assertNotEquals(privateKeyA, privateKeyB);
+  }
 
-	@Test
-	public void testPublicKeyCorrectlyCalculated() {
-		BigInteger primeA = BigInteger.valueOf(23);
-		BigInteger primeB = BigInteger.valueOf(5);
-		BigInteger privateKey = BigInteger.valueOf(6);
-		BigInteger expected = BigInteger.valueOf(8);
+  @Test
+  public void testPublicKeyCorrectlyCalculated() {
+    BigInteger primeA = BigInteger.valueOf(23);
+    BigInteger primeB = BigInteger.valueOf(5);
+    BigInteger privateKey = BigInteger.valueOf(6);
+    BigInteger expected = BigInteger.valueOf(8);
 
-		assertEquals(expected, diffieHellman.publicKey(primeA, primeB, privateKey));
-	}
+    assertEquals(expected, diffieHellman.publicKey(primeA, primeB, privateKey));
+  }
 
-	@Test
-	public void testSecretKeyCorrectlyCalculated() {
-		BigInteger prime = BigInteger.valueOf(23);
-		BigInteger publicKey = BigInteger.valueOf(19);
-		BigInteger privateKey = BigInteger.valueOf(6);
-		BigInteger expected = BigInteger.valueOf(2);
+  @Test
+  public void testSecretKeyCorrectlyCalculated() {
+    BigInteger prime = BigInteger.valueOf(23);
+    BigInteger publicKey = BigInteger.valueOf(19);
+    BigInteger privateKey = BigInteger.valueOf(6);
+    BigInteger expected = BigInteger.valueOf(2);
 
-		assertEquals(expected, diffieHellman.secret(prime, publicKey, privateKey));
-	}
+    assertEquals(expected, diffieHellman.secret(prime, publicKey, privateKey));
+  }
 
-	@Test
-	public void testExchange() {
-		BigInteger primeA = BigInteger.valueOf(23);
-		BigInteger primeB = BigInteger.valueOf(5);
+  @Test
+  public void testExchange() {
+    BigInteger primeA = BigInteger.valueOf(23);
+    BigInteger primeB = BigInteger.valueOf(5);
 
-		BigInteger alicePrivateKey = diffieHellman.privateKey(primeA);
-		BigInteger bobPrivateKey = diffieHellman.privateKey(primeB);
+    BigInteger alicePrivateKey = diffieHellman.privateKey(primeA);
+    BigInteger bobPrivateKey = diffieHellman.privateKey(primeB);
 
-		BigInteger alicePublicKey = diffieHellman.publicKey(primeA, primeB, alicePrivateKey);
-		BigInteger bobPublicKey = diffieHellman.publicKey(primeA, primeB, bobPrivateKey);
+    BigInteger alicePublicKey = diffieHellman.publicKey(primeA, primeB, alicePrivateKey);
+    BigInteger bobPublicKey = diffieHellman.publicKey(primeA, primeB, bobPrivateKey);
 
-		BigInteger secretA = diffieHellman.secret(primeA, bobPublicKey, alicePrivateKey);
-		BigInteger secretB = diffieHellman.secret(primeA, alicePublicKey, bobPrivateKey);
+    BigInteger secretA = diffieHellman.secret(primeA, bobPublicKey, alicePrivateKey);
+    BigInteger secretB = diffieHellman.secret(primeA, alicePublicKey, bobPrivateKey);
 
-		assertEquals(secretA, secretB);
-	}
-
+    assertEquals(secretA, secretB);
+  }
 }
