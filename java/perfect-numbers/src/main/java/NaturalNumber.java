eff511a80087f3d6d795d02a2144288385407121
Kai Wang <charles.wangkai@gmail.com>
2023-04-28 01:03:28 +0800

    java/perfect-numbers

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,28 +1,29 @@
 import java.util.stream.IntStream;
 
 class NaturalNumber {
-	int number;
+  int number;
 
-	NaturalNumber(int number) {
-		if (number <= 0) {
-			throw new IllegalArgumentException("You must supply a natural number (positive integer)");
-		}
+  NaturalNumber(int number) {
+    if (number <= 0) {
+      throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+    }
 
-		this.number = number;
-	}
+    this.number = number;
+  }
 
-	Classification getClassification() {
-		int aliquotSum = computeAliquotSum();
-		if (aliquotSum == number) {
-			return Classification.PERFECT;
-		} else if (aliquotSum > number) {
-			return Classification.ABUNDANT;
-		} else {
-			return Classification.DEFICIENT;
-		}
-	}
+  Classification getClassification() {
+    int aliquotSum = computeAliquotSum();
+    if (aliquotSum < number) {
+      return Classification.DEFICIENT;
+    }
+    if (aliquotSum > number) {
+      return Classification.ABUNDANT;
+    }
 
-	int computeAliquotSum() {
-		return IntStream.range(1, number).filter(x -> number % x == 0).sum();
-	}
+    return Classification.PERFECT;
+  }
+
+  int computeAliquotSum() {
+    return IntStream.range(1, number).filter(x -> number % x == 0).sum();
+  }
 }
