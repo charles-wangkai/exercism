caed6838ca347bc0964f78e7f0dddf0b7bae5eb9
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 23:15:58 +0800

    java/ocr-numbers

--- java/ocr-numbers/src/main/java/OpticalCharacterReader.java
+++ java/ocr-numbers/src/main/java/OpticalCharacterReader.java
@@ -1,101 +1,111 @@
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class OpticalCharacterReader {
-	@SuppressWarnings("rawtypes")
-	static final List[] IMAGES = { 
-			Arrays.asList(
-					" _ ", 
-					"| |", 
-					"|_|", 
-					"   "), 
-			Arrays.asList(
-					"   ", 
-					"  |", 
-					"  |", 
-					"   "),
-			Arrays.asList(
-					" _ ", 
-					" _|", 
-					"|_ ", 
-					"   "), 
-			Arrays.asList(
-					" _ ", 
-					" _|", 
-					" _|", 
-					"   "),
-			Arrays.asList(
-					"   ", 
-					"|_|", 
-					"  |", 
-					"   "), 
-			Arrays.asList(
-					" _ ", 
-					"|_ ", 
-					" _|", 
-					"   "),
-			Arrays.asList(
-					" _ ", 
-					"|_ ", 
-					"|_|", 
-					"   "), 
-			Arrays.asList(
-					" _ ", 
-					"  |", 
-					"  |", 
-					"   "),
-			Arrays.asList(
-					" _ ", 
-					"|_|", 
-					"|_|", 
-					"   "), 
-			Arrays.asList(
-					" _ ", 
-					"|_|", 
-					" _|", 
-					"   ") };
+  static final String[] IMAGES = {
+    """
+     _
+    | |
+    |_|
 
-	String parse(List<String> lines) {
-		int row = lines.size();
-		if (row % 4 != 0) {
-			throw new IllegalArgumentException("Number of input rows must be a positive multiple of 4");
-		}
+    """,
+    """
 
-		int col = lines.get(0).length();
-		if (col % 3 != 0) {
-			throw new IllegalArgumentException("Number of input columns must be a positive multiple of 3");
-		}
+      |
+      |
 
-		StringBuilder result = new StringBuilder();
-		for (int r = 0; r < row; r += 4) {
-			if (result.length() != 0) {
-				result.append(",");
-			}
+    """,
+    """
+     _
+     _|
+    |_
 
-			for (int c = 0; c < col; c += 3) {
-				result.append(recognize(lines, r, c));
-			}
-		}
+    """,
+    """
+     _
+     _|
+     _|
 
-		return result.toString();
-	}
+    """,
+    """
 
-	@SuppressWarnings("unchecked")
-	char recognize(List<String> lines, int r, int c) {
-		List<String> part = IntStream.range(r, r + 4).mapToObj(i -> lines.get(i).substring(c, c + 3))
-				.collect(Collectors.toList());
+    |_|
+      |
 
-		for (int i = 0; i < IMAGES.length; i++) {
-			if (isSame(part, IMAGES[i])) {
-				return (char) (i + '0');
-			}
-		}
-		return '?';
-	}
+    """,
+    """
+     _
+    |_
+     _|
 
-	boolean isSame(List<String> part, List<String> image) {
-		return IntStream.range(0, part.size()).allMatch(i -> part.get(i).equals(image.get(i)));
-	}
+    """,
+    """
+     _
+    |_
+    |_|
+
+    """,
+    """
+     _
+      |
+      |
+
+    """,
+    """
+     _
+    |_|
+    |_|
+
+    """,
+    """
+     _
+    |_|
+     _|
+
+    """
+  };
+
+  String parse(List<String> lines) {
+    int row = lines.size();
+    if (row % 4 != 0) {
+      throw new IllegalArgumentException("Number of input rows must be a positive multiple of 4");
+    }
+
+    int col = lines.get(0).length();
+    if (col % 3 != 0) {
+      throw new IllegalArgumentException(
+          "Number of input columns must be a positive multiple of 3");
+    }
+
+    StringBuilder result = new StringBuilder();
+    for (int r = 0; r < row; r += 4) {
+      if (result.length() != 0) {
+        result.append(",");
+      }
+
+      for (int c = 0; c < col; c += 3) {
+        result.append(recognize(lines, r, c));
+      }
+    }
+
+    return result.toString();
+  }
+
+  char recognize(List<String> lines, int r, int c) {
+    List<String> part =
+        IntStream.range(r, r + 4).mapToObj(i -> lines.get(i).substring(c, c + 3)).toList();
+
+    for (int i = 0; i < IMAGES.length; ++i) {
+      if (isSame(part, IMAGES[i])) {
+        return (char) (i + '0');
+      }
+    }
+
+    return '?';
+  }
+
+  boolean isSame(List<String> part, String image) {
+    return IntStream.range(0, part.size())
+        .allMatch(i -> part.get(i).stripTrailing().equals(image.split("\n", -1)[i]));
+  }
 }
--- java/ocr-numbers/src/test/java/OpticalCharacterReaderTest.java
+++ java/ocr-numbers/src/test/java/OpticalCharacterReaderTest.java
@@ -1,140 +1,182 @@
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.util.Arrays;
-
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class OpticalCharacterReaderTest {
 
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
+  @Test
+  public void testReaderRecognizesSingle0() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", "| |", "|_|", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle0() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", "| |", "|_|", "   "));
+    assertEquals("0", parsedInput);
+  }
 
-		assertEquals("0", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle1() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList("   ", "  |", "  |", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle1() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList("   ", "  |", "  |", "   "));
+    assertEquals("1", parsedInput);
+  }
 
-		assertEquals("1", parsedInput);
-	}
+  @Test
+  public void testReaderReturnsQuestionMarkForUnreadableButCorrectlySizedInput() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList("   ", "  _", "  |", "   "));
 
-	@Test
-	public void testReaderReturnsQuestionMarkForUnreadableButCorrectlySizedInput() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList("   ", "  _", "  |", "   "));
+    assertEquals("?", parsedInput);
+  }
 
-		assertEquals("?", parsedInput);
-	}
+  @Test
+  public void testReaderThrowsExceptionWhenNumberOfInputLinesIsNotAMultipleOf4() {
+    IllegalArgumentException expected =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new OpticalCharacterReader().parse(Arrays.asList(" _ ", "| |", "   ")));
 
-	@Test
-	public void testReaderThrowsExceptionWhenNumberOfInputLinesIsNotAMultipleOf4() {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException.expectMessage("Number of input rows must be a positive multiple of 4");
+    assertThat(expected).hasMessage("Number of input rows must be a positive multiple of 4");
+  }
 
-		new OpticalCharacterReader().parse(Arrays.asList(" _ ", "| |", "   "));
-	}
+  @Test
+  public void testReaderThrowsExceptionWhenNumberOfInputColumnsIsNotAMultipleOf3() {
+    IllegalArgumentException expected =
+        assertThrows(
+            IllegalArgumentException.class,
+            () ->
+                new OpticalCharacterReader().parse(Arrays.asList("    ", "   |", "   |", "    ")));
 
-	@Test
-	public void testReaderThrowsExceptionWhenNumberOfInputColumnsIsNotAMultipleOf3() {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException.expectMessage("Number of input columns must be a positive multiple of 3");
+    assertThat(expected).hasMessage("Number of input columns must be a positive multiple of 3");
+  }
 
-		new OpticalCharacterReader().parse(Arrays.asList("    ", "   |", "   |", "    "));
-	}
+  @Test
+  public void testReaderRecognizesBinarySequence110101100() {
+    String parsedInput =
+        new OpticalCharacterReader()
+            .parse(
+                Arrays.asList(
+                    "       _     _        _  _ ",
+                    "  |  || |  || |  |  || || |",
+                    "  |  ||_|  ||_|  |  ||_||_|",
+                    "                           "));
 
-	@Test
-	public void testReaderRecognizesBinarySequence110101100() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList("       _     _        _  _ ",
-				"  |  || |  || |  |  || || |", "  |  ||_|  ||_|  |  ||_||_|", "                           "));
+    assertEquals("110101100", parsedInput);
+  }
 
-		assertEquals("110101100", parsedInput);
-	}
+  @Test
+  public void testReaderReplacesUnreadableDigitsWithQuestionMarksWithinSequence() {
+    String parsedInput =
+        new OpticalCharacterReader()
+            .parse(
+                Arrays.asList(
+                    "       _     _           _ ",
+                    "  |  || |  || |     || || |",
+                    "  |  | _|  ||_|  |  ||_||_|",
+                    "                           "));
 
-	@Test
-	public void testReaderReplacesUnreadableDigitsWithQuestionMarksWithinSequence() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList("       _     _           _ ",
-				"  |  || |  || |     || || |", "  |  | _|  ||_|  |  ||_||_|", "                           "));
+    assertEquals("11?10?1?0", parsedInput);
+  }
 
-		assertEquals("11?10?1?0", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle2() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", " _|", "|_ ", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle2() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", " _|", "|_ ", "   "));
+    assertEquals("2", parsedInput);
+  }
 
-		assertEquals("2", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle3() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", " _|", " _|", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle3() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", " _|", " _|", "   "));
+    assertEquals("3", parsedInput);
+  }
 
-		assertEquals("3", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle4() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList("   ", "|_|", "  |", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle4() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList("   ", "|_|", "  |", "   "));
+    assertEquals("4", parsedInput);
+  }
 
-		assertEquals("4", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle5() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_ ", " _|", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle5() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_ ", " _|", "   "));
+    assertEquals("5", parsedInput);
+  }
 
-		assertEquals("5", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle6() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_ ", "|_|", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle6() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_ ", "|_|", "   "));
+    assertEquals("6", parsedInput);
+  }
 
-		assertEquals("6", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle7() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", "  |", "  |", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle7() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", "  |", "  |", "   "));
+    assertEquals("7", parsedInput);
+  }
 
-		assertEquals("7", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle8() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_|", "|_|", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle8() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_|", "|_|", "   "));
+    assertEquals("8", parsedInput);
+  }
 
-		assertEquals("8", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSingle9() {
+    String parsedInput =
+        new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_|", " _|", "   "));
 
-	@Test
-	public void testReaderRecognizesSingle9() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(" _ ", "|_|", " _|", "   "));
+    assertEquals("9", parsedInput);
+  }
 
-		assertEquals("9", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesSequence1234567890() {
+    String parsedInput =
+        new OpticalCharacterReader()
+            .parse(
+                Arrays.asList(
+                    "    _  _     _  _  _  _  _  _ ",
+                    "  | _| _||_||_ |_   ||_||_|| |",
+                    "  ||_  _|  | _||_|  ||_| _||_|",
+                    "                              "));
 
-	@Test
-	public void testReaderRecognizesSequence1234567890() {
-		String parsedInput = new OpticalCharacterReader().parse(Arrays.asList("    _  _     _  _  _  _  _  _ ",
-				"  | _| _||_||_ |_   ||_||_|| |", "  ||_  _|  | _||_|  ||_| _||_|", "                              "));
+    assertEquals("1234567890", parsedInput);
+  }
 
-		assertEquals("1234567890", parsedInput);
-	}
+  @Test
+  public void testReaderRecognizesAndCorrectlyFormatsMultiRowInput() {
+    String parsedInput =
+        new OpticalCharacterReader()
+            .parse(
+                Arrays.asList(
+                    "    _  _ ",
+                    "  | _| _|",
+                    "  ||_  _|",
+                    "         ",
+                    "    _  _ ",
+                    "|_||_ |_ ",
+                    "  | _||_|",
+                    "         ",
+                    " _  _  _ ",
+                    "  ||_||_|",
+                    "  ||_| _|",
+                    "         "));
 
-	@Test
-	public void testReaderRecognizesAndCorrectlyFormatsMultiRowInput() {
-		String parsedInput = new OpticalCharacterReader()
-				.parse(Arrays.asList("    _  _ ", "  | _| _|", "  ||_  _|", "         ", "    _  _ ", "|_||_ |_ ",
-						"  | _||_|", "         ", " _  _  _ ", "  ||_||_|", "  ||_| _|", "         "));
-
-		assertEquals("123,456,789", parsedInput);
-	}
-
+    assertEquals("123,456,789", parsedInput);
+  }
 }
