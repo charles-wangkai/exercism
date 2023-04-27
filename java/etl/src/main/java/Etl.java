a1bf9671aad1a291a5e8b50b6898093e31772103
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 17:45:27 +0800

    java/etl

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,17 +1,16 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class Etl {
-	public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-		Map<String, Integer> letter2score = new HashMap<String, Integer>();
-		for (Entry<Integer, List<String>> entry : old.entrySet()) {
-			int score = entry.getKey();
-			for (String letter : entry.getValue()) {
-				letter2score.put(letter.toLowerCase(), score);
-			}
-		}
-		return letter2score;
-	}
+  public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+    Map<String, Integer> letterToScore = new HashMap<>();
+    for (int score : old.keySet()) {
+      for (String letter : old.get(score)) {
+        letterToScore.put(letter.toLowerCase(), score);
+      }
+    }
+
+    return letterToScore;
+  }
 }
