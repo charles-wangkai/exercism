93e727a4b12f4d3cb1c62eed7309e4220447502b
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 21:22:36 +0800

    java/leap

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,5 +1,5 @@
 class Leap {
-	boolean isLeapYear(int year) {
-		return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-	}
+  boolean isLeapYear(int year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+  }
 }
--- java/leap/src/test/java/LeapTest.java
+++ java/leap/src/test/java/LeapTest.java
@@ -6,56 +6,55 @@ import org.junit.Test;
 
 public class LeapTest {
 
-	private Leap leap;
+  private Leap leap;
 
-	@Before
-	public void setup() {
-		leap = new Leap();
-	}
+  @Before
+  public void setup() {
+    leap = new Leap();
+  }
 
-	@Test
-	public void testYearNotDivBy4InCommonYear() {
-		assertFalse(leap.isLeapYear(2015));
-	}
+  @Test
+  public void testYearNotDivBy4InCommonYear() {
+    assertFalse(leap.isLeapYear(2015));
+  }
 
-	@Test
-	public void testYearDivBy2NotDivBy4InCommonYear() {
-		assertFalse(leap.isLeapYear(1970));
-	}
+  @Test
+  public void testYearDivBy2NotDivBy4InCommonYear() {
+    assertFalse(leap.isLeapYear(1970));
+  }
 
-	@Test
-	public void testYearDivBy4NotDivBy100InLeapYear() {
-		assertTrue(leap.isLeapYear(1996));
-	}
+  @Test
+  public void testYearDivBy4NotDivBy100InLeapYear() {
+    assertTrue(leap.isLeapYear(1996));
+  }
 
-	@Test
-	public void testYearDivBy4And5InLeapYear() {
-		assertTrue(leap.isLeapYear(1960));
-	}
+  @Test
+  public void testYearDivBy4And5InLeapYear() {
+    assertTrue(leap.isLeapYear(1960));
+  }
 
-	@Test
-	public void testYearDivBy100NotDivBy400InCommonYear() {
-		assertFalse(leap.isLeapYear(2100));
-	}
+  @Test
+  public void testYearDivBy100NotDivBy400InCommonYear() {
+    assertFalse(leap.isLeapYear(2100));
+  }
 
-	@Test
-	public void testYearDivBy100NotDivBy3IsNotLeapYear() {
-		assertFalse(leap.isLeapYear(1900));
-	}
+  @Test
+  public void testYearDivBy100NotDivBy3IsNotLeapYear() {
+    assertFalse(leap.isLeapYear(1900));
+  }
 
-	@Test
-	public void testYearDivBy400InLeapYear() {
-		assertTrue(leap.isLeapYear(2000));
-	}
+  @Test
+  public void testYearDivBy400InLeapYear() {
+    assertTrue(leap.isLeapYear(2000));
+  }
 
-	@Test
-	public void testYearDivBy400NotDivBy125IsLeapYear() {
-		assertTrue(leap.isLeapYear(2400));
-	}
+  @Test
+  public void testYearDivBy400NotDivBy125IsLeapYear() {
+    assertTrue(leap.isLeapYear(2400));
+  }
 
-	@Test
-	public void testYearDivBy200NotDivBy400InCommonYear() {
-		assertFalse(leap.isLeapYear(1800));
-	}
-
+  @Test
+  public void testYearDivBy200NotDivBy400InCommonYear() {
+    assertFalse(leap.isLeapYear(1800));
+  }
 }
