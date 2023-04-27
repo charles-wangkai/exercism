7f2b8b8c22ad76e8c12605b978d61b93307eafad
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 09:52:56 +0800

    java/affine-cipher

--- java/affine-cipher/src/main/java/AffineCipher.java
+++ java/affine-cipher/src/main/java/AffineCipher.java
@@ -1,66 +1,61 @@
 public class AffineCipher {
-	static final int M = 26;
+  static final int M = 26;
 
-	String encode(String plain, int a, int b) {
-		checkValidity(a);
+  String encode(String plain, int a, int b) {
+    checkValidity(a);
 
-		StringBuilder result = new StringBuilder();
-		for (char ch : plain.toLowerCase().toCharArray()) {
-			if (Character.isLetterOrDigit(ch)) {
-				char encoded;
-				if (Character.isLetter(ch)) {
-					encoded = (char) ((a * (ch - 'a') + b) % M + 'a');
-				} else {
-					encoded = ch;
-				}
+    StringBuilder result = new StringBuilder();
+    for (char c : plain.toLowerCase().toCharArray()) {
+      if (Character.isLetterOrDigit(c)) {
+        char encoded = Character.isLetter(c) ? (char) ((a * (c - 'a') + b) % M + 'a') : c;
 
-				if (result.length() % 6 == 5) {
-					result.append(' ');
-				}
-				result.append(encoded);
-			}
-		}
-		return result.toString();
-	}
+        if (result.length() % 6 == 5) {
+          result.append(' ');
+        }
+        result.append(encoded);
+      }
+    }
 
-	String decode(String cipher, int a, int b) {
-		checkValidity(a);
+    return result.toString();
+  }
 
-		StringBuilder result = new StringBuilder();
-		for (char ch : cipher.toCharArray()) {
-			if (Character.isLetterOrDigit(ch)) {
-				char decoded;
-				if (Character.isLetter(ch)) {
-					decoded = (char) (multiplyMod(modInverse(a), ((ch - 'a') - b) % M + M) % M + 'a');
-				} else {
-					decoded = ch;
-				}
+  String decode(String cipher, int a, int b) {
+    checkValidity(a);
 
-				result.append(decoded);
-			}
-		}
-		return result.toString();
-	}
+    StringBuilder result = new StringBuilder();
+    for (char c : cipher.toCharArray()) {
+      if (Character.isLetterOrDigit(c)) {
+        char decoded =
+            Character.isLetter(c)
+                ? (char) (multiplyMod(modInverse(a), ((c - 'a') - b) % M + M) % M + 'a')
+                : c;
 
-	int modInverse(int x) {
-		for (int i = 1;; i++) {
-			if (multiplyMod(x, i) == 1) {
-				return i;
-			}
-		}
-	}
+        result.append(decoded);
+      }
+    }
 
-	int multiplyMod(int x, int y) {
-		return x * y % M;
-	}
+    return result.toString();
+  }
 
-	void checkValidity(int a) {
-		if (gcd(a, M) != 1) {
-			throw new IllegalArgumentException("Error: keyA and alphabet size must be coprime.");
-		}
-	}
+  int modInverse(int x) {
+    for (int i = 1; ; ++i) {
+      if (multiplyMod(x, i) == 1) {
+        return i;
+      }
+    }
+  }
 
-	int gcd(int x, int y) {
-		return (y == 0) ? x : gcd(y, x % y);
-	}
+  int multiplyMod(int x, int y) {
+    return x * y % M;
+  }
+
+  void checkValidity(int a) {
+    if (gcd(a, M) != 1) {
+      throw new IllegalArgumentException("Error: keyA and alphabet size must be coprime.");
+    }
+  }
+
+  int gcd(int x, int y) {
+    return (y == 0) ? x : gcd(y, x % y);
+  }
 }
--- java/affine-cipher/src/test/java/AffineCipherTest.java
+++ java/affine-cipher/src/test/java/AffineCipherTest.java
@@ -1,110 +1,99 @@
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class AffineCipherTest {
 
-	private AffineCipher affineCipher;
+  private AffineCipher affineCipher = new AffineCipher();
 
-	@Before
-	public void genAffineCipher() {
-		affineCipher = new AffineCipher();
-	}
+  @Test
+  public void testEncodeYes() {
+    assertThat(affineCipher.encode("yes", 5, 7)).isEqualTo("xbt");
+  }
 
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
+  @Test
+  public void testEncodeNo() {
+    assertThat(affineCipher.encode("no", 15, 18)).isEqualTo("fu");
+  }
 
-	@Test
-	public void testEncodeYes() {
-		assertEquals("xbt", affineCipher.encode("yes", 5, 7));
-	}
+  @Test
+  public void testEncodeOMG() {
+    assertThat(affineCipher.encode("OMG", 21, 3)).isEqualTo("lvz");
+  }
 
-	@Test
-	public void testEncodeNo() {
-		assertEquals("fu", affineCipher.encode("no", 15, 18));
-	}
+  @Test
+  public void testEncodeO_M_G() {
+    assertThat(affineCipher.encode("O M G", 25, 47)).isEqualTo("hjp");
+  }
 
-	@Test
-	public void testEncodeOMG() {
-		assertEquals("lvz", affineCipher.encode("OMG", 21, 3));
-	}
+  @Test
+  public void testEncodeMindBlowingly() {
+    assertThat(affineCipher.encode("mindblowingly", 11, 15)).isEqualTo("rzcwa gnxzc dgt");
+  }
 
-	@Test
-	public void testEncodeO_M_G() {
-		assertEquals("hjp", affineCipher.encode("O M G", 25, 47));
-	}
+  @Test
+  public void testEncodeNumbers() {
+    assertThat(affineCipher.encode("Testing,1 2 3, testing.", 3, 4))
+        .isEqualTo("jqgjc rw123 jqgjc rw");
+  }
 
-	@Test
-	public void testEncodeMindBlowingly() {
-		assertEquals("rzcwa gnxzc dgt", affineCipher.encode("mindblowingly", 11, 15));
-	}
+  @Test
+  public void testEncodeDeepThought() {
+    assertThat(affineCipher.encode("Truth is fiction.", 5, 17)).isEqualTo("iynia fdqfb ifje");
+  }
 
-	@Test
-	public void testEncodeNumbers() {
-		assertEquals("jqgjc rw123 jqgjc rw", affineCipher.encode("Testing,1 2 3, testing.", 3, 4));
-	}
+  @Test
+  public void testEncodeAllTheLetters() {
+    assertThat(affineCipher.encode("The quick brown fox jumps over the lazy dog.", 17, 33))
+        .isEqualTo("swxtj npvyk lruol iejdc blaxk swxmh qzglf");
+  }
 
-	@Test
-	public void testEncodeDeepThought() {
-		assertEquals("iynia fdqfb ifje", affineCipher.encode("Truth is fiction.", 5, 17));
-	}
+  @Test
+  public void testEncodeThrowsMeaningfulException() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> affineCipher.encode("This is a test", 6, 17))
+        .withMessage("Error: keyA and alphabet size must be coprime.");
+  }
 
-	@Test
-	public void testEncodeAllTheLetters() {
-		assertEquals("swxtj npvyk lruol iejdc blaxk swxmh qzglf",
-				affineCipher.encode("The quick brown fox jumps over the lazy dog.", 17, 33));
-	}
+  @Test
+  public void testDecodeExercism() {
+    assertThat(affineCipher.decode("tytgn fjr", 3, 7)).isEqualTo("exercism");
+  }
 
-	@Test
-	public void testEncodeThrowsMeaningfulException() throws IllegalArgumentException {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException.expectMessage("Error: keyA and alphabet size must be coprime.");
+  @Test
+  public void testDecodeSentence() {
+    assertThat(affineCipher.decode("qdwju nqcro muwhn odqun oppmd aunwd o", 19, 16))
+        .isEqualTo("anobstacleisoftenasteppingstone");
+  }
 
-		affineCipher.encode("This is a test", 6, 17);
-	}
+  @Test
+  public void testDecodeNumbers() {
+    assertThat(affineCipher.decode("odpoz ub123 odpoz ub", 25, 7)).isEqualTo("testing123testing");
+  }
 
-	@Test
-	public void testDecodeExercism() {
-		assertEquals("exercism", affineCipher.decode("tytgn fjr", 3, 7));
-	}
+  @Test
+  public void testDecodeAllTheLetters() {
+    assertThat(affineCipher.decode("swxtj npvyk lruol iejdc blaxk swxmh qzglf", 17, 33))
+        .isEqualTo("thequickbrownfoxjumpsoverthelazydog");
+  }
 
-	@Test
-	public void testDecodeSentence() {
-		assertEquals("anobstacleisoftenasteppingstone",
-				affineCipher.decode("qdwju nqcro muwhn odqun oppmd aunwd o", 19, 16));
-	}
+  @Test
+  public void testDecodeWithNoSpaces() {
+    assertThat(affineCipher.decode("swxtjnpvyklruoliejdcblaxkswxmhqzglf", 17, 33))
+        .isEqualTo("thequickbrownfoxjumpsoverthelazydog");
+  }
 
-	@Test
-	public void testDecodeNumbers() {
-		assertEquals("testing123testing", affineCipher.decode("odpoz ub123 odpoz ub", 25, 7));
-	}
+  @Test
+  public void testDecodeWithTooManySpaces() {
+    assertThat(affineCipher.decode("vszzm    cly   yd cg    qdp", 15, 16))
+        .isEqualTo("jollygreengiant");
+  }
 
-	@Test
-	public void testDecodeAllTheLetters() {
-		assertEquals("thequickbrownfoxjumpsoverthelazydog",
-				affineCipher.decode("swxtj npvyk lruol iejdc blaxk swxmh qzglf", 17, 33));
-	}
-
-	@Test
-	public void testDecodeWithNoSpaces() {
-		assertEquals("thequickbrownfoxjumpsoverthelazydog",
-				affineCipher.decode("swxtjnpvyklruoliejdcblaxkswxmhqzglf", 17, 33));
-	}
-
-	@Test
-	public void testDecodeWithTooManySpaces() {
-		assertEquals("jollygreengiant", affineCipher.decode("vszzm    cly   yd cg    qdp", 15, 16));
-	}
-
-	@Test
-	public void testDecodeThrowsMeaningfulException() {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException.expectMessage("Error: keyA and alphabet size must be coprime.");
-
-		affineCipher.decode("Test", 13, 5);
-	}
-
+  @Test
+  public void testDecodeThrowsMeaningfulException() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> affineCipher.decode("Test", 13, 5))
+        .withMessage("Error: keyA and alphabet size must be coprime.");
+  }
 }
