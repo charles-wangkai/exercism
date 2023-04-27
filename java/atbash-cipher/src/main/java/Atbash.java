524ca7b21a17015acd7509aa14c81eda08d6c414
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 10:19:34 +0800

    java/atbash-cipher

--- java/atbash-cipher/src/main/java/Atbash.java
+++ java/atbash-cipher/src/main/java/Atbash.java
@@ -1,24 +1,33 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Atbash {
-	String encode(String plaintext) {
-		List<Integer> letters = plaintext.toLowerCase().chars().filter(Character::isLetterOrDigit)
-				.map(ch -> Character.isDigit(ch) ? ch : ('z' - ch + 'a'))
-				.collect(ArrayList<Integer>::new, List::add, List::addAll);
-		StringBuilder ciphertext = new StringBuilder();
-		for (int i = 0; i < letters.size(); i++) {
-			if (i > 0 && i % 5 == 0) {
-				ciphertext.append(" ");
-			}
-			ciphertext.append((char) letters.get(i).intValue());
-		}
-		return ciphertext.toString();
-	}
+  String encode(String plaintext) {
+    List<Character> letters =
+        plaintext
+            .toLowerCase()
+            .chars()
+            .filter(Character::isLetterOrDigit)
+            .mapToObj(c -> (char) (Character.isDigit(c) ? c : ('a' + 'z' - c)))
+            .toList();
 
-	String decode(String ciphertext) {
-		return ciphertext.replace(" ", "").chars()
-				.mapToObj(ch -> (char) (Character.isDigit(ch) ? ch : ('z' - ch + 'a')))
-				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
-	}
+    StringBuilder ciphertext = new StringBuilder();
+    for (int i = 0; i < letters.size(); ++i) {
+      if (i != 0 && i % 5 == 0) {
+        ciphertext.append(" ");
+      }
+      ciphertext.append(letters.get(i));
+    }
+
+    return ciphertext.toString();
+  }
+
+  String decode(String ciphertext) {
+    return ciphertext
+        .replace(" ", "")
+        .chars()
+        .mapToObj(c -> (char) (Character.isDigit(c) ? c : ('a' + 'z' - c)))
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
 }
--- java/atbash-cipher/src/test/java/AtbashTest.java
+++ java/atbash-cipher/src/test/java/AtbashTest.java
@@ -1,85 +1,88 @@
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AtbashTest {
 
-	private Atbash atbash;
+  private Atbash atbash;
 
-	@Before
-	public void setup() {
-		atbash = new Atbash();
-	}
+  @Before
+  public void setup() {
+    atbash = new Atbash();
+  }
 
-	@Test
-	public void testEncodeYes() {
-		assertEquals("bvh", atbash.encode("yes"));
-	}
+  @Test
+  public void testEncodeYes() {
+    assertThat(atbash.encode("yes")).isEqualTo("bvh");
+  }
 
-	@Test
-	public void testEncodeNo() {
-		assertEquals("ml", atbash.encode("no"));
-	}
+  @Test
+  public void testEncodeNo() {
+    assertThat(atbash.encode("no")).isEqualTo("ml");
+  }
 
-	@Test
-	public void testEncodeOmgInCapital() {
-		assertEquals("lnt", atbash.encode("OMG"));
-	}
+  @Test
+  public void testEncodeOmgInCapital() {
+    assertThat(atbash.encode("OMG")).isEqualTo("lnt");
+  }
 
-	@Test
-	public void testEncodeOmgWithSpaces() {
-		assertEquals("lnt", atbash.encode("O M G"));
-	}
+  @Test
+  public void testEncodeOmgWithSpaces() {
+    assertThat(atbash.encode("O M G")).isEqualTo("lnt");
+  }
 
-	@Test
-	public void testEncodeMindBlowingly() {
-		assertEquals("nrmwy oldrm tob", atbash.encode("mindblowingly"));
-	}
+  @Test
+  public void testEncodeMindBlowingly() {
+    assertThat(atbash.encode("mindblowingly")).isEqualTo("nrmwy oldrm tob");
+  }
 
-	@Test
-	public void testEncodeNumbers() {
-		assertEquals("gvhgr mt123 gvhgr mt", atbash.encode("Testing,1 2 3, testing."));
-	}
+  @Test
+  public void testEncodeNumbers() {
+    assertThat(atbash.encode("Testing,1 2 3, testing.")).isEqualTo("gvhgr mt123 gvhgr mt");
+  }
 
-	@Test
-	public void testEncodeDeepThought() {
-		assertEquals("gifgs rhurx grlm", atbash.encode("Truth is fiction."));
-	}
+  @Test
+  public void testEncodeDeepThought() {
+    assertThat(atbash.encode("Truth is fiction.")).isEqualTo("gifgs rhurx grlm");
+  }
 
-	@Test
-	public void testEncodeAllTheLetters() {
-		assertEquals("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt",
-				atbash.encode("The quick brown fox jumps over the lazy dog."));
-	}
+  @Test
+  public void testEncodeAllTheLetters() {
+    assertThat(atbash.encode("The quick brown fox jumps over the lazy dog."))
+        .isEqualTo("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt");
+  }
 
-	@Test
-	public void testDecodeExercism() {
-		assertEquals("exercism", atbash.decode("vcvix rhn"));
-	}
+  @Test
+  public void testDecodeExercism() {
+    assertThat(atbash.decode("vcvix rhn")).isEqualTo("exercism");
+  }
 
-	@Test
-	public void testDecodeASentence() {
-		assertEquals("anobstacleisoftenasteppingstone", atbash.decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"));
-	}
+  @Test
+  public void testDecodeASentence() {
+    assertThat(atbash.decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"))
+        .isEqualTo("anobstacleisoftenasteppingstone");
+  }
 
-	@Test
-	public void testDecodeNumbers() {
-		assertEquals("testing123testing", atbash.decode("gvhgr mt123 gvhgr mt"));
-	}
+  @Test
+  public void testDecodeNumbers() {
+    assertThat(atbash.decode("gvhgr mt123 gvhgr mt")).isEqualTo("testing123testing");
+  }
 
-	@Test
-	public void testDecodeAllTheLetters() {
-		assertEquals("thequickbrownfoxjumpsoverthelazydog", atbash.decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"));
-	}
+  @Test
+  public void testDecodeAllTheLetters() {
+    assertThat(atbash.decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"))
+        .isEqualTo("thequickbrownfoxjumpsoverthelazydog");
+  }
 
-	@Test
-	public void testDecodeWithTooManySpaces() {
-		assertEquals("exercism", atbash.decode("vc vix    r hn"));
-	}
+  @Test
+  public void testDecodeWithTooManySpaces() {
+    assertThat(atbash.decode("vc vix    r hn")).isEqualTo("exercism");
+  }
 
-	@Test
-	public void testDecodeWithNoSpaces() {
-		assertEquals("anobstacleisoftenasteppingstone", atbash.decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv"));
-	}
+  @Test
+  public void testDecodeWithNoSpaces() {
+    assertThat(atbash.decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv"))
+        .isEqualTo("anobstacleisoftenasteppingstone");
+  }
 }
