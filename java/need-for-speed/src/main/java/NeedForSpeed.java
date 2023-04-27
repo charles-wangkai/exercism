4b4eef12b74279946b4dc57120ea8445c6f34e02
Kai Wang <charles.wangkai@gmail.com>
2023-04-27 09:19:36 +0800

    java/need-for-speed

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,40 @@
+class NeedForSpeed {
+  int speed;
+  int batteryDrain;
+  int driveCount;
+
+  public NeedForSpeed(int speed, int batteryDrain) {
+    this.speed = speed;
+    this.batteryDrain = batteryDrain;
+  }
+
+  public boolean batteryDrained() {
+    return batteryDrain * (driveCount + 1) > 100;
+  }
+
+  public int distanceDriven() {
+    return speed * driveCount;
+  }
+
+  public void drive() {
+    if (!batteryDrained()) {
+      ++driveCount;
+    }
+  }
+
+  public static NeedForSpeed nitro() {
+    return new NeedForSpeed(50, 4);
+  }
+}
+
+class RaceTrack {
+  int distance;
+
+  public RaceTrack(int distance) {
+    this.distance = distance;
+  }
+
+  public boolean tryFinishTrack(NeedForSpeed car) {
+    return 100 / car.batteryDrain * car.speed >= distance;
+  }
+}
