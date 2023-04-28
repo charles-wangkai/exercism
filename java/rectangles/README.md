5d6d1845cd0c9092ecb99750ab60ea555f573305
Kai Wang <charles.wangkai@gmail.com>
2023-04-28 23:19:24 +0800

    java/rectangles

--- java/rectangles/README.md
+++ java/rectangles/README.md
@@ -1,5 +1,10 @@
 # Rectangles
 
+Welcome to Rectangles on Exercism's Java Track.
+If you need help running the tests or submitting your code, check out `HELP.md`.
+
+## Instructions
+
 Count the rectangles in an ASCII diagram like the one below.
 
 ```text
@@ -63,30 +68,28 @@ The above diagram contains 6 rectangles:
 You may assume that the input is always a proper rectangle (i.e. the length of
 every line equals the length of the first line).
 
-## Setup
+## Source
 
-Go through the setup instructions for Java to install the necessary
-dependencies:
+### Created by
 
-[https://exercism.io/tracks/java/installation](https://exercism.io/tracks/java/installation)
+- @stkent
 
-# Running the tests
+### Contributed to by
 
-You can run all the tests for an exercise by entering the following in your
-terminal:
-
-```sh
-$ gradle test
-```
-
-> Use `gradlew.bat` if you're on Windows
-
-In the test suites all tests but the first have been skipped.
-
-Once you get a test passing, you can enable the next one by removing the
-`@Ignore("Remove to run test")` annotation.
-
-
-## Submitting Incomplete Solutions
-It's possible to submit an incomplete solution so you can see how others have
-completed the exercise.
+- @aadityakulkarni
+- @FridaTveit
+- @jmrunkle
+- @jtigger
+- @kytrinyx
+- @lemoncurry
+- @mirkoperillo
+- @morrme
+- @msomji
+- @muzimuzhi
+- @redshirt4
+- @SleeplessByte
+- @Smarticles101
+- @sshine
+- @tendaitt
+- @vivshaw
+- @Zaldrick
--- java/rectangles/src/main/java/RectangleCounter.java
+++ java/rectangles/src/main/java/RectangleCounter.java
@@ -1,47 +1,44 @@
+import java.util.stream.IntStream;
+
 public class RectangleCounter {
-	int countRectangles(String[] grid) {
-		int row = grid.length;
-		if (row == 0) {
-			return 0;
-		}
+  int countRectangles(String[] grid) {
+    int row = grid.length;
+    if (row == 0) {
+      return 0;
+    }
 
-		int col = grid[0].length();
+    int col = grid[0].length();
 
-		int rectangleNum = 0;
-		for (int r1 = 0; r1 < row; r1++) {
-			for (int c1 = 0; c1 < col; c1++) {
-				for (int r2 = r1 + 1; r2 < row; r2++) {
-					for (int c2 = c1 + 1; c2 < col; c2++) {
-						if (isRectangle(grid, r1, c1, r2, c2)) {
-							rectangleNum++;
-						}
-					}
-				}
-			}
-		}
-		return rectangleNum;
-	}
+    int result = 0;
+    for (int r1 = 0; r1 < row; ++r1) {
+      for (int c1 = 0; c1 < col; ++c1) {
+        for (int r2 = r1 + 1; r2 < row; ++r2) {
+          for (int c2 = c1 + 1; c2 < col; ++c2) {
+            if (isRectangle(grid, r1, c1, r2, c2)) {
+              ++result;
+            }
+          }
+        }
+      }
+    }
 
-	boolean isRectangle(String[] grid, int r1, int c1, int r2, int c2) {
-		if (grid[r1].charAt(c1) != '+' || grid[r1].charAt(c2) != '+' || grid[r2].charAt(c1) != '+'
-				|| grid[r2].charAt(c2) != '+') {
-			return false;
-		}
+    return result;
+  }
 
-		for (int c = c1 + 1; c < c2; c++) {
-			if ((grid[r1].charAt(c) != '-' && grid[r1].charAt(c) != '+')
-					|| (grid[r2].charAt(c) != '-' && grid[r2].charAt(c) != '+')) {
-				return false;
-			}
-		}
-
-		for (int r = r1 + 1; r < r2; r++) {
-			if ((grid[r].charAt(c1) != '|' && grid[r].charAt(c1) != '+')
-					|| (grid[r].charAt(c2) != '|' && grid[r].charAt(c2) != '+')) {
-				return false;
-			}
-		}
-
-		return true;
-	}
+  boolean isRectangle(String[] grid, int r1, int c1, int r2, int c2) {
+    return grid[r1].charAt(c1) == '+'
+        && grid[r1].charAt(c2) == '+'
+        && grid[r2].charAt(c1) == '+'
+        && grid[r2].charAt(c2) == '+'
+        && IntStream.range(c1 + 1, c2)
+            .allMatch(
+                c ->
+                    "-+".indexOf(grid[r1].charAt(c)) != -1
+                        && "-+".indexOf(grid[r2].charAt(c)) != -1)
+        && IntStream.range(r1 + 1, r2)
+            .allMatch(
+                r ->
+                    "|+".indexOf(grid[r].charAt(c1)) != -1
+                        && "|+".indexOf(grid[r].charAt(c2)) != -1);
+  }
 }
