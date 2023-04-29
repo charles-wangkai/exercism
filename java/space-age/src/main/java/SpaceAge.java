d1d0979cab4a86bb748c98169ef5622f5021f90f
Kai Wang <charles.wangkai@gmail.com>
2023-04-29 22:35:07 +0800

    java/space-age

--- java/space-age/src/main/java/SpaceAge.java
+++ java/space-age/src/main/java/SpaceAge.java
@@ -1,44 +1,43 @@
 class SpaceAge {
-	double seconds;
+  double seconds;
 
-	SpaceAge(double seconds) {
-		this.seconds = seconds;
-	}
+  SpaceAge(double seconds) {
+    this.seconds = seconds;
+  }
 
-	double getSeconds() {
-		return seconds;
-	}
+  double getSeconds() {
+    return seconds;
+  }
 
-	double onEarth() {
-		return seconds / 31557600;
-	}
+  double onEarth() {
+    return seconds / 31557600;
+  }
 
-	double onMercury() {
-		return onEarth() / 0.2408467;
-	}
+  double onMercury() {
+    return onEarth() / 0.2408467;
+  }
 
-	double onVenus() {
-		return onEarth() / 0.61519726;
-	}
+  double onVenus() {
+    return onEarth() / 0.61519726;
+  }
 
-	double onMars() {
-		return onEarth() / 1.8808158;
-	}
+  double onMars() {
+    return onEarth() / 1.8808158;
+  }
 
-	double onJupiter() {
-		return onEarth() / 11.862615;
-	}
+  double onJupiter() {
+    return onEarth() / 11.862615;
+  }
 
-	double onSaturn() {
-		return onEarth() / 29.447498;
-	}
+  double onSaturn() {
+    return onEarth() / 29.447498;
+  }
 
-	double onUranus() {
-		return onEarth() / 84.016846;
-	}
+  double onUranus() {
+    return onEarth() / 84.016846;
+  }
 
-	double onNeptune() {
-		return onEarth() / 164.79132;
-	}
-
+  double onNeptune() {
+    return onEarth() / 164.79132;
+  }
 }
