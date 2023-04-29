388ec9bd199eb04471c738c5874cd3d01a660416
Kai Wang <charles.wangkai@gmail.com>
2023-04-29 22:32:30 +0800

    java/simple-linked-list

--- java/simple-linked-list/src/main/java/SimpleLinkedList.java
+++ java/simple-linked-list/src/main/java/SimpleLinkedList.java
@@ -1,67 +1,68 @@
 import java.util.NoSuchElementException;
 
 public class SimpleLinkedList<T> {
-	Node<T> head;
+  Node<T> head;
 
-	SimpleLinkedList() {
-	}
+  SimpleLinkedList() {}
 
-	SimpleLinkedList(T[] values) {
-		for (int i = values.length - 1; i >= 0; i--) {
-			push(values[i]);
-		}
-	}
+  SimpleLinkedList(T[] values) {
+    for (int i = values.length - 1; i >= 0; --i) {
+      push(values[i]);
+    }
+  }
 
-	int size() {
-		int result = 0;
-		for (Node<T> node = head; node != null; node = node.next) {
-			result++;
-		}
-		return result;
-	}
+  int size() {
+    int result = 0;
+    for (Node<T> node = head; node != null; node = node.next) {
+      ++result;
+    }
 
-	void push(T value) {
-		Node<T> node = new Node<T>(value);
-		node.next = head;
-		head = node;
-	}
+    return result;
+  }
 
-	T pop() {
-		if (head == null) {
-			throw new NoSuchElementException();
-		}
+  void push(T value) {
+    Node<T> node = new Node<>(value);
+    node.next = head;
+    head = node;
+  }
 
-		T result = head.value;
-		head = head.next;
-		return result;
-	}
+  T pop() {
+    if (head == null) {
+      throw new NoSuchElementException();
+    }
 
-	void reverse() {
-		SimpleLinkedList<T> reversed = new SimpleLinkedList<T>();
-		while (head != null) {
-			reversed.push(pop());
-		}
-		head = reversed.head;
-	}
+    T result = head.value;
+    head = head.next;
 
-	@SuppressWarnings("unchecked")
-	<E> E[] asArray(Class<E> c) {
-		Object[] result = new Object[size()];
-		Node<T> node = head;
-		for (int i = 0; i < result.length; i++) {
-			result[i] = node.value;
+    return result;
+  }
 
-			node = node.next;
-		}
-		return (E[]) result;
-	}
+  void reverse() {
+    SimpleLinkedList<T> reversed = new SimpleLinkedList<>();
+    while (head != null) {
+      reversed.push(pop());
+    }
+    head = reversed.head;
+  }
+
+  @SuppressWarnings("unchecked")
+  <E> E[] asArray(Class<E> c) {
+    Object[] result = new Object[size()];
+    Node<T> node = head;
+    for (int i = 0; i < result.length; ++i) {
+      result[i] = node.value;
+      node = node.next;
+    }
+
+    return (E[]) result;
+  }
 }
 
 class Node<T> {
-	T value;
-	Node<T> next;
+  T value;
+  Node<T> next;
 
-	Node(T value) {
-		this.value = value;
-	}
+  Node(T value) {
+    this.value = value;
+  }
 }
--- java/simple-linked-list/src/test/java/SimpleLinkedListTest.java
+++ java/simple-linked-list/src/test/java/SimpleLinkedListTest.java
@@ -1,82 +1,75 @@
-import static org.hamcrest.CoreMatchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
 
 import java.util.NoSuchElementException;
-
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class SimpleLinkedListTest {
 
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
+  @Test
+  public void aNewListIsEmpty() {
+    SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+    assertThat(list.size()).isEqualTo(0);
+  }
 
-	@Test
-	public void aNewListIsEmpty() {
-		SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
-		assertThat(list.size(), is(0));
-	}
+  @Test
+  public void canCreateFromArray() {
+    Character[] values = new Character[] {'1', '2', '3'};
+    SimpleLinkedList<Character> list = new SimpleLinkedList<Character>(values);
+    assertThat(list.size()).isEqualTo(3);
+  }
 
-	@Test
-	public void canCreateFromArray() {
-		Character[] values = new Character[] { '1', '2', '3' };
-		SimpleLinkedList<Character> list = new SimpleLinkedList<Character>(values);
-		assertThat(list.size(), is(3));
-	}
+  @Test
+  public void popOnEmptyListWillThrow() {
+    SimpleLinkedList<String> list = new SimpleLinkedList<String>();
 
-	@Test
-	public void popOnEmptyListWillThrow() {
-		expectedException.expect(NoSuchElementException.class);
-		SimpleLinkedList<String> list = new SimpleLinkedList<String>();
-		list.pop();
-	}
+    assertThrows(NoSuchElementException.class, list::pop);
+  }
 
-	@Test
-	public void popReturnsLastAddedElement() {
-		SimpleLinkedList<Integer> list = new SimpleLinkedList<Integer>();
-		list.push(9);
-		list.push(8);
-		assertThat(list.size(), is(2));
-		assertThat(list.pop(), is(8));
-		assertThat(list.pop(), is(9));
-		assertThat(list.size(), is(0));
-	}
+  @Test
+  public void popReturnsLastAddedElement() {
+    SimpleLinkedList<Integer> list = new SimpleLinkedList<Integer>();
+    list.push(9);
+    list.push(8);
+    assertThat(list.size()).isEqualTo(2);
+    assertThat(list.pop()).isEqualTo(8);
+    assertThat(list.pop()).isEqualTo(9);
+    assertThat(list.size()).isEqualTo(0);
+  }
 
-	@Test
-	public void reverseReversesList() {
-		SimpleLinkedList<String> list = new SimpleLinkedList<String>();
-		list.push("9");
-		list.push("8");
-		list.push("7");
-		list.push("6");
-		list.push("5");
-		list.reverse();
-		assertThat(list.pop(), is("9"));
-		assertThat(list.pop(), is("8"));
-		assertThat(list.pop(), is("7"));
-		assertThat(list.pop(), is("6"));
-		assertThat(list.pop(), is("5"));
-	}
+  @Test
+  public void reverseReversesList() {
+    SimpleLinkedList<String> list = new SimpleLinkedList<String>();
+    list.push("9");
+    list.push("8");
+    list.push("7");
+    list.push("6");
+    list.push("5");
+    list.reverse();
+    assertThat(list.pop()).isEqualTo("9");
+    assertThat(list.pop()).isEqualTo("8");
+    assertThat(list.pop()).isEqualTo("7");
+    assertThat(list.pop()).isEqualTo("6");
+    assertThat(list.pop()).isEqualTo("5");
+  }
 
-	@Test
-	public void canReturnListAsArray() {
-		SimpleLinkedList<Character> list = new SimpleLinkedList<Character>();
-		list.push('9');
-		list.push('8');
-		list.push('7');
-		list.push('6');
-		list.push('5');
-		Character[] expected = { '5', '6', '7', '8', '9' };
-		assertArrayEquals(expected, list.asArray(Character.class));
-	}
+  @Test
+  public void canReturnListAsArray() {
+    SimpleLinkedList<Character> list = new SimpleLinkedList<Character>();
+    list.push('9');
+    list.push('8');
+    list.push('7');
+    list.push('6');
+    list.push('5');
+    Character[] expected = {'5', '6', '7', '8', '9'};
+    assertArrayEquals(expected, list.asArray(Character.class));
+  }
 
-	@Test
-	public void canReturnEmptyListAsEmptyArray() {
-		SimpleLinkedList<Object> list = new SimpleLinkedList<Object>();
-		Object[] expected = {};
-		assertArrayEquals(expected, list.asArray(Object.class));
-	}
-
+  @Test
+  public void canReturnEmptyListAsEmptyArray() {
+    SimpleLinkedList<Object> list = new SimpleLinkedList<Object>();
+    Object[] expected = {};
+    assertArrayEquals(expected, list.asArray(Object.class));
+  }
 }
