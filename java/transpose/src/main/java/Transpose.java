dee4c814ce8883b2d8a405b8a46cc8bf5a7af73d
Kai Wang <charles.wangkai@gmail.com>
2023-04-30 10:10:54 +0800

    java/transpose

--- java/transpose/src/main/java/Transpose.java
+++ java/transpose/src/main/java/Transpose.java
@@ -1,30 +1,34 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Transpose {
-	String transpose(String s) {
-		String[] lines = s.split("\n");
+  String transpose(String s) {
+    String[] lines = s.split("\n");
 
-		int row = lines.length;
-		int col = Arrays.stream(lines).mapToInt(String::length).max().getAsInt();
+    int row = lines.length;
+    int col = Arrays.stream(lines).mapToInt(String::length).max().getAsInt();
 
-		Character[][] transposed = new Character[col][row];
+    char[][] transposed = new char[col][row];
+    for (int i = 0; i < lines.length; ++i) {
+      for (int j = 0; j < lines[i].length(); ++j) {
+        transposed[j][i] = lines[i].charAt(j);
+      }
+    }
 
-		for (int i = 0; i < lines.length; i++) {
-			for (int j = 0; j < lines[i].length(); j++) {
-				transposed[j][i] = lines[i].charAt(j);
-			}
-		}
+    return Arrays.stream(transposed)
+        .map(
+            line -> {
+              int lastIndex = line.length - 1;
+              while (lastIndex != -1 && line[lastIndex] == 0) {
+                --lastIndex;
+              }
 
-		return String.join("\n", Arrays.stream(transposed).map(this::convert).collect(Collectors.toList()));
-	}
-
-	String convert(Character[] r) {
-		int lastIndex = r.length - 1;
-		while (lastIndex >= 0 && r[lastIndex] == null) {
-			lastIndex--;
-		}
-		return Arrays.stream(r, 0, lastIndex + 1).map(ch -> ch == null ? ' ' : ch)
-				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
-	}
+              return IntStream.range(0, lastIndex + 1)
+                  .mapToObj(i -> (line[i] == 0) ? ' ' : line[i])
+                  .map(String::valueOf)
+                  .collect(Collectors.joining());
+            })
+        .collect(Collectors.joining("\n"));
+  }
 }
--- java/transpose/src/test/java/TransposeTest.java
+++ java/transpose/src/test/java/TransposeTest.java
@@ -1,116 +1,86 @@
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TransposeTest {
-	private Transpose transpose;
+  private Transpose transpose;
 
-	@Before
-	public void setup() {
-		transpose = new Transpose();
-	}
+  @Before
+  public void setup() {
+    transpose = new Transpose();
+  }
 
-	@Test
-	public void emptyString() {
-		String input = "";
+  @Test
+  public void emptyString() {
+    assertThat(transpose.transpose("")).isEqualTo("");
+  }
 
-		String expected = "";
+  @Test
+  public void twoCharactersInARow() {
+    assertThat(transpose.transpose("A1")).isEqualTo("A" + "\n1");
+  }
 
-		assertEquals(expected, transpose.transpose(input));
-	}
+  @Test
+  public void twoCharactersInAColumn() {
+    assertThat(transpose.transpose("A\n" + "1")).isEqualTo("A1");
+  }
 
-	@Test
-	public void twoCharactersInARow() {
-		String input = "A1";
+  @Test
+  public void simple() {
+    assertThat(transpose.transpose("ABC\n" + "123")).isEqualTo("A1\n" + "B2\n" + "C3");
+  }
 
-		String expected = "A" + "\n1";
+  @Test
+  public void singleLine() {
+    assertThat(transpose.transpose("Single line."))
+        .isEqualTo(
+            "S\n" + "i\n" + "n\n" + "g\n" + "l\n" + "e\n" + " \n" + "l\n" + "i\n" + "n\n" + "e\n"
+                + ".");
+  }
 
-		assertEquals(expected, transpose.transpose(input));
-	}
+  @Test
+  public void firstLineLongerThanSecondLine() {
+    assertThat(transpose.transpose("The fourth line.\n" + "The fifth line."))
+        .isEqualTo(
+            "TT\n" + "hh\n" + "ee\n" + "  \n" + "ff\n" + "oi\n" + "uf\n" + "rt\n" + "th\n" + "h \n"
+                + " l\n" + "li\n" + "in\n" + "ne\n" + "e.\n" + ".");
+  }
 
-	@Test
-	public void twoCharactersInAColumn() {
-		String input = "A\n" + "1";
+  @Test
+  public void secondLineLongerThanFirstLine() {
+    assertThat(transpose.transpose("The first line.\n" + "The second line."))
+        .isEqualTo(
+            "TT\n" + "hh\n" + "ee\n" + "  \n" + "fs\n" + "ie\n" + "rc\n" + "so\n" + "tn\n" + " d\n"
+                + "l \n" + "il\n" + "ni\n" + "en\n" + ".e\n" + " .");
+  }
 
-		String expected = "A1";
+  @Test
+  public void mixedLineLength() {
+    assertThat(
+            transpose.transpose(
+                "The longest line.\n" + "A long line.\n" + "A longer line.\n" + "A line."))
+        .isEqualTo(
+            "TAAA\n" + "h   \n" + "elll\n" + " ooi\n" + "lnnn\n" + "ogge\n" + "n e.\n" + "glr\n"
+                + "ei \n" + "snl\n" + "tei\n" + " .n\n" + "l e\n" + "i .\n" + "n\n" + "e\n" + ".");
+  }
 
-		assertEquals(expected, transpose.transpose(input));
-	}
+  @Test
+  public void square() {
+    assertThat(transpose.transpose("HEART\n" + "EMBER\n" + "ABUSE\n" + "RESIN\n" + "TREND"))
+        .isEqualTo("HEART\n" + "EMBER\n" + "ABUSE\n" + "RESIN\n" + "TREND");
+  }
 
-	@Test
-	public void simple() {
-		String input = "ABC\n" + "123";
+  @Test
+  public void rectangle() {
+    assertThat(transpose.transpose("FRACTURE\n" + "OUTLINED\n" + "BLOOMING\n" + "SEPTETTE"))
+        .isEqualTo(
+            "FOBS\n" + "RULE\n" + "ATOP\n" + "CLOT\n" + "TIME\n" + "UNIT\n" + "RENT\n" + "EDGE");
+  }
 
-		String expected = "A1\n" + "B2\n" + "C3";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void singleLine() {
-		String input = "Single line.";
-
-		String expected = "S\n" + "i\n" + "n\n" + "g\n" + "l\n" + "e\n" + " \n" + "l\n" + "i\n" + "n\n" + "e\n" + ".";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void firstLineLongerThanSecondLine() {
-		String input = "The fourth line.\n" + "The fifth line.";
-
-		String expected = "TT\n" + "hh\n" + "ee\n" + "  \n" + "ff\n" + "oi\n" + "uf\n" + "rt\n" + "th\n" + "h \n"
-				+ " l\n" + "li\n" + "in\n" + "ne\n" + "e.\n" + ".";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void secondLineLongerThanFirstLine() {
-		String input = "The first line.\n" + "The second line.";
-
-		String expected = "TT\n" + "hh\n" + "ee\n" + "  \n" + "fs\n" + "ie\n" + "rc\n" + "so\n" + "tn\n" + " d\n"
-				+ "l \n" + "il\n" + "ni\n" + "en\n" + ".e\n" + " .";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void mixedLineLength() {
-		String input = "The longest line.\n" + "A long line.\n" + "A longer line.\n" + "A line.";
-
-		String expected = "TAAA\n" + "h   \n" + "elll\n" + " ooi\n" + "lnnn\n" + "ogge\n" + "n e.\n" + "glr\n" + "ei \n"
-				+ "snl\n" + "tei\n" + " .n\n" + "l e\n" + "i .\n" + "n\n" + "e\n" + ".";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void square() {
-		String input = "HEART\n" + "EMBER\n" + "ABUSE\n" + "RESIN\n" + "TREND";
-
-		String expected = "HEART\n" + "EMBER\n" + "ABUSE\n" + "RESIN\n" + "TREND";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void rectangle() {
-		String input = "FRACTURE\n" + "OUTLINED\n" + "BLOOMING\n" + "SEPTETTE";
-
-		String expected = "FOBS\n" + "RULE\n" + "ATOP\n" + "CLOT\n" + "TIME\n" + "UNIT\n" + "RENT\n" + "EDGE";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
-	@Test
-	public void triangle() {
-		String input = "T\n" + "EE\n" + "AAA\n" + "SSSS\n" + "EEEEE\n" + "RRRRRR";
-
-		String expected = "TEASER\n" + " EASER\n" + "  ASER\n" + "   SER\n" + "    ER\n" + "     R";
-
-		assertEquals(expected, transpose.transpose(input));
-	}
-
+  @Test
+  public void triangle() {
+    assertThat(transpose.transpose("T\n" + "EE\n" + "AAA\n" + "SSSS\n" + "EEEEE\n" + "RRRRRR"))
+        .isEqualTo("TEASER\n" + " EASER\n" + "  ASER\n" + "   SER\n" + "    ER\n" + "     R");
+  }
 }
