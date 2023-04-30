cd1b03d2b7143f948d1328e41a46d60b3f7cf4b6
Kai Wang <charles.wangkai@gmail.com>
2023-04-30 10:39:04 +0800

    java/two-bucket

--- java/two-bucket/README.md
+++ java/two-bucket/README.md
@@ -1,5 +1,10 @@
 # Two Bucket
 
+Welcome to Two Bucket on Exercism's Java Track.
+If you need help running the tests or submitting your code, check out `HELP.md`.
+
+## Instructions
+
 Given two buckets of different size, demonstrate how to measure an exact number of liters by strategically transferring liters of fluid between the buckets.
 
 Since this mathematical problem is fairly subject to interpretation / individual approach, the tests have been written specifically to expect one overarching solution.
@@ -23,39 +28,30 @@ Example:
 Bucket one can hold up to 7 liters, and bucket two can hold up to 11 liters. Let's say bucket one, at a given step, is holding 7 liters, and bucket two is holding 8 liters (7,8). If you empty bucket one and make no change to bucket two, leaving you with 0 liters and 8 liters respectively (0,8), that counts as one "move". Instead, if you had poured from bucket one into bucket two until bucket two was full, leaving you with 4 liters in bucket one and 11 liters in bucket two (4,11), that would count as only one "move" as well.
 
 To conclude, the only valid moves are:
-- pouring from one bucket to another
-- emptying one bucket and doing nothing to the other
-- filling one bucket and doing nothing to the other
+- pouring from either bucket to another
+- emptying either bucket and doing nothing to the other
+- filling either bucket and doing nothing to the other
 
 Written with <3 at [Fullstack Academy](http://www.fullstackacademy.com/) by Lindsay Levine.
 
-## Setup
-
-Go through the setup instructions for Java to install the necessary
-dependencies:
-
-[https://exercism.io/tracks/java/installation](https://exercism.io/tracks/java/installation)
-
-# Running the tests
-
-You can run all the tests for an exercise by entering the following in your
-terminal:
-
-```sh
-$ gradle test
-```
-
-> Use `gradlew.bat` if you're on Windows
-
-In the test suites all tests but the first have been skipped.
-
-Once you get a test passing, you can enable the next one by removing the
-`@Ignore("Remove to run test")` annotation.
-
 ## Source
 
-Water Pouring Problem [http://demonstrations.wolfram.com/WaterPouringProblem/](http://demonstrations.wolfram.com/WaterPouringProblem/)
+### Created by
 
-## Submitting Incomplete Solutions
-It's possible to submit an incomplete solution so you can see how others have
-completed the exercise.
+- @jssander
+
+### Contributed to by
+
+- @FridaTveit
+- @jmrunkle
+- @lemoncurry
+- @mirkoperillo
+- @msomji
+- @muzimuzhi
+- @sjwarner-bp
+- @SleeplessByte
+- @sshine
+
+### Based on
+
+Water Pouring Problem - http://demonstrations.wolfram.com/WaterPouringProblem/
--- java/two-bucket/src/test/java/TwoBucketTest.java
+++ java/two-bucket/src/test/java/TwoBucketTest.java
@@ -4,69 +4,63 @@ import org.junit.Test;
 
 public class TwoBucketTest {
 
-	@Test
-	public void testBucketOneSizeThreeBucketTwoSizeFiveStartWithOne() {
+  @Test
+  public void testBucketOneSizeThreeBucketTwoSizeFiveStartWithOne() {
 
-		TwoBucket twoBucket = new TwoBucket(3, 5, 1, "one");
+    TwoBucket twoBucket = new TwoBucket(3, 5, 1, "one");
 
-		assertEquals(4, twoBucket.getTotalMoves());
-		assertEquals("one", twoBucket.getFinalBucket());
-		assertEquals(5, twoBucket.getOtherBucket());
+    assertEquals(4, twoBucket.getTotalMoves());
+    assertEquals("one", twoBucket.getFinalBucket());
+    assertEquals(5, twoBucket.getOtherBucket());
+  }
 
-	}
+  @Test
+  public void testBucketOneSizeThreeBucketTwoSizeFiveStartWithTwo() {
 
-	@Test
-	public void testBucketOneSizeThreeBucketTwoSizeFiveStartWithTwo() {
+    TwoBucket twoBucket = new TwoBucket(3, 5, 1, "two");
 
-		TwoBucket twoBucket = new TwoBucket(3, 5, 1, "two");
+    assertEquals(8, twoBucket.getTotalMoves());
+    assertEquals("two", twoBucket.getFinalBucket());
+    assertEquals(3, twoBucket.getOtherBucket());
+  }
 
-		assertEquals(8, twoBucket.getTotalMoves());
-		assertEquals("two", twoBucket.getFinalBucket());
-		assertEquals(3, twoBucket.getOtherBucket());
+  @Test
+  public void testBucketOneSizeSevenBucketTwoSizeElevenStartWithOne() {
 
-	}
+    TwoBucket twoBucket = new TwoBucket(7, 11, 2, "one");
 
-	@Test
-	public void testBucketOneSizeSevenBucketTwoSizeElevenStartWithOne() {
+    assertEquals(14, twoBucket.getTotalMoves());
+    assertEquals("one", twoBucket.getFinalBucket());
+    assertEquals(11, twoBucket.getOtherBucket());
+  }
 
-		TwoBucket twoBucket = new TwoBucket(7, 11, 2, "one");
+  @Test
+  public void testBucketOneSizeSevenBucketTwoSizeElevenStartWithTwo() {
 
-		assertEquals(14, twoBucket.getTotalMoves());
-		assertEquals("one", twoBucket.getFinalBucket());
-		assertEquals(11, twoBucket.getOtherBucket());
+    TwoBucket twoBucket = new TwoBucket(7, 11, 2, "two");
 
-	}
+    assertEquals(18, twoBucket.getTotalMoves());
+    assertEquals("two", twoBucket.getFinalBucket());
+    assertEquals(7, twoBucket.getOtherBucket());
+  }
 
-	@Test
-	public void testBucketOneSizeSevenBucketTwoSizeElevenStartWithTwo() {
+  @Test
+  public void testBucketOneSizeOneBucketTwoSizeThreeStartWithTwo() {
 
-		TwoBucket twoBucket = new TwoBucket(7, 11, 2, "two");
+    TwoBucket twoBucket = new TwoBucket(1, 3, 3, "two");
 
-		assertEquals(18, twoBucket.getTotalMoves());
-		assertEquals("two", twoBucket.getFinalBucket());
-		assertEquals(7, twoBucket.getOtherBucket());
+    assertEquals(1, twoBucket.getTotalMoves());
+    assertEquals("two", twoBucket.getFinalBucket());
+    assertEquals(0, twoBucket.getOtherBucket());
+  }
 
-	}
+  @Test
+  public void testBucketOneSizeTwoBucketTwoSizeThreeStartWithOne() {
 
-	@Test
-	public void testBucketOneSizeOneBucketTwoSizeThreeStartWithTwo() {
+    TwoBucket twoBucket = new TwoBucket(2, 3, 3, "one");
 
-		TwoBucket twoBucket = new TwoBucket(1, 3, 3, "two");
-
-		assertEquals(1, twoBucket.getTotalMoves());
-		assertEquals("two", twoBucket.getFinalBucket());
-		assertEquals(0, twoBucket.getOtherBucket());
-
-	}
-
-	@Test
-	public void testBucketOneSizeTwoBucketTwoSizeThreeStartWithOne() {
-
-		TwoBucket twoBucket = new TwoBucket(2, 3, 3, "one");
-
-		assertEquals(2, twoBucket.getTotalMoves());
-		assertEquals("two", twoBucket.getFinalBucket());
-		assertEquals(2, twoBucket.getOtherBucket());
-
-	}
+    assertEquals(2, twoBucket.getTotalMoves());
+    assertEquals("two", twoBucket.getFinalBucket());
+    assertEquals(2, twoBucket.getOtherBucket());
+  }
 }
