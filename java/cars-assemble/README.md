bb412d94379cfe21b199867bd58d63c791bad2a5
Kai Wang <charles.wangkai@gmail.com>
2025-01-21 12:18:04 +0800

    java/cars-assemble

--- java/cars-assemble/README.md
+++ java/cars-assemble/README.md
@@ -6,37 +6,89 @@ If you get stuck on the exercise, check out `HINTS.md`, but try and solve it wit
 
 ## Introduction
 
+## Numbers
+
 There are two different types of numbers in Java:
 
-- Integers: numbers with no digits behind the decimal separator (whole numbers). Examples are `-6`, `0`, `1`, `25`, `976` and `-500000`.
-- Floating-point numbers: numbers with zero or more digits behind the decimal separator. Examples are `-20.4`, `0.1`, `2.72`, `16.984025` and `1024.0`.
+- Integers: numbers with no digits behind the decimal separator (whole numbers).
+  Examples are `-6`, `0`, `1`, `25`, `976` and `-500000`.
+- Floating-point numbers: numbers with zero or more digits behind the decimal separator.
+  Examples are `-20.4`, `0.1`, `2.72`, `16.984025` and `1024.0`.
 
-The two most common numeric types in Java are `int` and `double`. An `int` is a 32-bit integer and a `double` is a 64-bit floating-point number.
+The two most common numeric types in Java are `int` and `double`.
+An `int` is a 32-bit integer and a `double` is a 64-bit floating-point number.
 
-Arithmetic is done using the standard arithmetic operators. Numbers can be compared using the standard numeric comparison operators and the equality (`==`) and inequality (`!=`) operators.
+Arithmetic is done using the standard arithmetic operators.
+Numbers can be compared using the standard numeric comparison operators (eg. `5 > 4` and `4 <= 5`) and the equality (`==`) and inequality (`!=`) operators.
 
 Java has two types of numeric conversions:
 
 1. Implicit conversions: no data will be lost and no additional syntax is required.
 2. Explicit conversions: data could be lost and additional syntax in the form of a _cast_ is required.
 
-As an `int` has less precision than a `double`, converting from an `int` to a `double` is safe and is thus an implicit conversion. However, converting from a `double` to an `int` could mean losing data, so that requires an explicit conversion.
+As an `int` has less precision than a `double`, converting from an `int` to a `double` is safe and is thus an implicit conversion.
+However, converting from a `double` to an `int` could mean losing data, so that requires an explicit conversion.
 
-In this exercise you must conditionally execute logic. The most common way to do this in Java is by using an `if/else` statement:
+## If-Else Statements
+
+### The _if-then_ statement
+
+The most basic control flow statement in Java is the _if-then_ statement.
+This statement is used to only execute a section of code if a particular condition is `true`.
+An _if-then_ statement is defined using the `if` clause:
 
 ```java
-int x = 6;
-
-if (x == 5) {
-    // Execute logic if x equals 5
-} else if (x > 7) {
-    // Execute logic if x greater than 7
-} else {
-    // Execute logic in all other cases
+class Car {
+    void drive() {
+        // the "if" clause: the car needs to have fuel left to drive
+        if (fuel > 0) {
+            // the "then" clause: the car drives, consuming fuel
+            fuel--;
+        }
+    }
 }
 ```
 
-The condition of an `if` statement must be of type `boolean`. Java has no concept of _truthy_ values.
+In the above example, if the car is out of fuel, calling the `Car.drive` method will do nothing.
+
+### The _if-then-else_ statement
+
+The _if-then-else_ statement provides an alternative path of execution for when the condition in the `if` clause evaluates to `false`.
+This alternative path of execution follows an `if` clause and is defined using the `else` clause:
+
+```java
+class Car {
+    void drive() {
+        if (fuel > 0) {
+            fuel--;
+        } else {
+            stop();
+        }
+    }
+}
+```
+
+In the above example, if the car is out of fuel, calling the `Car.drive` method will call another method to stop the car.
+
+The _if-then-else_ statement also supports multiple conditions by using the `else if` clause:
+
+```java
+class Car {
+    void drive() {
+        if (fuel > 5) {
+            fuel--;
+        } else if (fuel > 0) {
+            turnOnFuelLight();
+            fuel--;
+        } else {
+            stop();
+        }
+    }
+}
+```
+
+In the above example, driving the car when the fuel is less then or equal to `5` will drive the car, but it will turn on the fuel light.
+When the fuel reaches `0`, the car will stop driving.
 
 ## Instructions
 
--- java/cars-assemble/src/test/java/CarsAssembleTest.java
+++ java/cars-assemble/src/test/java/CarsAssembleTest.java
@@ -1,7 +1,10 @@
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.within;
 
 
 public class CarsAssembleTest {
@@ -9,67 +12,91 @@ public class CarsAssembleTest {
     private CarsAssemble carsAssemble;
     private double epsilon = 0.0000001d;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         carsAssemble = new CarsAssemble();
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The productionRatePerHour method returns the correct result when line's speed is 0")
     public void productionRatePerHourForSpeedZero() {
-        assertThat(Math.abs(carsAssemble.productionRatePerHour(0) - 0.0) < epsilon).isTrue();
+        assertThat(carsAssemble.productionRatePerHour(0)).isCloseTo(0.0, within(epsilon));
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The productionRatePerHour method returns the correct result when line's speed is 1")
     public void productionRatePerHourForSpeedOne() {
-        assertThat(Math.abs(carsAssemble.productionRatePerHour(1) - 221.0) < epsilon).isTrue();
+        assertThat(carsAssemble.productionRatePerHour(1)).isCloseTo(221.0, within(epsilon));
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The productionRatePerHour method returns the correct result when line's speed is 4")
     public void productionRatePerHourForSpeedFour() {
-        assertThat(Math.abs(carsAssemble.productionRatePerHour(4) - 884.0) < epsilon).isTrue();
+        assertThat(carsAssemble.productionRatePerHour(4)).isCloseTo(884.0, within(epsilon));
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The productionRatePerHour method returns the correct result when line's speed is 7")
     public void productionRatePerHourForSpeedSeven() {
-        assertThat(Math.abs(carsAssemble.productionRatePerHour(7) - 1392.3) < epsilon).isTrue();
+        assertThat(carsAssemble.productionRatePerHour(7)).isCloseTo(1392.3, within(epsilon));
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The productionRatePerHour method returns the correct result when line's speed is 9")
     public void productionRatePerHourForSpeedNine() {
-        assertThat(Math.abs(carsAssemble.productionRatePerHour(9) - 1591.2) < epsilon).isTrue();
+        assertThat(carsAssemble.productionRatePerHour(9)).isCloseTo(1591.2, within(epsilon));
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The productionRatePerHour method returns the correct result when line's speed is 10")
     public void productionRatePerHourForSpeedTen() {
-        assertThat(Math.abs(carsAssemble.productionRatePerHour(10) - 1701.7) < epsilon).isTrue();
+        assertThat(carsAssemble.productionRatePerHour(10)).isCloseTo(1701.7, within(epsilon));
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The workingItemsPerMinute should be 0 when line's speed is 0")
     public void workingItemsPerMinuteForSpeedZero() {
         assertThat(carsAssemble.workingItemsPerMinute(0)).isEqualTo(0);
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The workingItemsPerMinute should be 3 when line's speed is 1")
     public void workingItemsPerMinuteForSpeedOne() {
         assertThat(carsAssemble.workingItemsPerMinute(1)).isEqualTo(3);
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The workingItemsPerMinute should be 16 when line's speed is 5")
     public void workingItemsPerMinuteForSpeedFive() {
         assertThat(carsAssemble.workingItemsPerMinute(5)).isEqualTo(16);
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The workingItemsPerMinute should be 26 when line's speed is 8")
     public void workingItemsPerMinuteForSpeedEight() {
         assertThat(carsAssemble.workingItemsPerMinute(8)).isEqualTo(26);
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The workingItemsPerMinute should be 26 when line's speed is 9")
     public void workingItemsPerMinuteForSpeedNine() {
         assertThat(carsAssemble.workingItemsPerMinute(9)).isEqualTo(26);
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The workingItemsPerMinute should be 28 when line's speed is 10")
     public void workingItemsPerMinuteForSpeedTen() {
         assertThat(carsAssemble.workingItemsPerMinute(10)).isEqualTo(28);
     }
