f172047da76b24feaeb9ab2c6d6aa9892db03caa
Kai Wang <charles.wangkai@gmail.com>
2025-01-26 15:44:46 +0800

    java/space-age

--- java/space-age/src/test/java/SpaceAgeTest.java
+++ java/space-age/src/test/java/SpaceAgeTest.java
@@ -1,6 +1,7 @@
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class SpaceAgeTest {
 
@@ -10,55 +11,55 @@ public class SpaceAgeTest {
   public void ageOnEarth() {
     SpaceAge age = new SpaceAge(1000000000);
 
-    assertEquals(31.69, age.onEarth(), MAXIMUM_DELTA);
+    assertThat(age.onEarth()).isEqualTo(31.69, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnMercury() {
     SpaceAge age = new SpaceAge(2134835688);
 
-    assertEquals(280.88, age.onMercury(), MAXIMUM_DELTA);
+    assertThat(age.onMercury()).isEqualTo(280.88, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnVenus() {
     SpaceAge age = new SpaceAge(189839836);
 
-    assertEquals(9.78, age.onVenus(), MAXIMUM_DELTA);
+    assertThat(age.onVenus()).isEqualTo(9.78, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnMars() {
     SpaceAge age = new SpaceAge(2129871239L);
 
-    assertEquals(35.88, age.onMars(), MAXIMUM_DELTA);
+    assertThat(age.onMars()).isEqualTo(35.88, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnJupiter() {
     SpaceAge age = new SpaceAge(901876382);
 
-    assertEquals(2.41, age.onJupiter(), MAXIMUM_DELTA);
+    assertThat(age.onJupiter()).isEqualTo(2.41, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnSaturn() {
     SpaceAge age = new SpaceAge(2000000000L);
 
-    assertEquals(2.15, age.onSaturn(), MAXIMUM_DELTA);
+    assertThat(age.onSaturn()).isEqualTo(2.15, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnUranus() {
     SpaceAge age = new SpaceAge(1210123456L);
 
-    assertEquals(0.46, age.onUranus(), MAXIMUM_DELTA);
+    assertThat(age.onUranus()).isEqualTo(0.46, offset(MAXIMUM_DELTA));
   }
 
   @Test
   public void ageOnNeptune() {
     SpaceAge age = new SpaceAge(1821023456L);
 
-    assertEquals(0.35, age.onNeptune(), MAXIMUM_DELTA);
+    assertThat(age.onNeptune()).isEqualTo(0.35, offset(MAXIMUM_DELTA));
   }
 }
