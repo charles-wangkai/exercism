15ca0c0eb3eba9c1fcc7d23d8e8d2c6484c9e14f
Kai Wang <charles.wangkai@gmail.com>
2025-01-26 15:01:15 +0800

    java/pangram

--- java/pangram/src/test/java/PangramCheckerTest.java
+++ java/pangram/src/test/java/PangramCheckerTest.java
@@ -1,66 +1,67 @@
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PangramCheckerTest {
 
   private PangramChecker pangramChecker;
 
-  @Before
+  @BeforeEach
   public void setup() {
     pangramChecker = new PangramChecker();
   }
 
   @Test
   public void emptySentenceIsNotPangram() {
-    assertFalse(pangramChecker.isPangram(""));
+    assertThat(pangramChecker.isPangram("")).isFalse();
   }
 
   @Test
   public void perfectLowerCasePhraseIsPangram() {
-    assertTrue(pangramChecker.isPangram("abcdefghijklmnopqrstuvwxyz"));
+    assertThat(pangramChecker.isPangram("abcdefghijklmnopqrstuvwxyz")).isTrue();
   }
 
   @Test
   public void phraseWithOnlyLowerCaseIsPangram() {
-    assertTrue(pangramChecker.isPangram("the quick brown fox jumps over the lazy dog"));
+    assertThat(pangramChecker.isPangram("the quick brown fox jumps over the lazy dog")).isTrue();
   }
 
   @Test
   public void phraseMissingCharacterXIsNotPangram() {
-    assertFalse(
-        pangramChecker.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
+    assertThat(
+            pangramChecker.isPangram("a quick movement of the enemy will jeopardize five gunboats"))
+        .isFalse();
   }
 
   @Test
   public void phraseMissingCharacterHIsNotPangram() {
-    assertFalse(pangramChecker.isPangram("five boxing wizards jump quickly at it"));
+    assertThat(pangramChecker.isPangram("five boxing wizards jump quickly at it")).isFalse();
   }
 
   @Test
   public void phraseWithUnderscoresIsPangram() {
-    assertTrue(pangramChecker.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
+    assertThat(pangramChecker.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog")).isTrue();
   }
 
   @Test
   public void phraseWithNumbersIsPangram() {
-    assertTrue(pangramChecker.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
+    assertThat(pangramChecker.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"))
+        .isTrue();
   }
 
   @Test
   public void phraseWithMissingLettersReplacedByNumbersIsNotPangram() {
-    assertFalse(pangramChecker.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
+    assertThat(pangramChecker.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")).isFalse();
   }
 
   @Test
   public void phraseWithMixedCaseAndPunctuationIsPangram() {
-    assertTrue(pangramChecker.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
+    assertThat(pangramChecker.isPangram("\"Five quacking Zephyrs jolt my wax bed.\"")).isTrue();
   }
 
   @Test
   public void caseInsensitivePhraseIsNotPangram() {
-    assertFalse(pangramChecker.isPangram("the quick brown fox jumps over with lazy FX"));
+    assertThat(pangramChecker.isPangram("abcdefghijklm ABCDEFGHIJKLM")).isFalse();
   }
 }
