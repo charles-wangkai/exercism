a67d85039a0c729d178f5db47fd42f43d802c630
Kai Wang <charles.wangkai@gmail.com>
2025-01-26 13:06:10 +0800

    java/resistor-color-duo

--- java/resistor-color-duo/src/test/java/ResistorColorDuoTest.java
+++ java/resistor-color-duo/src/test/java/ResistorColorDuoTest.java
@@ -1,12 +1,12 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ResistorColorDuoTest {
   private ResistorColorDuo resistorColorDuo;
 
-  @Before
+  @BeforeEach
   public void setup() {
     resistorColorDuo = new ResistorColorDuo();
   }
@@ -29,6 +29,16 @@ public class ResistorColorDuoTest {
   @Test
   public void testOrangeAndOrange() {
     assertThat(resistorColorDuo.value(new String[] {"orange", "orange"})).isEqualTo(33);
+  }
+
+  @Test
+  public void testWhiteAndRed() {
+    assertThat(resistorColorDuo.value(new String[] {"white", "red"})).isEqualTo(92);
+  }
+
+  @Test
+  public void testBlackAndBrownOneDigit() {
+    assertThat(resistorColorDuo.value(new String[] {"black", "brown"})).isEqualTo(1);
   }
 
   @Test
