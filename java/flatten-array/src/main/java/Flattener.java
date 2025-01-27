505a5f4ce1aacb9d7d68a13bc34a5060d47e3893
Kai Wang <charles.wangkai@gmail.com>
2025-01-27 09:20:40 +0800

    java/flatten-array

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -3,20 +3,20 @@ import java.util.List;
 
 public class Flattener {
   List<Object> flatten(List<Object> list) {
-    List<Object> result = new ArrayList<>();
-    search(result, list);
+    List<Object> values = new ArrayList<>();
+    search(values, list);
 
-    return result;
+    return values;
   }
 
   @SuppressWarnings("unchecked")
-  void search(List<Object> result, List<Object> list) {
+  void search(List<Object> values, List<Object> list) {
     for (Object element : list) {
       if (element != null) {
-        if (element instanceof List) {
-          search(result, (List<Object>) element);
+        if (element instanceof List l) {
+          search(values, l);
         } else {
-          result.add(element);
+          values.add(element);
         }
       }
     }
--- java/flatten-array/src/test/java/FlattenerTest.java
+++ java/flatten-array/src/test/java/FlattenerTest.java
@@ -1,93 +1,117 @@
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class FlattenerTest {
 
   private Flattener flattener;
 
-  @Before
+  @BeforeEach
   public void setUp() {
     flattener = new Flattener();
   }
 
   @Test
+  public void testEmpty() {
+    assertThat(flattener.flatten(emptyList())).isEmpty();
+  }
+
+  @Test
   public void testFlatListIsPreserved() {
-    assertEquals(asList(0, '1', "two"), flattener.flatten(asList(0, '1', "two")));
+    assertThat(flattener.flatten(asList(0, '1', "two"))).containsExactly(0, '1', "two");
+  }
+
+  @Test
+  public void testNestedList() {
+    assertThat(flattener.flatten(singletonList(emptyList()))).isEmpty();
   }
 
   @Test
   public void testASingleLevelOfNestingWithNoNulls() {
-    assertEquals(
-        asList(1, '2', 3, 4, 5, "six", "7", 8),
-        flattener.flatten(asList(1, asList('2', 3, 4, 5, "six", "7"), 8)));
+    assertThat(flattener.flatten(asList(1, asList('2', 3, 4, 5, "six", "7"), 8)))
+        .containsExactly(1, '2', 3, 4, 5, "six", "7", 8);
   }
 
   @Test
   public void testFiveLevelsOfNestingWithNoNulls() {
-    assertEquals(
-        asList(0, '2', 2, "three", '8', 100, "four", 50, "-2"),
-        flattener.flatten(
-            asList(
-                0,
-                '2',
+    assertThat(
+            flattener.flatten(
                 asList(
-                    asList(2, "three"),
-                    '8',
-                    100,
-                    "four",
-                    singletonList(singletonList(singletonList(50)))),
-                "-2")));
+                    0,
+                    '2',
+                    asList(
+                        asList(2, "three"),
+                        '8',
+                        100,
+                        "four",
+                        singletonList(singletonList(singletonList(50)))),
+                    "-2")))
+        .containsExactly(0, '2', 2, "three", '8', 100, "four", 50, "-2");
   }
 
   @Test
   public void testSixLevelsOfNestingWithNoNulls() {
-    assertEquals(
-        asList("one", '2', 3, '4', 5, "six", 7, "8"),
-        flattener.flatten(
-            asList(
-                "one",
+    assertThat(
+            flattener.flatten(
                 asList(
-                    '2',
-                    singletonList(singletonList(3)),
-                    asList('4', singletonList(singletonList(5))),
-                    "six",
-                    7),
-                "8")));
+                    "one",
+                    asList(
+                        '2',
+                        singletonList(singletonList(3)),
+                        asList('4', singletonList(singletonList(5))),
+                        "six",
+                        7),
+                    "8")))
+        .containsExactly("one", '2', 3, '4', 5, "six", 7, "8");
+  }
+
+  @Test
+  public void testNullValuesAreOmitted() {
+    assertThat(flattener.flatten(asList("1", "two", null))).containsExactly("1", "two");
+  }
+
+  @Test
+  public void testConsecutiveNullValuesAtFrontOfListAreOmitted() {
+    assertThat(flattener.flatten(asList(null, null, 3))).containsExactly(3);
+  }
+
+  @Test
+  public void testConsecutiveNullValuesInMiddleOfListAreOmitted() {
+    assertThat(flattener.flatten(asList(1, null, null, "4"))).containsExactly(1, "4");
   }
 
   @Test
   public void testSixLevelsOfNestingWithNulls() {
-    assertEquals(
-        asList("0", 2, "two", '3', "8", "one hundred", "negative two"),
-        flattener.flatten(
-            asList(
-                "0",
-                2,
+    assertThat(
+            flattener.flatten(
                 asList(
-                    asList("two", '3'),
-                    "8",
-                    singletonList(singletonList("one hundred")),
-                    null,
-                    singletonList(singletonList(null))),
-                "negative two")));
+                    "0",
+                    2,
+                    asList(
+                        asList("two", '3'),
+                        "8",
+                        singletonList(singletonList("one hundred")),
+                        null,
+                        singletonList(singletonList(null))),
+                    "negative two")))
+        .containsExactly("0", 2, "two", '3', "8", "one hundred", "negative two");
   }
 
   @Test
   public void testNestedListsFullOfNullsOnly() {
-    assertEquals(
-        emptyList(),
-        flattener.flatten(
-            asList(
-                null,
-                singletonList(singletonList(singletonList(null))),
-                null,
-                null,
-                asList(asList(null, null), null),
-                null)));
+    assertThat(
+            flattener.flatten(
+                asList(
+                    null,
+                    singletonList(singletonList(singletonList(null))),
+                    null,
+                    null,
+                    asList(asList(null, null), null),
+                    null)))
+        .isEmpty();
   }
 }
