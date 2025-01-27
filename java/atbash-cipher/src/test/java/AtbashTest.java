9ed53a73df9f420e410cc03433bde441ec5a966d
Kai Wang <charles.wangkai@gmail.com>
2025-01-27 09:08:52 +0800

    java/atbash-cipher

--- java/atbash-cipher/src/test/java/AtbashTest.java
+++ java/atbash-cipher/src/test/java/AtbashTest.java
@@ -1,13 +1,13 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class AtbashTest {
 
   private Atbash atbash;
 
-  @Before
+  @BeforeEach
   public void setup() {
     atbash = new Atbash();
   }
