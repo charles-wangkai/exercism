5961e0785f4916e696a83e797eec864b43a8b388
Kai Wang <charles.wangkai@gmail.com>
2025-01-28 17:55:49 +0800

    java/beer-song

--- java/beer-song/src/main/java/BeerSong.java
+++ java/beer-song/src/main/java/BeerSong.java
@@ -5,26 +5,25 @@ public class BeerSong {
   String verse(int verse) {
     if (verse == 0) {
       return """
-				No more bottles of beer on the wall, no more bottles of beer.
-				Go to the store and buy some more, 99 bottles of beer on the wall.
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall.
 
-				""";
+      """;
     }
     if (verse == 1) {
       return """
-				1 bottle of beer on the wall, 1 bottle of beer.
-				Take it down and pass it around, no more bottles of beer on the wall.
+      1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.
 
-				""";
+      """;
     }
 
-    return String.format(
-        """
-				%d bottles of beer on the wall, %d bottles of beer.
-				Take one down and pass it around, %d bottle%s of beer on the wall.
+    return """
+    %d bottles of beer on the wall, %d bottles of beer.
+    Take one down and pass it around, %d bottle%s of beer on the wall.
 
-				""",
-        verse, verse, verse - 1, (verse == 2) ? "" : "s");
+    """
+        .formatted(verse, verse, verse - 1, (verse == 2) ? "" : "s");
   }
 
   String sing(int startVerse, int verseNum) {
