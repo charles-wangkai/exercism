118039e1f6dac64b250d5cfcc1a5ff55588d0a6a
Kai Wang <charles.wangkai@gmail.com>
2025-01-29 10:09:31 +0800

    java/binary-search-tree

--- java/binary-search-tree/src/test/java/BinarySearchTreeTest.java
+++ java/binary-search-tree/src/test/java/BinarySearchTreeTest.java
@@ -2,7 +2,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class BinarySearchTreeTest {
 
